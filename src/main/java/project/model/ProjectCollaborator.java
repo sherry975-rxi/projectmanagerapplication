3b9956b21fef20026c9dd5e04e459cc9754ddac4
João Bessa <1171465>
2017-12-06 15:22:19 +0000

    Added ProjectCollaborator class and tests (100% Coverage)

---
+++ src/main/java/project/model/ProjectCollaborator.java
@@ -0,0 +1,60 @@
+package main.java.project.model;
+
+public class ProjectCollaborator {
+
+	private User collaborator;
+	private boolean inProject;
+	private int costPerEffort;
+
+	/**
+	 * Constructor of the class ProjectCollaborator. This contains a collaborator's
+	 * user data, their status on the project, and their cost per effort.
+	 * 
+	 * @param collab
+	 *            the collaborator's user data
+	 * @param cost
+	 *            how much the collaborator costs per unit of effort
+	 */
+
+	public ProjectCollaborator(User collab, int cost) {
+		this.collaborator = collab;
+		this.inProject = true;
+		this.costPerEffort = cost;
+	}
+
+	/**
+	 * This method sets the Collaborator as working or not working on the project.
+	 * 
+	 * @param status
+	 *            determines whether the collaborator is still working on the
+	 *            project (true by default)
+	 */
+	public void setState(boolean status) {
+		this.inProject = status;
+	}
+
+	/**
+	 * This method views whether the Collaborator is working or not on the project
+	 * 
+	 */
+	public boolean isCollaboratorInProject() {
+		return this.inProject;
+	}
+
+	/**
+	 * This method views the Collaborator's cost per effort unit
+	 * 
+	 */
+	public int getCollaboratorCost() {
+		return this.costPerEffort;
+	}
+
+	/**
+	 * This method views the Collaborator's User Data
+	 * 
+	 */
+	public User getCollaboratorUserData() {
+		return this.collaborator;
+	}
+
+}
