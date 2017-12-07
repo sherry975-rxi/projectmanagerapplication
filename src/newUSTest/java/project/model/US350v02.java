bce421f8839122226aeb801fb3fa30ad49afa861
Inês Pinto <1171476@isep.ipp.pt>
2017-12-07 12:02:53 +0000

    Create newUStest folder and file

---
+++ src/newUSTest/java/project/model/US350v02.java
@@ -0,0 +1,14 @@
+package newUSTest.java.project.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class US350v02 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
