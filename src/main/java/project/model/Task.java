1b949fd04cefa786502aa47c307d33335601f7f8
Fábio  Brás <1160058@isep.ipp.pt>
2017-12-07 14:22:31 +0000

    Changed addUserToTask and  removeUserFromTask to void

--- src/main/java/project/model/Task.java
+++ src/main/java/project/model/Task.java
@@ -246,11 +246,11 @@ public class Task {
 	 * @return TRUE if the user was added to the list of users (taskTeam) in a task
 	 *         FALSE if user is already in the list of users in a task
 	 */
-	public boolean addUserToTask(User user) {
+	public void addUserToTask(User user) {
 		if (!taskTeamContainsUser(user)) {
-			return this.taskTeam.add(new TaskWorker(user));
+			this.taskTeam.add(new TaskWorker(user));
 		}
-		return false;
+
 	}
 
 	/**
@@ -265,13 +265,12 @@ public class Task {
 	 *         task or if it was missing from the list
 	 */
 
-	public boolean removeUserFromTask(User user) {
+	public void removeUserFromTask(User user) {
 		for (TaskWorker other : taskTeam) {
 			if (other.getCollaborator().equals(user)) {
 				other.disableCollaborator();
 			}
 		}
-		return false;
 	}
 
 	public double getTimeSpentOntask(User user) {
