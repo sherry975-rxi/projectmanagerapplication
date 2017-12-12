5d0a58a8c6ac24e4d685cd9eb928a2f74dd3e2a5
Fábio  Brás <1160058@isep.ipp.pt>
2017-12-12 17:13:58 +0000

    Revert to [161449f]

---
+++ src/test/usTest/java/project/model/US112Tests.java
@@ -0,0 +1,86 @@
+package test.usTest.java.project.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.java.project.model.Company;
+import main.java.project.model.Profile;
+import main.java.project.model.User;
+
+class US112Tests {
+
+	/**
+	 * Tests US112
+	 * 
+	 * US112: Como Administrador, pretendo atribuir o perfil de "Colaborador" a
+	 * utilizadores registados.
+	 * 
+	 * uses methods createUser(), addUserToUserList(), setUserProfile(),
+	 * getProfile(), getProfileInt()
+	 * 
+	 * @author Rita Machado
+	 * @author João Bessa
+	 * 
+	 */
+	Company Armis;
+	User newUser2;
+	User newUser3;
+
+	@BeforeEach
+	void setUp() {
+		Armis = Company.getTheInstance();
+		Armis.getUsersRepository().getAllUsersFromRepository().clear();
+		Armis.getProjectsRepository().getAllProjects().clear();
+		newUser2 = Armis.getUsersRepository().createUser("Manel", "user2@gmail.com", "001", "Empregado", "930000000",
+				"Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
+		newUser3 = Armis.getUsersRepository().createUser("Manelinho", "user3@gmail.com", "002", "Telefonista",
+				"940000000", "Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
+
+		Armis.getUsersRepository().addUserToUserRepository(newUser2);
+	}
+
+	@AfterEach
+	void tearDown() {
+		Armis = null;
+		newUser2 = null;
+		newUser3 = null;
+
+	}
+
+	/**
+	 * Tests the setUserProfile method to check if the profile collaborator was
+	 * correctly attributed. Then attempts to promote the User to Director, then
+	 * demote back to Collaborator; Compares the User's profile to Collaborator and
+	 * Director ID's (must be equal)
+	 */
+	@Test
+	void testSetUserAsCollaborator() {
+
+		assertTrue(Armis.getUsersRepository().getAllUsersFromRepository().contains(newUser2));
+		assertFalse(Armis.getUsersRepository().getAllUsersFromRepository().contains(newUser3));
+
+		assertEquals(newUser2.getUserProfile(), Profile.VISITOR);
+
+		Armis.getUsersRepository().addUserToUserRepository(newUser3);
+
+		newUser3.setUserProfile(Profile.COLLABORATOR);
+
+		assertEquals(newUser2.getUserProfile(), Profile.VISITOR);
+		assertEquals(newUser3.getUserProfile(), Profile.COLLABORATOR);
+
+		// newUser2 becomes Director
+
+		newUser2.setUserProfile(Profile.DIRECTOR);
+		assertEquals(newUser2.getUserProfile(), Profile.DIRECTOR);
+
+		// newUser2 becomes Collaborador
+		newUser2.setUserProfile(Profile.COLLABORATOR);
+		assertEquals(newUser2.getUserProfile(), Profile.COLLABORATOR);
+
+	}
+}
---
+++ src/test/usTest/java/project/model/US136Tests.java
@@ -0,0 +1,79 @@
+package test.usTest.java.project.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.java.project.model.Company;
+import main.java.project.model.Profile;
+import main.java.project.model.User;
+
+class US136Tests {
+
+	/**
+	 * Tests US136
+	 * 
+	 * US136: Como Administrador, pretendo pesquisar utilizadores do sistema por
+	 * perfil de utilizador.
+	 * 
+	 * uses methods searchUsersByProfile()
+	 * 
+	 */
+
+	Company myCompany;
+	User newUser2;
+	User newUser3;
+
+	@BeforeEach
+	void setUp() {
+		myCompany = Company.getTheInstance();
+		myCompany.getUsersRepository().getAllUsersFromRepository().clear();
+		myCompany.getProjectsRepository().getAllProjects().clear();
+		newUser2 = myCompany.getUsersRepository().createUser("Manel", "user2@gmail.com", "001", "Empregado",
+				"930000000", "rua cinzenta", "6789-654", "porto", "porto", "portugal");
+		newUser3 = myCompany.getUsersRepository().createUser("Manelinho", "user3@gmail.com", "002", "Telefonista",
+				"940000000", "rua rosa", "6799-654", "porto", "porto", "portugal");
+
+		/* Set the testCollab profile type to collaborator */
+
+		/* Set the newUser2 and newUser3 profile type to collaborator */
+		newUser2.setUserProfile(Profile.COLLABORATOR);
+		newUser3.setUserProfile(Profile.COLLABORATOR);
+	}
+
+	@AfterEach
+	void tearDown() {
+		myCompany = null;
+		newUser2 = null;
+		newUser3 = null;
+
+	}
+
+	@Test
+	void testSearchUsersByProfile() {
+		/* Compares a search of a profile type that doesn't exist with a empty List */
+		List<User> emptyList = new ArrayList<User>();
+		assertEquals(myCompany.getUsersRepository().searchUsersByProfile(Profile.COLLABORATOR), emptyList);
+	}
+
+	@Test
+	void testSearchUsersByProfile2() {
+		/* Adds the created users to the Company user list */
+		myCompany.getUsersRepository().addUserToUserRepository(newUser2);
+		myCompany.getUsersRepository().addUserToUserRepository(newUser3);
+
+		/*
+		 * Compares a search of collaborator with a list where two collaborators exist
+		 */
+		List<User> collaboratorstest = new ArrayList<User>();
+		collaboratorstest.add(newUser2);
+		collaboratorstest.add(newUser3);
+
+		assertEquals(myCompany.getUsersRepository().searchUsersByProfile(Profile.COLLABORATOR), collaboratorstest);
+	}
+}
