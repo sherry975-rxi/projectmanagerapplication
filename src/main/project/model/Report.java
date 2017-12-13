8b311e7b63586bd9b0bab04861597d58c327e2b9
João Leite <1161661@isep.ipp.pt>
2017-12-13 11:47:20 +0000

    added TaskWorker in Report Class

--- src/main/project/model/Report.java
+++ src/main/project/model/Report.java
@@ -3,10 +3,12 @@ package main.project.model;
 public class Report {
 
 	int reportedTime;
+	TaskWorker taskWorker;
 
-	public Report(int time) {
+	public Report(int time, TaskWorker taskWorker) {
 
 		this.reportedTime = time;
+		this.taskWorker = taskWorker;
 	}
 
 }
