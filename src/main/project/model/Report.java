9385bc085621dd6f81440c92d6a87f323fe43eeb
Rui Miguel Borges <1171473@isep.ipp.pt>
2017-12-13 13:05:49 +0000

    Refactor of class Report RuiBorgesRitaMachado

--- src/main/project/model/Report.java
+++ src/main/project/model/Report.java
@@ -1,14 +1,25 @@
 package main.project.model;
 
+import java.util.Calendar;
+
 public class Report {
 
 	private int reportedTime;
 	private TaskWorker taskWorker;
 
-	public Report(int time, TaskWorker taskWorker) {
+	public Report(TaskWorker taskWorker) {
 
-		this.reportedTime = time;
+		this.reportedTime = 0;
 		this.taskWorker = taskWorker;
+	}
+	
+	public void setReportedTime( int time) {
+		this.reportedTime = time;
+		
+	}
+	
+	public int getReportedTime() {
+		return this.reportedTime;
 	}
 
 }
