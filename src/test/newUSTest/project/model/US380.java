0a2901166342b282addee65fde6cf3a9a65fd247
Daniel Almeida <1171459@isep.ipp.pt>
2017-12-13 15:46:17 +0000

    Created US380 test

---
+++ src/test/newUSTest/project/model/US380.java
@@ -0,0 +1,14 @@
+package test.newUSTest.project.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class US380 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
