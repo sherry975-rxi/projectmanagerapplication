326c81eb65a4eaf2db0c7b6259ceca8b218c0321
João Leite <1161661@isep.ipp.pt>
2017-12-20 13:28:24 +0000

    Added comments to DoLoginController

--- src/main/java/project/controller/DoLoginController.java
+++ src/main/java/project/controller/DoLoginController.java
@@ -5,6 +5,14 @@ public class DoLoginController {
 	private String username;
 	private String password;
 
+	/**
+	 * This method does the login of the user
+	 * 
+	 * @param username
+	 *            Sets private variable username to inserted username by user
+	 * @param password
+	 *            Sets private variable password to inserted password by user
+	 */
 	public void doLogin(String username, String password) {
 
 		this.username = username;
@@ -12,6 +20,10 @@ public class DoLoginController {
 
 	}
 
+	/**
+	 * this method does logOut of user. Sets the private variables of
+	 * DoLoginController to null
+	 */
 	public void doLogOut() {
 
 		this.username = null;
