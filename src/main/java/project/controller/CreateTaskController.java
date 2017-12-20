0171547529cd99befe6b37f36798998314b9fd59
João Afonso <Joo Afonso@LBessa-TOSH>
2017-12-20 12:09:25 +0000

    Added comments to createTaskController

--- src/main/java/project/controller/CreateTaskController.java
+++ src/main/java/project/controller/CreateTaskController.java
@@ -12,12 +12,26 @@ public class CreateTaskController {
 	private TaskRepository target;
 	
 	// TODO must receive the project the User manages
-	
+	/**
+	 * This constructor creates a target controller. Currently, it recieves a project but it should recieve a Project Controller or Project Manager controller
+	 * 
+	 * 
+	 * @param target
+	 */
 	public CreateTaskController(Project target) {
 		
 		this.target=target.getTaskRepository();
 	}
-	
+	/**
+	 * This controller calls the create task method, then the add task method, and finally confirms whether it was added successfully
+	 * 
+	 * @param taskDescription
+	 * @param estimatedTaskEffort
+	 * @param estimatedTaskStartDate
+	 * @param taskDeadline
+	 * @param estimatedCost
+	 * @return a string stating whether or not the task was added successfully
+	 */
 	public String addTask(String taskDescription, int estimatedTaskEffort, Calendar estimatedTaskStartDate, Calendar taskDeadline, int estimatedCost) {
 		Task toAdd = target.createTask(taskDescription, estimatedTaskEffort, estimatedTaskStartDate, taskDeadline, estimatedCost);
 		
