91da1249c93187acaffa7fa3aa67f98dcf9482e1
Inês Pinto <1171476@isep.ipp.pt>
2017-12-20 11:08:47 +0000

    Create addCollaboratorToProjectTeamController class.

--- src/main/java/project/controller/addCollaboratorToProjectTeamController.java
+++ src/main/java/project/controller/addCollaboratorToProjectTeamController.java
@@ -1,9 +1,21 @@
 package project.controller;
 
+import project.model.Company;
+import project.model.Project;
+import project.model.User;
+
 public class addCollaboratorToProjectTeamController {
 
-	public addCollaboratorToProjectTeamController() {
-		// TODO Auto-generated constructor stub
+	private Company c1;
+	private User u1;
+	private Project p1;
+	
+	public addCollaboratorToProjectTeamController(Company c1, User u1, Project p1) {
+		c1 = Company.getTheInstance();
+		this.u1 = u1;
+		this.p1 = p1;
+		
+		
 	}
 
 }
