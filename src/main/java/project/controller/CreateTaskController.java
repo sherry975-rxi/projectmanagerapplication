49e56db59c64d476fbb71aa25daf3dc6ba2f2317
João Afonso <Joo Afonso@LBessa-TOSH>
2017-12-20 13:37:49 +0000

    Simplified addTask method on Controller

--- src/main/java/project/controller/CreateTaskController.java
+++ src/main/java/project/controller/CreateTaskController.java
@@ -9,7 +9,7 @@ import project.model.Task;
 
 public class CreateTaskController {
 	
-	private TaskRepository target;
+	private TaskRepository taskRepository;
 	
 	// TODO must receive the project the User manages
 	/**
@@ -20,11 +20,11 @@ public class CreateTaskController {
 	 */
 	public CreateTaskController(TaskRepository target) {
 		
-		this.target=target;
+		this.taskRepository=target;
 	}
 	
 	public TaskRepository getTaskRepository() {
-		return this.target;
+		return this.taskRepository;
 	}
 	/**
 	 * This controller calls the create task method, then the add task method, and finally confirms whether it was added successfully
@@ -37,13 +37,11 @@ public class CreateTaskController {
 	 * @return the added task
 	 */
 	public Task addTask(String taskDescription, int estimatedTaskEffort, Calendar estimatedTaskStartDate, Calendar taskDeadline, int estimatedCost) {
-		Task toAdd = target.createTask(taskDescription, estimatedTaskEffort, estimatedTaskStartDate, taskDeadline, estimatedCost);
+		Task newTask = taskRepository.createTask(taskDescription, estimatedTaskEffort, estimatedTaskStartDate, taskDeadline, estimatedCost);
 		
-		target.addProjectTask(toAdd);
-		
-		int index = target.getUnstartedTasks().size();
-		
-		return target.getUnstartedTasks().get(index-1);
+		taskRepository.addProjectTask(newTask);
+
+		return newTask;
 	}
 
 }
