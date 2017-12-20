a1e38c4bb353713f35001edeb282cb71e2eda719
Inês Pinto <1171476@isep.ipp.pt>
2017-12-20 11:07:39 +0000

    Create addCollaboratorToProjectTeamController class.

---
+++ src/main/java/project/controller/addCollaboratorToProjectTeamController.java
@@ -0,0 +1,9 @@
+package project.controller;
+
+public class addCollaboratorToProjectTeamController {
+
+	public addCollaboratorToProjectTeamController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
