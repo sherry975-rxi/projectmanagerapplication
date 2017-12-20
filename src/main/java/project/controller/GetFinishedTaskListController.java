dab1a743ccb37bda327f3550c58751676f2e4301
João Leite <1161661@isep.ipp.pt>
2017-12-20 12:36:54 +0000

    Added comments to class getFinishedTaskListController

--- src/main/java/project/controller/GetFinishedTaskListController.java
+++ src/main/java/project/controller/GetFinishedTaskListController.java
@@ -7,6 +7,13 @@ import project.model.Task;;
 
 public class GetFinishedTaskListController {
 
+	/**
+	 * @param p1
+	 *            The project to search for it's finished tasks
+	 * @return This method returns a list of tasks that belong to p1 and were marked
+	 *         as finished.
+	 */
+
 	public List<Task> getFinishedTaskListController(Project p1) {
 
 		return p1.getTaskRepository().getFinishedTasks();
