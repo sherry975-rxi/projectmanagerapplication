e56229c373e0853aa32b3ecf31d4cc422058ce04
João Leite <1161661@isep.ipp.pt>
2017-12-21 11:57:18 +0000

    Created another method in SearchUsersController (now it search users by email and by profile)

--- src/test/java/project/controller/CreateProjectControllerTest.java
+++ src/test/java/project/controller/CreateProjectControllerTest.java
@@ -1,14 +1,12 @@
 package project.controller;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class CreateProjectControllerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createProjectControllerTest() {
+
 	}
 
 }
