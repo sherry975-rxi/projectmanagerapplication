812f15884483720f7c434f2cfdf48d91d923b87b
Inês Pinto <1171476@isep.ipp.pt>
2017-12-21 14:33:01 +0000

    Create addNewAddressTest controller in UpdateUserInfoController_Test

--- src/test/java/project/controller/UpdateUserInfoController_Test.java
+++ src/test/java/project/controller/UpdateUserInfoController_Test.java
@@ -1,9 +1,13 @@
 package project.controller;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import project.model.Address;
 import project.model.Company;
 import project.model.Profile;
 import project.model.User;
@@ -12,6 +16,7 @@ public class UpdateUserInfoController_Test {
 
 	Company c1;
 	User u1;
+	Address address1;
 
 	@Before
 	public void setUp() {
@@ -22,12 +27,16 @@ public class UpdateUserInfoController_Test {
 		// create users
 		u1 = c1.getUsersRepository().createUser("Daniel", "user2@gmail.com", "123", "Empregado", "930000000",
 				"Rua Maria", "4444-444", "221234567", "Porto", "Portugal");
-
+		
+		// create a new address
+		address1 = u1.createAddress("Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
+		
 		// set user profile to collaborator
 		u1.setUserProfile(Profile.COLLABORATOR);
 
 		// add users to company
 		c1.getUsersRepository().addUserToUserRepository(u1);
+		
 
 	}
 
@@ -35,6 +44,7 @@ public class UpdateUserInfoController_Test {
 	public void tearDown() {
 		Company.clear();
 		u1 = null;
+		address1 = null;
 	}
 
 	@Test
@@ -42,9 +52,16 @@ public class UpdateUserInfoController_Test {
 
 	}
 
+	/**
+	 * This test BLABLABLA
+	 */
 	@Test
-	public void addNewAddress() {
-
+	public void addNewAddressTest() {
+		addNewAddressController testController = new addNewAddressController;
+		
+		assertFalse(u1.getAddressList().contains(address1));
+		u1.addAddress(address1);
+		assertTrue(u1.getAddressList().contains(address1));
 	}
 
 }
