6c6cb6ada21a95cb58e419752b4c04d50e2d2cab
Daniel Pereira <1171460@isep.ipp.pt>
2017-12-21 14:51:39 +0000

    Added a step to create the controller in the setUp Removed useless lines of code in the test

--- src/test/java/project/controller/TasksFilterControllerTest.java
+++ src/test/java/project/controller/TasksFilterControllerTest.java
@@ -1,10 +1,8 @@
 package project.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -19,34 +17,33 @@ import project.model.TaskCollaborator;
 import project.model.User;
 
 public class TasksFilterControllerTest {
-	
+
 	TasksFiltersController tasksFiltersController;
 	Company company1;
-	User user1, user2, user3; 
+	User user1, user2, user3;
 	Project project1;
 	ProjectCollaborator projCollab1, projCollab2, projCollab3;
 	Task task1, task2, task3;
 	TaskCollaborator taskCollab1, taskCollab2;
 
-
 	@Before
 	public void setUp() {
 		// create company 1
 		company1 = Company.getTheInstance();
-		
-		//create users in company
+
+		// create users in company
 		user2 = company1.getUsersRepository().createUser("João", "user2@gmail.com", "001", "Manager", "930025000",
 				"rua doutor antónio", "7689-654", "porto", "porto", "portugal");
 		user1 = company1.getUsersRepository().createUser("Juni", "user3@gmail.com", "002", "Code Monkey", "930000000",
 				"rua engenheiro joão", "789-654", "porto", "porto", "portugal");
-		
-		//change profiles of users from VISITOR (default) to COLLABORATOR
+
+		// change profiles of users from VISITOR (default) to COLLABORATOR
 		user2.setUserProfile(Profile.COLLABORATOR);
 		user1.setUserProfile(Profile.COLLABORATOR);
-		
-		//create project 1 in company 1
+
+		// create project 1 in company 1
 		project1 = company1.getProjectsRepository().createProject("name3", "description4", user2);
-		
+
 		// add project 1 to company 1
 		company1.getProjectsRepository().addProjectToProjectRepository(project1);
 
@@ -56,7 +53,7 @@ public class TasksFilterControllerTest {
 		estimatedTaskStartDateTest.set(Calendar.MONTH, Calendar.SEPTEMBER);
 		estimatedTaskStartDateTest.set(Calendar.DAY_OF_MONTH, 25);
 		estimatedTaskStartDateTest.set(Calendar.HOUR_OF_DAY, 14);
-		
+
 		// create an estimated Task Dead line Date
 		Calendar taskDeadlineDateTest = Calendar.getInstance();
 		taskDeadlineDateTest.set(Calendar.YEAR, 2018);
@@ -64,7 +61,8 @@ public class TasksFilterControllerTest {
 		taskDeadlineDateTest.set(Calendar.DAY_OF_MONTH, 29);
 		taskDeadlineDateTest.set(Calendar.HOUR_OF_DAY, 14);
 
-		// create a Date before to the previous Dead line created in order to result in an expired Task
+		// create a Date before to the previous Dead line created in order to result in
+		// an expired Task
 		Calendar taskExpiredDeadlineDateTest = Calendar.getInstance();
 		taskExpiredDeadlineDateTest.set(Calendar.YEAR, 2017);
 		taskExpiredDeadlineDateTest.set(Calendar.MONTH, Calendar.SEPTEMBER);
@@ -72,68 +70,72 @@ public class TasksFilterControllerTest {
 		taskExpiredDeadlineDateTest.set(Calendar.HOUR_OF_DAY, 14);
 
 		// create tasks in project 1
-		task1 = project1.getTaskRepository().createTask("Do this", 10, estimatedTaskStartDateTest,
-				taskDeadlineDateTest, 10);
-		task2 = project1.getTaskRepository().createTask("Do that", 10, estimatedTaskStartDateTest,
-				taskDeadlineDateTest, 10);
+		task1 = project1.getTaskRepository().createTask("Do this", 10, estimatedTaskStartDateTest, taskDeadlineDateTest,
+				10);
+		task2 = project1.getTaskRepository().createTask("Do that", 10, estimatedTaskStartDateTest, taskDeadlineDateTest,
+				10);
 		task3 = project1.getTaskRepository().createTask("Merge everything", 10, estimatedTaskStartDateTest,
 				taskDeadlineDateTest, 10);
-		
+
 		// add tasks to task repository of project 1
 		project1.getTaskRepository().addProjectTask(task1);
 		project1.getTaskRepository().addProjectTask(task2);
-		
-		//add costPerEffort to users in project 1, resulting in a Project Collaborator for each one
+
+		// add costPerEffort to users in project 1, resulting in a Project Collaborator
+		// for each one
 		projCollab1 = project1.createProjectCollaborator(user1, 250);
 		projCollab2 = project1.createProjectCollaborator(user2, 120);
 		projCollab3 = project1.createProjectCollaborator(user2, 200);
-		
+
 		// associate Project Collaborators to project 1 (info user + costPerEffort)
 		project1.addProjectCollaboratorToProjectTeam(projCollab1);
 		project1.addProjectCollaboratorToProjectTeam(projCollab2);
-		
-		// create Task Collaborator to regist the period that the user was in the task while he was active in project 1 
+
+		// create Task Collaborator to regist the period that the user was in the task
+		// while he was active in project 1
 		taskCollab1 = task1.createTaskCollaborator(projCollab1);
 		taskCollab2 = task2.createTaskCollaborator(projCollab2);
 
-		// associate Task Collaborators to task (info project collaborator + period he was in the task)
+		// associate Task Collaborators to task (info project collaborator + period he
+		// was in the task)
 		task1.addTaskCollaboratorToTask(taskCollab1);
 		task2.addTaskCollaboratorToTask(taskCollab2);
 
 		// defines finish date to task, and mark it as Finished
 		task1.setFinishDate();
 		task1.markTaskAsFinished();
+
+		// creates the controller
+		tasksFiltersController = new TasksFiltersController();
+		tasksFiltersController.setMyCompany(company1);
 	}
-	
+
 	@After
 	public void tearDown() {
 		Company.clear();
-		user1= null;
-		user2= null;
-		user3= null;
-		project1= null;
-		projCollab1= null;
-		projCollab2= null;
-		projCollab3= null;
-		task1= null;
-		task2= null;
-		task3= null;
-		taskCollab1= null;
-		taskCollab2= null;
+		user1 = null;
+		user2 = null;
+		user3 = null;
+		project1 = null;
+		projCollab1 = null;
+		projCollab2 = null;
+		projCollab3 = null;
+		task1 = null;
+		task2 = null;
+		task3 = null;
+		taskCollab1 = null;
+		taskCollab2 = null;
+		tasksFiltersController = null;
 	}
-	
+
 	/**
 	 * US203: Como colaborador, eu pretendo consultar a minha lista de tarefas
 	 * pendentes de modo a saber o que tenho para fazer hoje.
 	 */
 	@Test
 	public void testGetUserUnfinishedTaskList() {
-		
-		// create a test list with the expected result
-		List<Task> testList = new ArrayList<Task>();
-		testList.add(task2);
 
-		assertEquals(testList.size(),tasksFiltersController.getUserUnfinishedTaskList(user2).size());
+		assertEquals(1, tasksFiltersController.getUserUnfinishedTaskList(user2).size());
 	}
 
 }
