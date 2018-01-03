b0dccecd0858131578d1d68f8f19810e785b3cbc
Rita Machado <1171471@isep.ipp.pt>
2018-01-03 16:28:18 +0000

    Create method getProjectsfromUser() in US342Controller Class in Controller Package RitaMachado_DanielAlmeida

--- src/main/java/project/controller/US342Controller.java
+++ src/main/java/project/controller/US342Controller.java
@@ -1,6 +1,12 @@
 package project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import project.model.Company;
+import project.model.Project;
 import project.model.Task;
+import project.model.User;
 
 /**
  * @author Group 3
@@ -10,8 +16,29 @@ import project.model.Task;
  *
  */
 public class US342Controller {
+	
+	/**
+	 * This method returns a set of Projects where a certain user was defined as Project Manager
+	 * 
+	 * @param projectManager User defined as Project Manager
+	 * 
+	 * @return List of Projects of a Project Manager
+	 */
+	public List<Project> getProjectsfromUser (User projectManager) {
+		
+		Company singleCompany = Company.getTheInstance();
+		
+		List<Project> listOfProjectsOfProjectManager = new ArrayList<>();
+		
+		listOfProjectsOfProjectManager.addAll(singleCompany.getProjectsRepository().getProjectsOfProjectManager(projectManager));
+		
+		return listOfProjectsOfProjectManager;
+	}
+	
 
 	/**
+	 * This method creates the Dependence of a Task from another task and defines the number of days that must be spent util the dependent task starts.
+	 * 
 	 * @param taskDependent
 	 *            Task that will be set as dependent from another one.
 	 * @param taskReference
@@ -24,7 +51,8 @@ public class US342Controller {
 	public void createDependenceFromTask(Task taskDependent, Task taskReference, int incrementDays) {
 
 		taskDependent.createTaskDependence(taskReference, incrementDays);
-
 	}
+	
+	
 
 }
