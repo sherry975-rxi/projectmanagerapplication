3eb5aaa29c5c4d20d330c09415f08500dd01a88a
Daniel Almeida <1171459@isep.ipp.pt>
2018-01-04 18:02:18 +0000

    Changed the names of the tasks instatiated in the MainUi Class.

--- src/main/java/project/ui/MainMenuUI.java
+++ src/main/java/project/ui/MainMenuUI.java
@@ -46,11 +46,10 @@ public class MainMenuUI {
 		taskDeadline.set(2018, Calendar.DECEMBER, 22);
 
 		// Instantiates the tasks
-		task1 = project1.getTaskRepository().createTask("descriptiondescriptiondescriptiondescriptiondescription", 10, estimatedTaskStartDate, taskDeadline, 1000);
-		task2 = project1.getTaskRepository().createTask("DESCRIPTIONDESCRIPTIONDESCRIPTIONDESCRIPTIONDESCRIPTIONDESCRIPTION", 10, estimatedTaskStartDate, taskDeadline, 1000);
+		task1 = project1.getTaskRepository().createTask("Tarefa aaaa", 10, estimatedTaskStartDate, taskDeadline, 1000);
+		task2 = project1.getTaskRepository().createTask("Tarefa bbbb", 10, estimatedTaskStartDate, taskDeadline, 1000);
 		project1.getTaskRepository().addProjectTask(task1);
 		project1.getTaskRepository().addProjectTask(task2);
-		
 
 		mainMenu();
 	}
