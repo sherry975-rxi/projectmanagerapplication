510d73322d725d3f22fd2465d9921c212f36d973
Inês Pinto <1171476@isep.ipp.pt>
2018-01-04 14:39:31 +0000

    Create updateSetPassword method in UpdateUserInfoController class.

--- src/main/java/project/controller/UpdateUserInfoController.java
+++ src/main/java/project/controller/UpdateUserInfoController.java
@@ -42,6 +42,19 @@ public class UpdateUserInfoController {
 	}
 
 	/**
+	 * This method controls the update of user's password by calling the method
+	 * setPassword that set this parameter of user's data.
+	 * 
+	 * @param user
+	 * @param password
+	 */
+	public void updateUserPassword(User user, String password) {
+
+		user.setPassword(password);
+	}
+
+	
+	/**
 	 * This method controls the update of user's phone by calling the method
 	 * setPhone that set this parameter of user's data.
 	 * 
