1381b3145f76073a1f685382216d2da5657f7583
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-01-10 18:20:23 +0000

    class Planned in package TaskState: created isValid() and changeToAssigned()

--- src/main/java/project/model/taskStateInterface/Planned.java
+++ src/main/java/project/model/taskStateInterface/Planned.java
@@ -1,5 +1,137 @@
 package project.model.taskStateInterface;
 
+import project.model.Task;
+
 public class Planned implements TaskStateInterface {
+	
+	Task task;
+	
+	public Planned (Task t) {
+		this.task = t;
+	}
+
+	/**
+	 * This method verifies if the State "Planned" requirements are fulfilled for a specific Task. 
+	 * If so, it returns true,
+	 * If not, it returns false
+	 * 
+	 * @return true if is Valid, false if not
+	 */
+	public boolean isValid () {
+		boolean validation = false;
+		if(task.getEstimatedTaskStartDate()!=null && task.getTaskDeadline()!=null) {
+			validation = true;
+		}
+
+		return validation;
+	}
+
+	
+	public void changeToCreated() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void changeToPlanned() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void changeToAssigned() {
+		
+		if (isTransitionToAssignedPossible()) {
+
+			TaskStateInterface stateAssigned = new Assigned(task);
+			if (stateAssigned.isValid())
+				task.setTaskState(stateAssigned);
+		}
+		
+	}
+
+	
+	public void changeToReady() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeToOnGoing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeToStandBy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeToCancelled() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeToFinished() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTransitionToCreatedPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionToPlannedPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionToAssignedPossible() {
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionToReadyPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionToOnGoingPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionToStandByPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isTransitionToCancelled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionToFinishedPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean isTransitionToCancelledPossible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
 }
