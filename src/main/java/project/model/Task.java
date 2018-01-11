132e2cd9c6332e0d3b09edb46a376c183c2d293d
Daniel Almeida <1171459@isep.ipp.pt>
2018-01-11 15:47:27 +0000

    Revert "no message"
    This reverts commit 21968a542fd58a54ec080ad9e59b23429b0791d4.

--- src/main/java/project/model/Task.java
+++ src/main/java/project/model/Task.java
@@ -57,10 +57,10 @@ public class Task {
 		this.finishDate = null;
 		this.taskTeam = new ArrayList<>();
 		this.reports = new ArrayList<>();
-		this.estimatedTaskEffort = null;
+		this.estimatedTaskEffort = 0;
 		this.estimatedTaskStartDate = null;
 		this.taskDeadline = null;
-		this.taskBudget = null;
+		this.taskBudget = 0;
 		this.startDateInterval = null;
 		this.deadlineInterval = null;
 		this.taskDependency = null;
