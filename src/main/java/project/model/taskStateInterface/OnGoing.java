0f91cd0d1d3364dc37b10fcee65ce1cae01d39ed
ana rosa [1171453] <1171453@isep.ipp.pt>
2018-01-11 14:02:57 +0000

    Fixed errors and added empty constructor to OnGoing task state

--- src/main/java/project/model/taskStateInterface/OnGoing.java
+++ src/main/java/project/model/taskStateInterface/OnGoing.java
@@ -8,6 +8,10 @@ public class OnGoing implements TaskStateInterface {
 	
 	Task task;
 	
+	public OnGoing() {
+		
+	}
+	
 	public OnGoing(Task t){
 	  
 		this.task = t;
@@ -23,40 +27,62 @@ public class OnGoing implements TaskStateInterface {
 	public boolean isValid() {
 		boolean valid = false;
 			
-		if(task.getTaskState() == new StandBy()) {
-			return valid = true;
+		if(task.getStartDate() != null && task.doesTaskTeamHaveActiveUsers()) {
+			valid = true;
 		}
 				return valid;
 			}
 		
-		/*	if(taskOngoing.getTasstate == new Cancelled())
-			return true;
-		
-		if(taskOngoing.state == new Finished())
-			return true;
-		
-*/
-	
+	/**
+	 * This method changes the state of a Task to the "Created" state
+	 * 
+	 * @return Void
+	 */		
 	public void changeToCreated() {
-		
+	
 	}
-
+	
+	/**
+	 * This method changes the state of a Task to the "Planned" state
+	 * 
+	 * @return Void
+	 */
 	public void changeToPlanned() {
 		
 	}
 
+	/**
+	 * This method changes the state of a Task to the "Assigned" state
+	 * 
+	 * @return Void
+	 */
 	public void changeToAssigned() {
 		
 	}
 
+	/**
+	 * This method changes the state of a Task to the "Ready" state
+	 * 
+	 * @return Void
+	 */
 	public void changeToReady() {
 		
 	}
 	
+	/**
+	 * This method changes the state of a Task to the "OnGoing" state
+	 * 
+	 * @return Void
+	 */
 	public void changeToOnGoing() {
 		
 	}
-
+	
+	/**
+	 * This method changes the state of a Task to the "StandBy" state
+	 * 
+	 * @return Void
+	 */
 	public void changeToStandBy() {
 		
 		if (isTransitionToStandByPossible()) {
@@ -67,7 +93,12 @@ public class OnGoing implements TaskStateInterface {
 	        }
 	    
 	    }
-		
+	
+	/**
+	 * This method changes the state of a Task to the "Cancelled" state
+	 * 
+	 * @return Void
+	 */	
 	public void changeToCancelled() {
 		
 		if (isTransitionToCancelledPossible()) {
@@ -78,7 +109,12 @@ public class OnGoing implements TaskStateInterface {
         }
 		
 	}
-
+	
+	/**
+	 * This method changes the state of a Task to the "Finished" state
+	 * 
+	 * @return Void
+	 */
 	public void changeToFinished() {
 		
 		if (isTransitionToFinishedPossible()) {
@@ -95,7 +131,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “Created” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return false 
 	 */
 	public boolean isTransitionToCreatedPossible() {
 		return false;
@@ -106,7 +142,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “Planned” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return  false
 	 */
 	public boolean isTransitionToPlannedPossible() {
 		return false;
@@ -117,7 +153,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “Assigned” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return false 
 	 */
 	public boolean isTransitionToAssignedPossible() {
 		return false;
@@ -128,7 +164,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “Ready” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return false 
 	 */
 	public boolean isTransitionToReadyPossible() {
 		return false;
@@ -139,7 +175,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “OnGoing” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return false
 	 */
 	public boolean isTransitionToOnGoingPossible() {
 		return false;
@@ -150,7 +186,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “StandBy” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return true 
 	 */
 	public boolean isTransitionToStandByPossible() {
 		return true;
@@ -161,7 +197,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “Cancelled” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return true
 	 */
 	public boolean isTransitionToCancelledPossible() {
 		return true;
@@ -172,7 +208,7 @@ public class OnGoing implements TaskStateInterface {
 	 * This method verifies if the transition to the “Finished” state of a Task is
 	 * possible
 	 * 
-	 * @return true if possible, false if not
+	 * @return true
 	 */
 	public boolean isTransitionToFinishedPossible() {
 		return true;
