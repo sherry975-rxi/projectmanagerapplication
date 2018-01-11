bea58a1ceeacfd9ac4333ede518dff5e47c4bf79
Daniel Monteiro <1171461@isep.ipp.pt>
2018-01-11 15:41:30 +0000

    add tests to Created state  associated with taskStateInterface in Model

---
+++ src/test/java/project/model/states/CreatedTaskTests.java
@@ -0,0 +1,123 @@
+package project.model.states;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import project.model.Task;
+import project.model.taskStateInterface.Created;
+
+public class CreatedTaskTests {
+
+	Task testTask;
+	Task testTask1;
+	Task taskReadyToPlan;
+	Created test;
+	Created test1;
+	Created test2;
+	Calendar estimatedTaskStartDate, taskDeadline;
+
+	@Before
+	public void setUp() {
+		// create tasks
+		testTask = new Task(1, 1, "Task 1");
+		testTask1 = new Task(1, 1, null);
+		taskReadyToPlan = new Task(1, 1, "Task 1");
+
+		// create Created with task
+		test = new Created(testTask);
+		test1 = new Created(testTask1);
+		test2 = new Created(taskReadyToPlan);
+
+		// create a estimated task date
+		estimatedTaskStartDate = Calendar.getInstance();
+		estimatedTaskStartDate.add(Calendar.MONTH, -1);
+
+		// create a dead line date
+		taskDeadline = Calendar.getInstance();
+		taskDeadline.add(Calendar.MONTH, 1);
+
+		// set estimated task date and dead line date to task
+		taskReadyToPlan.setEstimatedTaskStartDate(estimatedTaskStartDate);
+		taskReadyToPlan.setTaskDeadline(taskDeadline);
+
+	}
+
+	@After
+	public void tearDown() {
+		testTask = null;
+		testTask1 = null;
+		taskReadyToPlan = null;
+		test = null;
+		test1 = null;
+		test2 = null;
+		estimatedTaskStartDate = null;
+		taskDeadline = null;
+
+	}
+
+	/**
+	 * this test verify if the State "Created" requirements are fulfilled for a
+	 * specific Task. The requirements are If the taskID and the description of the
+	 * task is not null, the task is created.
+	 */
+	@Test
+	public final void testIsValid() {
+
+		assertTrue(test.isValid());
+	}
+
+	/**
+	 * this test verify if the State "Created" requirements are not valid for a
+	 * specific Task. The requirements are If the taskID and the description of the
+	 * task is not null, the task is created.
+	 */
+	@Test
+	public final void testIsNotValid() {
+
+		assertFalse(test1.isValid());
+	}
+
+	/**
+	 * this test verify if the method changeToPlanned changes the state of a Task to
+	 * the "Planned" state using the method viewTaskStateName that returns the name
+	 * of the task's current state.
+	 */
+	@Test
+	public final void testChangeToPlanned() {
+
+		test2.changeToPlanned();
+
+		assertEquals("Planned", taskReadyToPlan.viewTaskStateName());
+	}
+
+	/**
+	 * this test verify if the method changeToPlanned cannot changes the state of a
+	 * Task to the "Planned" state using the method viewTaskStateName that returns
+	 * the name of the task's current state.
+	 */
+	@Test
+	public final void testCannotChangeToPlanned() {
+
+		test1.changeToPlanned();
+
+		assertEquals("Created", taskReadyToPlan.viewTaskStateName());
+	}
+
+	/**
+	 * this test verify if the transition to the “Planned” state of a Task is
+	 * possible
+	 */
+	@Test
+	public final void testIsTransitionToPlannedPossible() {
+
+		assertTrue(test2.isTransitionToPlannedPossible());
+	}
+
+}
