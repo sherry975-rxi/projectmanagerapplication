53ef73ba470bf619657f9c0b30c2f7f6ab0bd35c
João Afonso <Joo Afonso@LBessa-TOSH>
2018-01-12 12:29:30 +0000

    Corrected Assigned validation and greened all tests

--- src/test/java/project/model/states/AssignedTaskTests.java
+++ src/test/java/project/model/states/AssignedTaskTests.java
@@ -13,14 +13,15 @@ import project.model.ProjectCollaborator;
 import project.model.Task;
 import project.model.User;
 import project.model.taskStateInterface.Assigned;
+import project.model.taskStateInterface.Finished;
 import project.model.taskStateInterface.TaskStateInterface;
 
 public class AssignedTaskTests {
 
 	User tester;
 	ProjectCollaborator testerCollab;
-	Calendar randomDate;
-	Task assignedTask;
+	Calendar randomStartDate, randomDeadline, earlierStartDate;
+	Task assignedTask, dependency;
 	TaskStateInterface assignedStateTester;
 	
 	@Before
@@ -32,9 +33,20 @@ public class AssignedTaskTests {
 		// makes a project collaborator from the tester
 		testerCollab = new ProjectCollaborator(tester, 10);
 		
-		// creates a random date and uses it to create one task in the project
-		randomDate = Calendar.getInstance();
-		assignedTask = new Task(01, 01,"Assigned Test", 20, randomDate, randomDate, 200000);
+		// creates a random start date and deadline
+		// uses it to create one task in the project
+		randomStartDate = Calendar.getInstance();
+		randomDeadline = Calendar.getInstance();
+		randomDeadline.add(Calendar.DAY_OF_YEAR, 5);
+		assignedTask = new Task(01, 01,"Assigned Test");
+		assignedTask.setEstimatedTaskStartDate(randomStartDate);
+		assignedTask.setTaskDeadline(randomDeadline);
+
+		// creates a dependency with an earlier start date
+		earlierStartDate = Calendar.getInstance();
+		earlierStartDate.add(Calendar.DAY_OF_YEAR, -5);
+		dependency = new Task(01,02,"dependency");
+		dependency.setEstimatedTaskStartDate(earlierStartDate);
 	}
 	
 	@After
@@ -42,7 +54,9 @@ public class AssignedTaskTests {
 		tester= null;
 		testerCollab=null;
 		assignedTask=null;
+		dependency=null;
 		assignedStateTester=null;
+		
 	}
 	
 	// Given a new Task without dependencies:
@@ -54,8 +68,10 @@ public class AssignedTaskTests {
 		assertFalse(assignedStateTester.isValid());
 		
 		// adds a team member to the task, giving it 1 active member
+		// adds a dependency to the task, giving it one unresolved dependency
 		// then asserts its "Assigned" State is valid
 		assignedTask.addProjectCollaboratorToTask(testerCollab);
+		assignedTask.createTaskDependence(dependency);
 		assignedStateTester=new Assigned(assignedTask);
 		assertTrue(assignedStateTester.isValid());
 		
@@ -88,17 +104,16 @@ public class AssignedTaskTests {
 		assertTrue(assignedStateTester.isTransitionToReadyPossible());
 	}
 	
-	// TODO implement Ready State validation and remove @Ignore from this test
 	
 	// Given a new Task with active team (valid Assigned State) and a dependency...
 	@Test
-	@Ignore
 	public void testTransitionAssignedToReady() {
 		assignedTask.addProjectCollaboratorToTask(testerCollab);
+		assignedTask.createTaskDependence(dependency);
 		assignedStateTester=new Assigned(assignedTask);
-		
-		Task dependency = new Task(01,02,"dependent", 10, randomDate, randomDate, 100);
-		assignedTask.createTaskDependence(dependency, 20);
+		assignedTask.setTaskState(assignedStateTester);
+
+		// asserts a valid assigned state
 		assertTrue(assignedTask.viewTaskStateName().equals("Assigned"));
 		
 		// while the task has a dependency, attempts to change its state to Ready 
@@ -108,19 +123,23 @@ public class AssignedTaskTests {
 		
 		// marks the dependent task as Finished before attempting change state  
 		// then asserts it's state is now Ready
-		dependency.markTaskAsFinished();
-		assignedStateTester.changeToReady();	
+		TaskStateInterface dependencyFinishedState = new Finished(dependency);
+		dependency.setTaskState(dependencyFinishedState);
+		assignedStateTester.changeToReady();
 		assertTrue(assignedTask.viewTaskStateName().equals("Ready"));
 	}
 	
-	// TODO implement Planned State validation: Planned Task cannot have active team
 	// Given a new Task with active team (valid Assigned State)...	
 	@Test
-	@Ignore
 	public void testTransitionAssignedToPlanned() {
 		assignedTask.addProjectCollaboratorToTask(testerCollab);
+		assignedTask.createTaskDependence(dependency);
 		assignedStateTester=new Assigned(assignedTask);
+		assignedTask.setTaskState(assignedStateTester);
+		
+		// asserts a valid assigned state
 		assertTrue(assignedTask.viewTaskStateName().equals("Assigned"));
+				
 		
 		//while the task still has an active team, attempts to return it to planned
 		// then, asserts the state is still Assigned (transition invalid)
