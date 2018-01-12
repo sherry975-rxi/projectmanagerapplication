1d7ccc23a5273ccac1a130837de77d81aa27b873
Daniel Monteiro <1171461@isep.ipp.pt>
2018-01-12 16:49:18 +0000

    fix to project state machine tests

--- src/test/java/project/model/projectStates/DeliveryProjectTests.java
+++ src/test/java/project/model/projectStates/DeliveryProjectTests.java
@@ -1,14 +1,10 @@
 package project.model.projectStates;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class DeliveryProjectTests {
 
-	@Test
-	public final void test() {
-		fail("Not yet implemented"); // TODO
-	}
+	// @Test
+	// public final void test() {
+	// fail("Not yet implemented"); // TODO
+	// }
 
 }
--- src/test/java/project/model/projectStates/ReviewProjectTests.java
+++ src/test/java/project/model/projectStates/ReviewProjectTests.java
@@ -1,14 +1,10 @@
 package project.model.projectStates;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class ReviewProjectTests {
 
-	@Test
-	public final void test() {
-		fail("Not yet implemented"); // TODO
-	}
+	// @Test
+	// public final void test() {
+	// fail("Not yet implemented"); // TODO
+	// }
 
 }
