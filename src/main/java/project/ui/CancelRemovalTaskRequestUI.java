00b7b8d528a6be5db7d54869db3a88ca41e8343e
Daniel Monteiro <1171461@isep.ipp.pt>
2018-01-22 18:01:58 +0000

    create CancelRemovalTaskRequestUI Daniel Monteiro/Rita Machado

--- src/main/java/project/ui/CancelRemovalTaskRequestUI.java
+++ src/main/java/project/ui/CancelRemovalTaskRequestUI.java
@@ -1,5 +1,80 @@
 package project.ui;
 
-public class CancelRemovalTaskRequestUI {
+import java.util.Scanner;
 
+import project.controller.PrintProjectInfoController;
+import project.controller.PrintTaskInfoController;
+import project.controller.US206V2RemovalTaskRequestController;
+import project.model.ProjectCollaborator;
+import project.model.User;
+
+public class CancelRemovalTaskRequestUI {
+	User user;
+	String taskID;
+	Integer projID;
+	ProjectCollaborator projcollab;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param user
+	 *            User to add task to user task list
+	 * 
+	 * @param taskID
+	 *            Task to add to user task list
+	 */
+	public CancelRemovalTaskRequestUI(User user, String taskID) {
+
+		this.user = user;
+		this.taskID = taskID;
+		this.projID = 0;
+	}
+
+	public void cancelRemovalTaskRequestUI() {
+
+		US206V2RemovalTaskRequestController controller = new US206V2RemovalTaskRequestController(this.user);
+		controller.setProjectIDFromTaskID(taskID);
+		projID = controller.getProjectID();
+
+		PrintProjectInfoController printProjectInfoController = new PrintProjectInfoController(this.projID);
+		String projectName = printProjectInfoController.printProjectNameInfo();
+		PrintTaskInfoController printTaskInfoController = new PrintTaskInfoController(this.taskID, this.projID);
+		String taskName = printTaskInfoController.printTaskNameInfo();
+
+		System.out.println("PROJECT - " + projectName);
+		System.out.println("__________________________________________________");
+		System.out.println("Task - " + taskName + "\n");
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Are you sure you want to remove yourself from this task ? \n");
+		System.out.println("[Y] to remove");
+		System.out.println("[N] to cancel\n");
+
+		String yerOrNo = input.nextLine();
+
+		// In case user writes something different from "y" or "n"
+		while (!("n".equalsIgnoreCase(yerOrNo)) && !("y".equalsIgnoreCase(yerOrNo))) {
+			System.out.println("\nInvalid answer. Try again (\"y\" or \"n\")");
+			yerOrNo = input.nextLine();
+		}
+
+		if (yerOrNo.equalsIgnoreCase("y")) {
+			if (controller.createRequest() == true) {
+				System.out.println("Your task removal is pending Project Manager approval");
+				// TaskDetailsUI taskDetailsUI = new
+				// TaskDetailsUI(assignTaskToCollaboratorsController.getTaskByTaskID(this.taskID));
+				// taskDetailsUI.taskDataDisplay();
+			} else {
+				System.out.println("[ERROR!: Please choose a valid Task]");
+				// TaskDetailsUI taskDetailsUI = new
+				// TaskDetailsUI(assignTaskToCollaboratorsController.getTaskByTaskID(this.taskID));
+				// taskDetailsUI.taskDataDisplay();
+			}
+		} else {
+			// TaskDetailsUI taskDetailsUI = new
+			// TaskDetailsUI(assignTaskToCollaboratorsController.getTaskByTaskID(this.taskID));
+			// taskDetailsUI.taskDataDisplay();
+		}
+	}
 }
