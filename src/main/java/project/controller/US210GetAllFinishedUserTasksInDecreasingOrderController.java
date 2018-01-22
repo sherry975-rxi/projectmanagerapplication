cde6a539f7f59734cbb47b5d0f344e1291c587c6
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-01-22 12:02:41 +0000

    Controller: correction in US210GetAllFinishedUserTasksInDecreasingOrderController

--- src/main/java/project/controller/US210GetAllFinishedUserTasksInDecreasingOrderController.java
+++ src/main/java/project/controller/US210GetAllFinishedUserTasksInDecreasingOrderController.java
@@ -1,5 +1,9 @@
 package project.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import project.model.Company;
@@ -12,11 +16,36 @@ public class US210GetAllFinishedUserTasksInDecreasingOrderController {
 	User myUser;
 	ProjectRepository myProjRepo;
 
+	/**
+	 * Creator of the controller, receives a user
+	 * @param user
+	 */
 	public US210GetAllFinishedUserTasksInDecreasingOrderController(User user) {
 		this.myProjRepo = Company.getTheInstance().getProjectsRepository();
 		this.myUser = user;
 	}
 
+	
+	
+	
+	/**
+	 * Method to return the user name in string
+	 * @return string
+	 */
+	public String printUserNameInfo() {
+		return this.myUser.getName();
+	}
+	
+	public String convertCalendarToString(Calendar dateToConvert) {
+	
+			
+			Date conversion = dateToConvert.getTime();
+			SimpleDateFormat newDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			String dateConverted = newDateFormat.format(conversion).toString();
+	
+			return dateConverted;
+	}
+	
 	/**
 	 * Returns the list of user finished tasks in decreasing order. - US210
 	 * 
@@ -25,7 +54,15 @@ public class US210GetAllFinishedUserTasksInDecreasingOrderController {
 	 * @return Task List
 	 * 
 	 */
-	public List<Task> getAllFinishedUserTasksInDecreasingOrder() {
-		return this.myProjRepo.getAllFinishedUserTasksInDecreasingOrder(this.myUser);
+	public List<String> getAllFinishedUserTasksInDecreasingOrder(){
+		List<Task> taskList = this.myProjRepo.getAllFinishedUserTasksInDecreasingOrder(this.myUser);
+		List<String> finishedTaskListDecreasingOrder = new ArrayList<>();
+		for (Task finishedTask : taskList) {
+			String[] stringList = finishedTask.getTaskID().split("\\.");
+			finishedTaskListDecreasingOrder.add("[" + stringList[0] + "." + stringList[1] + "]" + " " + convertCalendarToString(finishedTask.getFinishDate()) + " - " +  finishedTask.getDescription());
+		}
+	return finishedTaskListDecreasingOrder;
 	}
+	
+	
 }
