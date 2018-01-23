e44ebd222d4fa2e7e87bd9445d284bebf64b2809
Johnny <Johnny@172.18.153.236>
2018-01-23 10:31:32 +0000

    Updated US115/116 controllers, UI and tests

--- src/main/java/project/ui/uiAdministrator/US115andUS116SetUserStateUI.java
+++ src/main/java/project/ui/uiAdministrator/US115andUS116SetUserStateUI.java
@@ -9,18 +9,22 @@ public class US115andUS116SetUserStateUI {
 
 	public void changeUserState(User user) {
 
+		US115andUS116SetUserStateController controller = new US115andUS116SetUserStateController(user);
+		System.out.println(controller.userDataAsString());
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Proceeding will change this user's state. Are you sure you want to continue?");
 		System.out.println("Press [Y] to confirm.");
 		String choice = input.nextLine();
 
 		if ("Y".equalsIgnoreCase(choice)) {
-			US115andUS116SetUserStateController controller = new US115andUS116SetUserStateController();
-			controller.changeUserState(user);
+			controller.changeUserState();
 			System.out.println("User state was changed successfully.");
 		} else {
 			System.out.println("User state was not changed.");
 		}
+		System.out.println("Returning to admin menu...");
+		System.out.println("");
 	}
 
 }
