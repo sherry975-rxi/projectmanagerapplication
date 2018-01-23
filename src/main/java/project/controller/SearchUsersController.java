36cc92f4cb38185a8385a1dd1252b57338ba2e6e
Johnny <Johnny@172.18.153.236>
2018-01-23 11:06:21 +0000

    Completed Search Users Controller and Tests

--- src/main/java/project/controller/SearchUsersController.java
+++ src/main/java/project/controller/SearchUsersController.java
@@ -1,5 +1,6 @@
 package project.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import project.model.Company;
@@ -7,6 +8,9 @@ import project.model.Profile;
 import project.model.User;
 
 public class SearchUsersController {
+
+	List<User> userList;
+	User selectedUser = null;
 
 	/**
 	 * Este controlador permite ao Administrador pesquisar utilizadores do sistema
@@ -17,24 +21,57 @@ public class SearchUsersController {
 	/**
 	 * @param profileToSearch
 	 *            The type of of profile to search for
-	 * @return This method returns a list of users with the profile that match the
-	 *         user profile
+	 * @return This method returns a list of user's Data Strings with the profile
+	 *         that match the user profile
 	 */
-	public List<User> searchUsersByProfileController(Profile profileToSearch) {
+	public List<String> searchUsersByProfileController(Profile profileToSearch) {
+		this.userList = Company.getTheInstance().getUsersRepository().searchUsersByProfile(profileToSearch);
 
-		return Company.getTheInstance().getUsersRepository().searchUsersByProfile(profileToSearch);
+		List<String> userListAsString = new ArrayList<>();
+
+		for (int i = 0; i < userList.size(); i++) {
+			Integer showIndex = i + 1;
+			String toShowUser = showIndex.toString() + ":: " + userDataToString(userList.get(i));
+			userListAsString.add(toShowUser);
+		}
+
+		return userListAsString;
 
 	}
 
 	/**
 	 * @param emailToSearch
 	 *            The string of email to search for
-	 * @return This method returns a list of users with an email that contain the
-	 *         same text that the user typed
+	 * @return This method returns a list of user's Data Strings with an email that
+	 *         contain the same text that the user typed
 	 */
-	public List<User> searchUsersByEmailController(String emailToSearch) {
+	public List<String> searchUsersByEmailController(String emailToSearch) {
+		this.userList = Company.getTheInstance().getUsersRepository().searchUsersByEmail(emailToSearch);
 
-		return Company.getTheInstance().getUsersRepository().searchUsersByEmail(emailToSearch);
+		List<String> userListAsString = new ArrayList<>();
+
+		for (int i = 0; i < userList.size(); i++) {
+			Integer showIndex = i + 1;
+			String toShowUser = showIndex.toString() + ":: " + userDataToString(userList.get(i));
+			userListAsString.add(toShowUser);
+		}
+
+		return userListAsString;
+
+	}
+
+	/**
+	 * This method selects a User and returns it to the UI, to be handled by the
+	 * Admin
+	 * 
+	 * @return User to be Returned and handled by the Admin
+	 */
+	public User selectUser(int index) {
+		int actualIndex = index - 1;
+		if (actualIndex >= 0 && actualIndex < userList.size()) {
+			selectedUser = userList.get(actualIndex);
+		}
+		return selectedUser;
 
 	}
 
--- src/test/java/project/controller/SearchUsersControllerTest.java
+++ src/test/java/project/controller/SearchUsersControllerTest.java
@@ -1,6 +1,7 @@
 package project.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,9 +40,6 @@ public class SearchUsersControllerTest {
 		// Creates an UserRepository
 		userRepository = company.getUsersRepository();
 
-		// Creates a searchController
-		searchController = new SearchUsersController();
-
 		// create user
 		newUser1 = new User("Daniel", "daniel@gmail.com", "001", "collaborator", "910000000");
 		// create user2
@@ -49,6 +47,14 @@ public class SearchUsersControllerTest {
 
 		// create user3
 		newUser3 = new User("Miguel", "miguel@gmail.com", "001", "Admin", "920000000");
+
+		/* Adds the created users to the user Repository */
+		userRepository.addUserToUserRepository(newUser1);
+		userRepository.addUserToUserRepository(newUser2);
+		userRepository.addUserToUserRepository(newUser3);
+
+		// Creates a searchController
+		searchController = new SearchUsersController();
 
 	}
 
@@ -66,55 +72,80 @@ public class SearchUsersControllerTest {
 	public void testSearchUsersControllerByProfile() {
 
 		/* Compares a search of a profile type that doesn't exist with a empty List */
-		List<User> emptyList = new ArrayList<User>();
+		List<String> emptyList = new ArrayList<>();
 		assertEquals(searchController.searchUsersByProfileController(Profile.COLLABORATOR), emptyList);
 
 	}
 
+	/**
+	 * This tests the basic search by profile method and asserts the UserData to
+	 * String conversion is working as intended
+	 * 
+	 * 
+	 */
 	@Test
 	public void testSearchUsersControllerByProfile2() {
-
-		/* Adds the created users to the user Repository */
-		userRepository.addUserToUserRepository(newUser1);
-		userRepository.addUserToUserRepository(newUser2);
-		userRepository.addUserToUserRepository(newUser3);
 
 		/* Set the newUser1 and newUser2 profile type to collaborator */
 		newUser1.setUserProfile(Profile.COLLABORATOR);
 
+		// Creates a String data for newUser1 and compares it to the the ToString Method
+		// from the controller
+		String newUser1String = "001 - Collaborator: Daniel (daniel@gmail.com; 910000000) - collaborator";
+		assertTrue(searchController.userDataToString(newUser1).equals(newUser1String));
+
 		/*
 		 * Compares a search of collaborator with a list where two collaborators exist
 		 */
-		List<User> collaboratorstest = new ArrayList<User>();
-		collaboratorstest.add(newUser1);
+		List<String> collaboratorstest = new ArrayList<>();
+		collaboratorstest.add("1:: " + newUser1String);
 
 		assertEquals(searchController.searchUsersByProfileController(Profile.COLLABORATOR), collaboratorstest);
 	}
 
+	/**
+	 * This test verifies that searching a nonexistent email will return no users
+	 * 
+	 */
 	@Test
 	public void testSearchUsersControllerByEmailThatDoesntExist() {
 
-		/* Adds the created users to the user Repository */
-		userRepository.addUserToUserRepository(newUser1);
-		userRepository.addUserToUserRepository(newUser2);
-		userRepository.addUserToUserRepository(newUser3);
 		/* Compares a search of a mail that doesn't exist with a empty List */
-		List<User> emptyList = new ArrayList<User>();
+		List<String> emptyList = new ArrayList<>();
 		assertEquals(searchController.searchUsersByEmailController("yahoo"), emptyList);
 
 	}
 
+	/**
+	 * This test asserts that searching by an entire, existing email returns a
+	 * single user
+	 * 
+	 */
 	@Test
 	public void searchByEntireEmail() {
 
-		/* Adds the created users to the user Repository */
-		userRepository.addUserToUserRepository(newUser1);
-		userRepository.addUserToUserRepository(newUser2);
-		userRepository.addUserToUserRepository(newUser3);
 		/* Compares a search by entire mail address with a list with that mail */
-		List<User> testUsersEmail1 = new ArrayList<User>();
-		testUsersEmail1.add(newUser2);
+		List<String> testUsersEmail1 = new ArrayList<>();
+		testUsersEmail1.add("1:: " + searchController.userDataToString(newUser2));
 		assertEquals(searchController.searchUsersByEmailController("joao@gmail.com"), testUsersEmail1);
+
+		assertEquals(searchController.searchUsersByEmailController("joao@gmail.com").size(), 1);
+	}
+
+	/**
+	 * Asserts the user selection method is working correctly, attempting to select
+	 * the first indexed User from the list
+	 * 
+	 */
+	@Test
+	public void testListUsersController_SelectUser() {
+		// calls the Search Users controller to generate a List
+		searchController.searchUsersByEmailController("joao@gmail.com");
+
+		// selects user from visible Index number 1 (corresponding to the User List's
+		// user 0)
+		// it must be equal to newUser2
+		assertEquals(searchController.selectUser(1), newUser2);
 	}
 
 }
