4ffbce126936c642079e6eb13263dc32efe6063d
Rita Machado <1171471@isep.ipp.pt>
2018-01-24 01:16:39 +0000

    changes to confirmTaskAssignmentToCollaborator method in US204v2ConfirmTaskAssignmentToCollaboratorUI to call US204v2ConfirmTaskAssignmentToCollaboratorController Rita Machado / Daniel Monteiro

--- src/main/java/project/ui/uiCollaborator/US204v2ConfirmTaskAssignmentToCollaboratorUI.java
+++ src/main/java/project/ui/uiCollaborator/US204v2ConfirmTaskAssignmentToCollaboratorUI.java
@@ -5,9 +5,9 @@ package project.ui.uiCollaborator;
 
 import java.util.Scanner;
 
-import project.controller.AssignTaskToCollaboratorsController;
 import project.controller.PrintProjectInfoController;
 import project.controller.PrintTaskInfoController;
+import project.controller.US204v2createRequestAddCollaboratorToTaskTeamController;
 import project.controller.US206CancelRemovalTaskRequestController;
 import project.model.ProjectCollaborator;
 import project.model.User;
@@ -43,34 +43,38 @@ public class US204v2ConfirmTaskAssignmentToCollaboratorUI {
 
 		this.user = user;
 		this.taskID = taskID;
+		this.projID = null;
 	}
 
 	public void confirmTaskAssignmentToCollaborator() {
 
-		US206CancelRemovalTaskRequestController controller = new US206CancelRemovalTaskRequestController(this.user);
-		controller.setProjectIDFromTaskID(taskID);
-		projID = controller.getProjectID();
+		US206CancelRemovalTaskRequestController control = new US206CancelRemovalTaskRequestController(this.user);
+		control.setProjectIDFromTaskID(taskID);
+		projID = control.getProjectID();
 
 		PrintProjectInfoController printProjectInfoController = new PrintProjectInfoController(this.projID);
 		String projectName = printProjectInfoController.printProjectNameInfo();
 		PrintTaskInfoController printTaskInfoController = new PrintTaskInfoController(this.taskID, this.projID);
 		String taskName = printTaskInfoController.printTaskNameInfo();
 
+		printLion();
+
 		System.out.println("PROJECT - " + projectName);
-		System.out.println("__________________________________________________");
-		System.out.println("Task - " + taskName + "\n");
+		System.out.println("");
+		System.out.println("                     TASK                    ");
+		System.out.println("*** " + taskName + " ***");
+		System.out.println("______________________________________________");
 
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("Are you sure you want to assign yourself to this task ? \n");
-		System.out.println("[Y] to add \n");
+		System.out.println("[Y] to add");
 		System.out.println("[N] to cancel \n");
 
 		String yerOrNo = input.nextLine();
 
-		AssignTaskToCollaboratorsController assignTaskToCollaboratorsController = new AssignTaskToCollaboratorsController(
-				this.taskID);
-		projcollab = assignTaskToCollaboratorsController.getProjectCollaboratorFromUser(this.user);
+		US204v2createRequestAddCollaboratorToTaskTeamController controller = new US204v2createRequestAddCollaboratorToTaskTeamController(
+				taskID, user);
 
 		// In case user writes something different from "y" or "n"
 		while (!("n".equalsIgnoreCase(yerOrNo)) && !("y".equalsIgnoreCase(yerOrNo))) {
@@ -79,20 +83,167 @@ public class US204v2ConfirmTaskAssignmentToCollaboratorUI {
 		}
 
 		if (yerOrNo.equalsIgnoreCase("y")) {
-			if (assignTaskToCollaboratorsController.assignTaskToProjectCollaboratorController(taskID,
-					projcollab) == true) {
-				System.out.println("Your were successfully assigned to this task");
+			if (controller.createTaskTeamRequest() == true) {
+				System.out.println("Your request is pending approval.");
 				TaskDetailsUI taskDetailsUI = new TaskDetailsUI(taskID, projID, user);
 				taskDetailsUI.taskDataDisplay();
 			} else {
-				System.out.println("Your were not assigned to this task");
+				System.out.println("Your request was not done.");
 				TaskDetailsUI taskDetailsUI = new TaskDetailsUI(taskID, projID, user);
 				taskDetailsUI.taskDataDisplay();
 			}
 		} else {
+			System.out.println("Your request was not created.");
 			TaskDetailsUI taskDetailsUI = new TaskDetailsUI(taskID, projID, user);
 			taskDetailsUI.taskDataDisplay();
 		}
+	}
+
+	public void printLion() {
+
+		System.out.println(
+				"                                                                                                                                                      ");
+		System.out.println(
+				"                                                                                                                                                      ");
+		System.out.println(
+				"                                                                                                                                                      ");
+		System.out.println(
+				"                                                                                                                                                      ");
+		System.out.println(
+				"                                                            `           ```.---`                                                                      ");
+		System.out.println(
+				"                                                    ``.-:/+/--::- ``  `//:oo+:` ````                                                                  ");
+		System.out.println(
+				"                                                ``-:/+ssyyhdddhhhs:-.:yhhdhs+/++++/-``                                                                ");
+		System.out.println(
+				"                                             ``.-/osyhyyyyyyhdmmmmdhyydddhysosyyyyyo+/:--.``                                                          ");
+		System.out.println(
+				"                                          ``.-/+syyhyyyso++ooyyyhddhhhdyyoo+++oyhhhdddhhhys+:.`                                                       ");
+		System.out.println(
+				"                                        `-:+syhhdddhysoo+++/++soyyhhyyyoo++///++osyhddddddhs+:.`                                                      ");
+		System.out.println(
+				"                                       `-/oyddmddhhysooooooo+o++syyyyss++o+//+++ooossyhdddddhs+.`                                                     ");
+		System.out.println(
+				"                                     `.:+ssyyhdddhyysooooooooo/+++/+o+o+++oo++o++ooosyyyhhhhyyy+:.                                                    ");
+		System.out.println(
+				"                                     `.:/++osydhysoooooo+++++++/::-:/://+++++++++oooooosyyyysssso/`                                                   ");
+		System.out.println(
+				"                                     ``:++oosssoo+ossooo+//://::::::::/:/+:+/++ooooooo+++oyyyyysso.                                                   ");
+		System.out.println(
+				"                                       -o+++/+o+ooo++++s+::--:::/:/+://///::+++o+o+ssooosssyhyysso`                                                   ");
+		System.out.println(
+				"                                        /+://ossoo+::/s/:---:///+++yo+///:::/+++oo//ohmmmmdhhysso:`                                                   ");
+		System.out.println(
+				"                                       ..//:/+so///:/+/-----:-:++osy+++/::////+o+o/:/shNMNdso+os-`                                                    ");
+		System.out.println(
+				"                                      ``./h//+:-:+/+/:-::---..-:/oyo/::--:/:/oosooo::/ohdmds++dd/`                                                    ");
+		System.out.println(
+				"                                       .odh+-.-:++/::-:::.....---/o/::::--+::ossyso/::/ysoyyosmdy-`                                                   ");
+		System.out.println(
+				"                                     `-+o/-..:////+///so+:-`.....-:-.-:/....-/+osyso::/ss+:++smNdo.                                                   ");
+		System.out.println(
+				"                                     ---...-/+/:+//:/oh/+/h:`...----.-:/../ysohdsyy+//:+o+/-::ohdds.                                                  ");
+		System.out.println(
+				"                                    ```..--:////+/:::/-//+yo....--:.-://-/hdooo:/+ss+++/+++/:---+hdh:                                                 ");
+		System.out.println(
+				"                                    `...---:////::----/:.-:+-----::--:+/+hs/--:://ss+oo++++/:---:/ymd+`                                               ");
+		System.out.println(
+				"                                   `/:...--:///:--.-..---:/+/:::::////+oyhys/:///+yo++/////::-----+hNms.                                              ");
+		System.out.println(
+				"                                  `::-...-::///:..---...-//:-:::++oo+++/oyyho////ss+///:///:::---.:ymNds:`                                            ");
+		System.out.println(
+				"                                  .:-....--::/+:...-..-.:/:-://++ooo++ss+/hyho++/++:+//://:/::----:shdms:.                                            ");
+		System.out.println(
+				"                                 `.--.`..--:////-.....-./:-:::/++++++oooo+yyhys+://:+::/://::::---:smNNms-`                                           ");
+		System.out.println(
+				"                                 `::/-...--:::/+/-....`.::::::::::::/+ooooysyyso/::/::///::/:------+dmNNds-                                           ");
+		System.out.println(
+				"                                 .+o/....---:://+/...``----::oss/:/+sysoo+osshso///::::///::/:----:/hdmmmh/`                                          ");
+		System.out.println(
+				"                                ./++:.`..--::://++-..``-..`/sdNmddddNNmhs-//oys++o++/::/:::::/:--:/oohdddds.                                          ");
+		System.out.println(
+				"                               `-/++:.....-:-::/+++:-..-...-..-+dddmy:..::+/+oooossy+/::/::///::--+shhhddhy+`                                         ");
+		System.out.println(
+				"                               .:/+/:.-....---://oo+/-.-....``` `od:```..--::/+oohdho////:::::/---+yymddhhyo:`                                        ");
+		System.out.println(
+				"                              `-/++++--....:-::/++///+:.`````   `-h.`````...-:/oydhhy+::::-:--::-//hdddhyyso/`                                        ");
+		System.out.println(
+				"                              .:/++o+:-.`.:-.:::/:-:/::-...`` ``.:y:.```...--/ooyysss+/:::--:-::-+symmdhyss//:`                                       ");
+		System.out.println(
+				"                              -:++oo//-.`--.---:-.-:::+:::-.``-/oyysss+-.-:+osshsys//++/:----::::/dhmNdyysss-..                                       ");
+		System.out.println(
+				"                             `-//sy+++/..-..----.--:/oo+/so//:...--.../syhyhdhdmhso:-:///:----/:/odNmNNdyosys:-....``                                 ");
+		System.out.println(
+				"                             .:/oysoss+.-....:---::/+so++/.`````````````.//syhhdhs//:://:::---:/+yNNNNNNmysyhsssooo++//:.`                            ");
+		System.out.println(
+				"                             :-:yysyhy/--.-.-:--/::/+++oo/.`  ```````````.:oyhhhs+/:/-:/:-::::-/odNMMNNNNmhyyyysyhyyyyyso+/-                          ");
+		System.out.println(
+				"                            `-`/syhdhyo:....:--::/+//+oso++//----:..--..-+shhyys++::::::/:-::/-/odNMMNNNMNmdhyhyydddhdhhys++:                         ");
+		System.out.println(
+				"                             `-+ydmdmhy+...::::////+/oyysyyys+osos++syyyyhyhmhso+/::/:::::--://+sdNMMNNNNMNmdhhhhddmdddhys++/-                        ");
+		System.out.println(
+				"                             `:/yddNmhms---:.-:+::+/ooyyssyyssyyyyshsydddhohdyso++////:::::-:/+oydNMMMNNNMMNmmdhddmmmdhyyo+++:                        ");
+		System.out.println(
+				"                             ``/hmNmmNNy/::-.-//-/+ooosyyyyosyyhyhohsyhhdyyddsso+so/////:/:::+++ymNMMMMMNNMMNmmdddNNmhyss+++/-                        ");
+		System.out.println(
+				"                              `sdmmmNNmh++:.-:::-/+++ooyys+osssshyshhyyshyyhhyss+oo+:::::::/+os+smNMMMMMMNNMMNmmddmmdysoo+///:                        ");
+		System.out.println(
+				"                              -ydmmNMNmhos/-+/::/:///o+os+++yoysdyyyyhysyyyysshso++//:/+/://yyhyydNMMMMMMNNMNNNmmmmdyooo++//::.                       ");
+		System.out.println(
+				"                              /+dmNMMNmdyds/s++o/++////+o+//s+sydyyhoyhsoyyso+syo/+:+::+o//osddmdmNMMMMMMMNNNNNNmmdhyso++//:::/                       ");
+		System.out.println(
+				"                             `:sydNMMNmNmNdsyssyyhs+++++o+/+osyhhhddsydhoysy+/+so+o:++ooyosoymNNNNNMMMNMMMNNNNNNmmhhys+///:--//`                      ");
+		System.out.println(
+				"                              -/:yNNNmNNNNNhmdhmmdssooo+so++oshhyhdhhshhsysy+++yyosossysmhhhhNMMMNMMMMNMNNNNNNNNmdhhyo+///:--//`                      ");
+		System.out.println(
+				"                              .-.odmNNNNNNNNNmmNNmNmyyhyoy+osydhhdhhhsyyssyyooohdohddhmdNNmdNNMMMNMMMMMNMNNNNNmNdhhhhyo//:--/+:                       ");
+		System.out.println(
+				"                              .`.odmmmNNNNNNNNNMNNMNdmymhhhoshdmddhdmhyyysyoosyhdymNNNNMMMMNMMMMMNMNMMMNNNNNNNmNdhhdys+/----o+`                       ");
+		System.out.println(
+				"                              ` `syyymNNNNNNNNNNNMMMmNmmNmmhyhmNmdmdNdhdhssohdmmmmMMMMMMMMMMMMMMMMMNMMMMMNNNNNmmdddhys+:--:o+-                        ");
+		System.out.println(
+				"                                .o/-+dNNNNNNNNNNNNNNNNMMMMNNmdmMNNdNNmdmdyhhmNNNMMMMMMMMMMMMMMMMMMMNMMMMMNNNNNmddddhyso/:os/.                         ");
+		System.out.println(
+				"                                -:``+dNNNNNNNNNNNNNNNNMMNMNMNNNMNNNNNNmNddmmNMMMMMMMMMMMMMMMMMMMMMMMNMMMMMNNNmmmdddhysosys-                           ");
+		System.out.println(
+				"                                -. -smmhdNNNNNNNNNNNNNNNNMNMMMNNMMMMMNMNNNMNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNmmmdhysssss:`                            ");
+		System.out.println(
+				"                                `` +ddyhdNNNNNNNNNNmNNNNNNNNNNMNMMMMMMMMNMMNNMMMMMMMMMMMMMMMMMMMMMMMNNMMMMNNNmmdhssys:`                               ");
+		System.out.println(
+				"                                  .yhsysymNNNNNmNNmmmmmmNmmmmmmmNMMMMMMMNMMNNMMMMMMMMMMMMMMMNNmNNMMMMNMMMNNNNmmhso+-                                  ");
+		System.out.println(
+				"                                 `+y/-..ymmmmNNmddhyysyyyhhyss+++oydNMMMMMMMMMMMMMMMMMMMMMMmhyhmmNNMMMMMMNNNNmdy+o:                                   ");
+		System.out.println(
+				"                                 .:-`  :hyy+ohdyss+/::///++++++++///+oshmNMMMMMMMMMMMMMMMms+osyhddmNNMMMMNNNNdho//.                                   ");
+		System.out.println(
+				"                                 ``   .o++s+oss++/:--..-----:://++++++////ohNMMMMMMMMMMMd::/+ossyhdmNNMNNNNNmhs+:-`                                   ");
+		System.out.println(
+				"                                    `-oyhdmmd+/:::--.......-----::://///:::+/yNMMMMMMMNy/:/oo++oosydmmmmNmhdh+:-`                                     ");
+		System.out.println(
+				"                                 `.:oyhhdmmhyo:-:--.........----:----:::::/+//+dNMMMNh/--::+++///+oyddhhhs//+.`                                       ");
+		System.out.println(
+				"                                `-/oydhyddhsss/---............----.--::-://///:/oyhy/----.-://+++++odNdso:..``                                        ");
+		System.out.println(
+				"                                `.-/++ssyyhyyyo:---..-.-.........`..-:-::///+/::/:-:-::-:/+:-:++:/++yNdh+-`                                           ");
+		System.out.println(
+				"                                     ``..-::+oo/:-.........-....```.---:/:---::/::::-:-+dh+:/++//++/:so:.`                                            ");
+		System.out.println(
+				"                                              ``````............```.....++`..-://:://+/-oo/:-+sds//:-::-`                                             ");
+		System.out.println(
+				"                                                        ``````````````..-/````.-::./s+s/:ss/ohyo://--://`                                             ");
+		System.out.println(
+				"                                                                ```````-/-.```-s/:.-hm+y:hmd/-.---/o:::`                                              ");
+		System.out.println(
+				"                                                                    ````/.`` ``:+.-`/y+//+hNs:---::---`                                               ");
+		System.out.println(
+				"                                                                       ````` ```.....:///:/+:--``````                                                 ");
+		System.out.println(
+				"                                                                                                                                                      ");
+		System.out.println(
+				"                                                                                                                                                      ");
+		System.out.println(
+				"                                                                                                                                                      ");
+
 	}
 
 }
