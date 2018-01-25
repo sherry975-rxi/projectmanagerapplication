14acb23c33c8859ce9d106e34134ff15d93b5441
Daniel Almeida <1171459@isep.ipp.pt>
2018-01-25 19:42:07 +0000

    Created the US361AssignUserToTaskUI and all the necessary methods, which are: displayUsersToAssign, chooseAction and ChooseUserToAssign.

--- src/main/java/project/ui/console/uiProjectManager/tasks/US361AssignUserToTaskUI.java
+++ src/main/java/project/ui/console/uiProjectManager/tasks/US361AssignUserToTaskUI.java
@@ -1,9 +1,102 @@
 package project.ui.console.uiProjectManager.tasks;
 
+import java.util.List;
+import java.util.Scanner;
+
+import project.controller.US361AssignTaskToCollaboratorsController;
+import project.model.Project;
+import project.model.Task;
+import project.model.User;
+import project.ui.console.MainMenuUI;
+import project.ui.console.uiProjectManager.ProjectManagerMainMenuUI;
+
 public class US361AssignUserToTaskUI {
 
-	// Integer projectID
-	// Display Company users that are not in task
-	// 1.Choose user to add to project
+	User user;
+	Project project;
+	Task task;
 
+	public US361AssignUserToTaskUI(Project project, Task task, User user) {
+		this.user = user;
+		this.project = project;
+		this.task = task;
+	}
+
+	public void displayUsersToAssign() {
+
+		System.out.println("            PROJECT COLLABORATORS           ");
+		System.out.println("____________________________________________");
+
+		US361AssignTaskToCollaboratorsController assignController = new US361AssignTaskToCollaboratorsController(
+				project, task);
+		List<String> userList = assignController.getProjectActiveTeam();
+		int number = 1;
+
+		for (String other : userList) {
+			System.out.println("[" + number + "]" + other + "\n");
+			number++;
+		}
+
+		chooseAction(assignController);
+	}
+
+	public void chooseAction(US361AssignTaskToCollaboratorsController assignController) {
+
+		Scanner input = new Scanner(System.in);
+		boolean condition = true;
+		while (condition) {
+
+			System.out.println("-------------CHOOSE A USER TO ADD-------------");
+			System.out.println("[B] Back");
+			System.out.println("[M] MainMenu");
+			System.out.println("[E] Exit");
+			System.out.println();
+
+			String choice = input.nextLine().toUpperCase();
+
+			switch (choice) {
+
+			case "B":
+				break;
+			case "M":
+				MainMenuUI.mainMenu();
+				break;
+			case "E":
+				System.out.println("\n");
+				System.out.println("--YOU HAVE EXIT FROM THE APPLICATION--");
+				condition = false;
+				break;
+			default:
+				chooseUserToAssign(assignController, choice);
+				break;
+			}
+		}
+	}
+
+	public void chooseUserToAssign(US361AssignTaskToCollaboratorsController assignController, String choice) {
+
+		Integer choiceInt;
+
+		try {
+			choiceInt = Integer.parseInt(choice);
+			int listSize = assignController.getProjectActiveTeam().size();
+
+			if (choiceInt != null && choiceInt > 0 && choiceInt <= listSize) {
+				assignController.setUserToAddToTask(choiceInt - 1);
+				if (assignController.assignCollaboratorToTask()) {
+					System.out.println("--------USER ASSIGNED--------");
+					ProjectManagerMainMenuUI projectmanagerMenu = new ProjectManagerMainMenuUI(user, project);
+					projectmanagerMenu.displayOptions(); // to
+
+				} else {
+					System.out.println("Choose a valid user!");
+					ProjectManagerMainMenuUI projectmanagerMenu = new ProjectManagerMainMenuUI(user, project);
+				}
+			}
+		}
+
+		catch (NumberFormatException npe) {
+			System.out.println("-----Insert a valid user!-----");
+		}
+	}
 }
