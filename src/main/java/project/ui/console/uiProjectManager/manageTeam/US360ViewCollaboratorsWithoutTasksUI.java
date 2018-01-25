1b4b3eb75ed042a719aaa8f43312253b2dd705c3
João Bessa <1171465@isep.ipp.pt>
2018-01-25 20:56:23 +0000

    Completed US360UI, Controller and Tests (Show Unassigned Collaborators)

--- src/main/java/project/ui/console/uiProjectManager/manageTeam/US360ViewCollaboratorsWithoutTasksUI.java
+++ src/main/java/project/ui/console/uiProjectManager/manageTeam/US360ViewCollaboratorsWithoutTasksUI.java
@@ -1,8 +1,30 @@
 package project.ui.console.uiProjectManager.manageTeam;
 
+import java.util.List;
+
+import project.controller.US360ViewCollaboratorsWithoutTasksController;
+import project.model.Project;
+
 public class US360ViewCollaboratorsWithoutTasksUI {
 
-	// Integer projectID
-	// Display Collaborators Without tasks
+	public void viewUnassignedCollaborators(Project selectedProject) {
 
+		List<String> idleCollaboratorsInfo;
+
+		US360ViewCollaboratorsWithoutTasksController controller = new US360ViewCollaboratorsWithoutTasksController(
+				selectedProject);
+
+		idleCollaboratorsInfo = controller.showCollaboratorsWithoutTasks();
+
+		if (idleCollaboratorsInfo.isEmpty())
+			System.out.println("Every Team member has one or more tasks assigned!");
+		else {
+			System.out.println("The following Collaborators have no Tasks assigned");
+			for (String other : idleCollaboratorsInfo) {
+				System.out.println(other);
+				System.out.println("");
+			}
+		}
+
+	}
 }
