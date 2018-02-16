664947e46149d20b2a185c8eaccb513df52d342d
Fábio Brás <1160058@isep.ipp.pt>
2018-02-16 16:41:49 +0000

    Changed 3 varibles to only be declared in the methods they are used to fix CS

--- src/main/java/project/controller/US205MarkTaskAsFinishedCollaborator.java
+++ src/main/java/project/controller/US205MarkTaskAsFinishedCollaborator.java
@@ -13,36 +13,34 @@ import project.model.User;
 public class US205MarkTaskAsFinishedCollaborator {
 	private User username;
 	private int projectIndex;
-	private String taskIndex;
 	private Task taskToBeMarked;
-	private ProjectRepository projectList;
-	private ProjectCollaborator collab;
-	private Project selectedProject;
 
 	public List<Project> getProjectsThatIAmCollaborator(User user) {
 		List<Project> projectsThatImProjectCollaborator = new ArrayList<>();
 		this.username = user;
-		projectList = Company.getTheInstance().getProjectsRepository();
+		ProjectRepository projectList = Company.getTheInstance().getProjectsRepository();
 		projectsThatImProjectCollaborator.addAll(projectList.getProjectsFromUser(this.username));
 		return projectsThatImProjectCollaborator;
 	}
 
 	public List<Task> getUnfinishedTasksOfProjectFromCollaborator(int projectIndex) {
+		ProjectCollaborator collab;
+		Project selectedProject;
 		List<Task> unfinishedTaskFromProject = new ArrayList<>();
 		this.projectIndex = projectIndex;
 		selectedProject = Company.getTheInstance().getProjectsRepository().getProjById(this.projectIndex);
-		this.collab = Company.getTheInstance().getProjectsRepository().getProjById(this.projectIndex)
+		collab = Company.getTheInstance().getProjectsRepository().getProjById(this.projectIndex)
 				.getProjectCollaboratorFromUser(this.username);
 		unfinishedTaskFromProject = selectedProject.getTaskRepository()
-				.getUnfinishedTasksFromProjectCollaborator(this.collab);
+				.getUnfinishedTasksFromProjectCollaborator(collab);
 		return unfinishedTaskFromProject;
 	}
 
-	public Task getTaskToBeMarkedFinished(String taskIndex) {
-		this.taskIndex = taskIndex;
+	public Task getTaskToBeMarkedFinished(String taskIndex1) {
+		String taskIndex = taskIndex1;
 		List<Task> unfinishedTaskFromProject = getUnfinishedTasksOfProjectFromCollaborator(this.projectIndex);
 		taskToBeMarked = Company.getTheInstance().getProjectsRepository().getProjById(this.projectIndex)
-				.getTaskRepository().getTaskByID(this.taskIndex);
+				.getTaskRepository().getTaskByID(taskIndex);
 
 		return taskToBeMarked;
 	}
