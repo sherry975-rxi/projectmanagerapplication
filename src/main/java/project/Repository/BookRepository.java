4f8a7659d3b4b77cdc6dc8e60e3eb76f212fae3f
Daniel Pereira <1171460@isep.ipp.pt>
2018-03-06 09:18:51 +0000

    Removed unused classes and interfaces which were her only to provide examples.

--- src/main/java/project/Repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package project.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-}
