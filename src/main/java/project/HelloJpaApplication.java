193d08b8c729201e12ec0e4dc292122096f0b273
Rita Machado <1171471@isep.ipp.pt>
2018-03-08 15:17:43 +0000

    changes made to main for presistence purposes

--- src/main/java/project/HelloJpaApplication.java
+++ src/main/java/project/HelloJpaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import project.model.*;
 
+import java.util.Calendar;
+
 
 @SpringBootApplication
 public class HelloJpaApplication implements CommandLineRunner {
@@ -36,14 +38,27 @@ public class HelloJpaApplication implements CommandLineRunner {
         Project project = company.getProjectsContainer().createProject("NOME PROJECTO", "DESCRIÇÃO PROJECTO", manuel);
 
         //Creates the task through the project
-        Task task = project.getTaskRepository().createTask("DESCRIÇÃO TAREFA");
+        Task task = project.getTaskRepository().createTask("Description Task");
+        Task task2 = project.getTaskRepository().createTask("Inject Dependencies");
         //Adds the task to the task repository
         project.getTaskRepository().addProjectTask(task);
+        project.getTaskRepository().addProjectTask(task2);
         //Creates ProjectCollaborator through the Project
         ProjectCollaborator manuelProjCollab = project.createProjectCollaborator(manuel,7);
         //Adds the projectCollaborator to the ProjectTeam
         //project.addProjectCollaboratorToProjectTeam(manuelProjCollab);
         project.addUserToProjectTeam(manuel,7000);
+        //Adds taskCollaborator to Task
+        project.getTaskRepository().getTaskByID(task.getTaskID()).addProjectCollaboratorToTask(manuelProjCollab);
+        project.getTaskRepository().getTaskByID(task2.getTaskID()).addProjectCollaboratorToTask(manuelProjCollab);
+        //Create Task Report
+        Calendar date = Calendar.getInstance();
+        project.getTaskRepository().getTaskByID(task.getTaskID()).createReport(task.getTaskTeam().get(0), date, 5 );
+
+        //Create dependency between two tasks
+        project.getTaskRepository().getTaskByID(task.getTaskID()).setStartDate(date);
+        project.getTaskRepository().getTaskByID(task2.getTaskID()).createTaskDependence(task,2);
+
         //Saves the project to the database
         company.getProjectsContainer().saveProject(project);
         //Gets the project from the database by the id
