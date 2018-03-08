37eea479636461476d110388b3bc1eb0fac0c52b
Fábio  Brás <1160058@isep.ipp.pt>
2018-03-08 16:55:04 +0000

    Created new Controller to manage the update of the Lists in UserContainer & ProjectContainer with the information contained in the DB

---
+++ src/main/java/project/controller/UpdateDbToContainersController.java
@@ -0,0 +1,20 @@
+package project.controller;
+
+import project.model.Company;
+import project.model.ProjectContainer;
+import project.model.UserContainer;
+
+public class UpdateDbToContainersController {
+
+    UserContainer userContainer;
+    ProjectContainer projectContainer;
+
+    public void updateDBtoContainer(){
+        this.projectContainer = Company.getTheInstance().getProjectsContainer();
+        this.userContainer = Company.getTheInstance().getUsersContainer();
+        projectContainer.updateProjectContainer();
+        userContainer.updateUserContainer();
+
+    }
+
+}
