e4930bb6e489b2cb2a8b47850e4ec751d45aa384
Daniel Almeida <1171459@isep.ipp.pt>
2018-03-08 10:04:33 +0000

    Declared the method findByEmail in the UserRepository interface.

--- src/main/java/project/Repository/UserRepository.java
+++ src/main/java/project/Repository/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+    User findByEmail(String email);
 }
