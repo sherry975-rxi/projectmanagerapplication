2059d7266d52f910cfb1b1a8dd42051c3d67c963
Daniel Almeida <1171459@isep.ipp.pt>
2018-03-08 09:59:27 +0000

    Declared the method findById to the ProjectRepository interface.

--- src/main/java/project/Repository/ProjectsRepository.java
+++ src/main/java/project/Repository/ProjectsRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.model.Project;
 
 public interface ProjectsRepository extends JpaRepository<Project, Integer>{
+
+    Project findById(long id);
 }
