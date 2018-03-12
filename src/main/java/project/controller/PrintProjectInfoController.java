e711d45aa607e7af351735efda0fc4774bde00d0
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-03-12 12:18:41 +0000

    PrintProjectInfoController: remove Company

--- src/main/java/project/controller/PrintProjectInfoController.java
+++ src/main/java/project/controller/PrintProjectInfoController.java
@@ -2,6 +2,7 @@ package project.controller;
 
 import project.model.Project;
 import project.model.ProjectCollaborator;
+import project.model.ProjectContainer;
 import project.model.Task;
 
 import java.text.SimpleDateFormat;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class PrintProjectInfoController {
 
+	private ProjectContainer projContainer;
 	private Project project;
 	private SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
 	Integer projID;
@@ -21,11 +23,11 @@ public class PrintProjectInfoController {
 
 	public PrintProjectInfoController(Integer projID) {
 		this.projID = projID;
-		project = Company.getTheInstance().getProjectsContainer().getProjById(this.projID);
+		project = projContainer.getProjById(this.projID);
 	}
 
 	public void setProject() {
-		this.project = Company.getTheInstance().getProjectsContainer().getProjById(this.project.getIdCode());
+		this.project = projContainer.getProjById(this.project.getIdCode());
 	}
 
 	/**
@@ -161,7 +163,7 @@ public class PrintProjectInfoController {
 	 * @return List of Strings of project's task (task ID + task description)
 	 */
 	public List<String> getProjectTaskList() {
-		List<Task> taskList = Company.getTheInstance().getProjectsContainer().getProjById(this.project.getIdCode())
+		List<Task> taskList = projContainer.getProjById(this.project.getIdCode())
 				.getTaskRepository().getProjectTaskRepository();
 		List<String> projectTaskList = new ArrayList<>();
 		for (Task projectTask : taskList) {
@@ -179,7 +181,7 @@ public class PrintProjectInfoController {
 	 */
 	public List<String> getTasksIDs() {
 
-		List<Task> taskList = Company.getTheInstance().getProjectsContainer().getProjById(this.project.getIdCode())
+		List<Task> taskList = projContainer.getProjById(this.project.getIdCode())
 
 				.getTaskRepository().getProjectTaskRepository();
 		List<String> projectTasksID = new ArrayList<>();
@@ -195,7 +197,7 @@ public class PrintProjectInfoController {
 	 * @return List of project's task
 	 */
 	public List<Task> getTasks() {
-		return Company.getTheInstance().getProjectsContainer().getProjById(this.project.getIdCode())
+		return projContainer.getProjById(this.project.getIdCode())
 				.getTaskRepository().getProjectTaskRepository();
 	}
 
