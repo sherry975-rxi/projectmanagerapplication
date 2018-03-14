5077d3df17424deb2e6775d3dde0e24c75b7e8d7
Rita Machado <1171471@isep.ipp.pt>
2018-03-14 23:57:22 +0000

    Call methods findAllByEmail and findAllByProfile in UserRepository

--- src/main/java/project/Repository/UserRepository.java
+++ src/main/java/project/Repository/UserRepository.java
@@ -1,11 +1,18 @@
 package project.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.model.Profile;
 import project.model.User;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
     User findByEmail(String email);
+
+    List<User> findAllByEmail(String email);
+
+    List<User> findAllByProfile(Profile profile);
 
     void deleteByEmail(String email);
 }
