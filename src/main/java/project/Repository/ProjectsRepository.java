1d5a06a5b774e697b6ad29a83ceacf8e92db2f7b
Daniel Almeida <1171459@isep.ipp.pt>
2018-03-14 11:04:01 +0000

    Declared the new method findByprojectStatus in the ProjectRepository interface.

--- src/main/java/project/Repository/ProjectsRepository.java
+++ src/main/java/project/Repository/ProjectsRepository.java
@@ -3,7 +3,16 @@ package project.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.model.Project;
 
+import java.util.List;
+
 public interface ProjectsRepository extends JpaRepository<Project, Integer>{
 
     Project findById(long id);
+
+    List<Project> findByProjectStatus(int status);
+
 }
+
+
+
+
