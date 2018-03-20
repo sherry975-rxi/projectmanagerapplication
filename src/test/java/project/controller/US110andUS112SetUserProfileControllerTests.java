808662c7e89b983aa933450f19824d9aa1354d29
Fábio  Brás <1160058@isep.ipp.pt>
2018-03-20 13:07:22 +0000

    Added the necessary annotations to US110andUS112 Controller to support integration testing, fixed US110andUS112 integration tests

--- src/test/java/project/controller/US110andUS112SetUserProfileControllerTests.java
+++ src/test/java/project/controller/US110andUS112SetUserProfileControllerTests.java
@@ -1,105 +1,94 @@
 package project.controller;
 
-import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.mockito.Mockito.*;
-
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import project.Repository.UserRepository;
 import project.Services.UserService;
 import project.model.Profile;
 import project.model.User;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.MockitoAnnotations.initMocks;
-
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class US110andUS112SetUserProfileControllerTests {
 
-    @InjectMocks
-    private UserService userContainer;
+	@Autowired
+	UserRepository userRepository;
 
-    @Mock
-    private US110andUS112SetUserProfileController us110andUS112SetUserProfileController;
+	UserService userContainer;
+	US110andUS112SetUserProfileController us110andUS112SetUserProfileController;
 
-    private User newUser2, newUser3;
+	User newUser2;
+	User newUser3;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
 
-        initMocks(this);
+		userContainer = new UserService();
 
-        when(us110andUS112SetUserProfileController.getUserContainer()).thenReturn(userContainer);
+		userContainer.setUserRepository(userRepository);
 
-        newUser2 = us110andUS112SetUserProfileController.getUserContainer().createUser("Manel", "user2@gmail.com",
-                "001", "Empregado", "930000000",
-                "Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
+		us110andUS112SetUserProfileController = new US110andUS112SetUserProfileController();
+		us110andUS112SetUserProfileController.setUserContainer(userContainer);
 
-        newUser3 = us110andUS112SetUserProfileController.getUserContainer().createUser("Manelinho", "user3@gmail.com",
-                "002", "Telefonista",
-                "940000000", "Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
+		newUser2 = us110andUS112SetUserProfileController.getUserContainer().createUser("Manel", "user2@gmail.com",
+				"001", "Empregado", "930000000", "Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
 
-        us110andUS112SetUserProfileController.getUserContainer().addUserToUserRepository(newUser2);
-        us110andUS112SetUserProfileController.getUserContainer().addUserToUserRepository(newUser3);
+		newUser3 = us110andUS112SetUserProfileController.getUserContainer().createUser("Manelinho", "user3@gmail.com",
+				"002", "Telefonista", "940000000", "Testy Street", "2401-343", "Testburg", "Testo", "Testistan");
 
-    }
+	}
 
-    @After
-    public void breakDown() {
-        newUser2 = null;
-        newUser3 = null;
-        us110andUS112SetUserProfileController = null;
+	@Test
+	public void testSetUserAsDirectorController() {
+		// given a User - newUser2 - asserts they start as Visitor when created
+		// and then creates the controller
+		assertEquals(newUser2.getUserProfile(), Profile.UNASSIGNED);
 
-    }
+		// sets newUser2 as Director and asserts its profile
+		us110andUS112SetUserProfileController.setUserAsDirector(newUser2);
+		assertEquals(newUser2.getUserProfile(), Profile.DIRECTOR);
 
-    @Test
-    public void testSetUserAsDirectorController() {
-        // given a User - newUser2 - asserts they start as Visitor when created
-        // and then creates the controller
-        assertEquals(newUser2.getUserProfile(), Profile.UNASSIGNED);
+		// finally, asserts that no other Users were changed
+		// and that user repository contains the new director
+		assertTrue(us110andUS112SetUserProfileController.getUserContainer().searchUsersByProfile(Profile.DIRECTOR)
+				.contains(newUser2));
+		assertEquals(newUser3.getUserProfile(), Profile.UNASSIGNED);
+	}
 
-        // sets newUser2 as Director and asserts its profile
-        us110andUS112SetUserProfileController.setUserAsDirector(newUser2);
-        assertEquals(newUser2.getUserProfile(), Profile.DIRECTOR);
+	@Test
+	public void testSetUserAsCollaboratorController() {
+		// given a User - newUser2 - asserts they start as Visitor when created
+		// and then creates the controller
+		assertEquals(newUser2.getUserProfile(), Profile.UNASSIGNED);
 
-        // finally, asserts that no other Users were changed
-        // and that user repository contains the new director
-        assertTrue(us110andUS112SetUserProfileController.getUserContainer().searchUsersByProfile(Profile.DIRECTOR).contains(newUser2));
-        assertEquals(newUser3.getUserProfile(), Profile.UNASSIGNED);
-    }
+		// sets newUser2 as Collaborator and asserts its profile
+		us110andUS112SetUserProfileController.setUserAsCollaborator(newUser2);
+		assertEquals(newUser2.getUserProfile(), Profile.COLLABORATOR);
 
-    @Test
-    public void testSetUserAsCollaboratorController() {
-        // given a User - newUser2 - asserts they start as Visitor when created
-        // and then creates the controller
-        assertEquals(newUser2.getUserProfile(), Profile.UNASSIGNED);
+		// finally, asserts that no other Users were changed
+		// and that user repository contains the new director
+		assertTrue(us110andUS112SetUserProfileController.getUserContainer().searchUsersByProfile(Profile.COLLABORATOR)
+				.contains(newUser2));
+		assertEquals(newUser3.getUserProfile(), Profile.UNASSIGNED);
+	}
 
-        // sets newUser2 as Collaborator and asserts its profile
-        us110andUS112SetUserProfileController.setUserAsCollaborator(newUser2);
-        assertEquals(newUser2.getUserProfile(), Profile.COLLABORATOR);
+	@Test
+	public void testUserProfileAsString() {
+		assertTrue("Unassigned".equals(us110andUS112SetUserProfileController.userProfileAsString(newUser2)));
+		us110andUS112SetUserProfileController.setUserAsCollaborator(newUser2);
 
-        // finally, asserts that no other Users were changed
-        // and that user repository contains the new director
-        assertTrue(us110andUS112SetUserProfileController.getUserContainer().searchUsersByProfile(Profile.COLLABORATOR).contains(newUser2));
-        assertEquals(newUser3.getUserProfile(), Profile.UNASSIGNED);
-    }
+		assertTrue("Collaborator".equals(us110andUS112SetUserProfileController.userProfileAsString(newUser2)));
+		us110andUS112SetUserProfileController.setUserAsDirector(newUser2);
 
-    @Test
-    public void testUserProfileAsString() {
-        assertTrue("Unassigned".equals(us110andUS112SetUserProfileController.userProfileAsString(newUser2)));
-        us110andUS112SetUserProfileController.setUserAsCollaborator(newUser2);
-
-        assertTrue("Collaborator".equals(us110andUS112SetUserProfileController.userProfileAsString(newUser2)));
-        us110andUS112SetUserProfileController.setUserAsDirector(newUser2);
-
-        assertTrue("Director".equals(us110andUS112SetUserProfileController.userProfileAsString(newUser2)));
-    }
-
+		assertTrue("Director".equals(us110andUS112SetUserProfileController.userProfileAsString(newUser2)));
+	}
 
 }
