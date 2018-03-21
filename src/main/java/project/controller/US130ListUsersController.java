be8f5d53db7ae1ccb604a0f4968c691d59fd72bc
Daniel Pereira <1171460@isep.ipp.pt>
2018-03-21 14:10:34 +0000

    Removed unnecessary User.

--- src/main/java/project/controller/US130ListUsersController.java
+++ src/main/java/project/controller/US130ListUsersController.java
@@ -16,7 +16,6 @@ public class US130ListUsersController {
 	public UserService userContainer;
 	
 	List<User> userList;
-	User selectedUser = null;
 
 	/**
 	 * This controller returns a list of all users in the User Repository
@@ -44,13 +43,14 @@ public class US130ListUsersController {
 	 * @return User to be Returned and handled by the Admin
 	 */
 	public User selectUser(int index) {
+		User result;
 		int actualIndex = index - 1;
 		if (actualIndex >= 0 && actualIndex < userList.size()) {
-			selectedUser = userList.get(actualIndex);
+			result = userList.get(actualIndex);
 		} else {
-			selectedUser = null;
+			result = null;
 		}
-		return selectedUser;
+		return result;
 
 	}
 
