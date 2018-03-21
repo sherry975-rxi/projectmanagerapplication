5cf1da7e0108f12508443da87ac3c721fe7bef5b
João Leite <1161661@isep.ipp.pt>
2018-03-21 11:49:05 +0000

    ISSUE39 - Fixed controller US347 and tests

--- src/test/java/project/controller/US347CancelOnGoingTaskControllerTest.java
+++ src/test/java/project/controller/US347CancelOnGoingTaskControllerTest.java
@@ -3,18 +3,33 @@
  */
 package project.controller;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import project.Services.ProjectService;
-import project.Services.UserService;
-import project.model.*;
-import project.model.taskstateinterface.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import project.Repository.ProjectsRepository;
+import project.Repository.TaskRepository;
+import project.Repository.UserRepository;
+import project.Services.ProjectService;
+import project.Services.TaskService;
+import project.Services.UserService;
+import project.model.Profile;
+import project.model.Project;
+import project.model.ProjectCollaborator;
+import project.model.Task;
+import project.model.TaskCollaborator;
+import project.model.User;
+import project.model.taskstateinterface.Cancelled;
+import project.model.taskstateinterface.OnGoing;
+import project.model.taskstateinterface.Planned;
+import project.model.taskstateinterface.Ready;
 
 /**
  * This class tests the methods that are called in Controller to execute the
@@ -23,11 +38,21 @@ import static org.junit.Assert.assertEquals;
  * @author Group3
  *
  */
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class US347CancelOnGoingTaskControllerTest {
 
 	// TasksFiltersController tasksFiltersController;
-	UserService userContainer;
-	ProjectService projectContainer;
+	@Autowired
+	private UserRepository userRepository;
+	UserService userService;
+	ProjectService projectService;
+	@Autowired
+	ProjectsRepository projectRepository;
+	TaskService taskService;
+	@Autowired
+	TaskRepository taskRepository;
 	User user1, user2, projectManager;
 	Project project1;
 	ProjectCollaborator projCollab1, projCollab2;
@@ -37,60 +62,53 @@ public class US347CancelOnGoingTaskControllerTest {
 	Calendar estimatedTaskStartDateTest;
 	Calendar taskDeadlineDateTest;
 	Calendar taskExpiredDeadlineDateTest;
+	US347CancelOnGoingTaskController controllerCancel;
 
 	@Before
 	public void setUp() {
 
-		// creates an UserContainer
-		userContainer = new UserService();
+		// creates an UserService
+		userService = new UserService();
+		userService.setUserRepository(userRepository);
+
+		// creates a ProjectService
+		projectService = new ProjectService();
+		projectService.setProjectsRepository(projectRepository);
+
+		// creates a TaskService
+		taskService = new TaskService();
+		taskService.setTaskRepository(taskRepository);
 
 		// create users
-		user1 = userContainer.createUser("Joe Smith", "jsmith@gmail.com", "001", "Junior Programmer",
-				"930000000", "Rua da Caparica, 19", "7894-654", "Porto", "Porto", "Portugal");
-		user2 = userContainer.createUser("John Smith", "johnsmith@gmail.com", "001", "General Manager",
-				"930025000", "Rua Doutor Armando", "4455-654", "Rio Tinto", "Gondomar", "Portugal");
-		projectManager = userContainer.createUser("Mary MacJohn", "mmacjohn@gmail.com", "003",
-				"Product Manager", "930025000", "Rua Terceira, 44", "4455-122", "Leça da Palmeira", "Matosinhos",
-				"Portugal");
-
-		// add users to company
-		userContainer.addUserToUserRepository(user1);
-		userContainer.addUserToUserRepository(user2);
-		userContainer.addUserToUserRepository(projectManager);
+		user1 = userService.createUser("Joe Smith", "jsmith@gmail.com", "001", "Junior Programmer", "930000000",
+				"Rua da Caparica, 19", "7894-654", "Porto", "Porto", "Portugal");
+		user2 = userService.createUser("John Smith", "johnsmith@gmail.com", "001", "General Manager", "930025000",
+				"Rua Doutor Armando", "4455-654", "Rio Tinto", "Gondomar", "Portugal");
+		projectManager = userService.createUser("Mary MacJohn", "mmacjohn@gmail.com", "003", "Product Manager",
+				"930025000", "Rua Terceira, 44", "4455-122", "Leça da Palmeira", "Matosinhos", "Portugal");
 
 		// set user as collaborator
 		user1.setUserProfile(Profile.COLLABORATOR);
 		user2.setUserProfile(Profile.COLLABORATOR);
 		projectManager.setUserProfile(Profile.COLLABORATOR);
-		
-		// creates a Project Container
-		projectContainer = new ProjectService();
 
 		// create project and establishes collaborator projectManager as project manager
 		// of project 1
-		project1 = projectContainer.createProject("Project Management software",
-				"This software main goals are ....", projectManager);
-
-		// add project to company
-		projectContainer.addProjectToProjectContainer(project1);
+		project1 = projectService.createProject("Project Management software", "This software main goals are ....",
+				projectManager);
 
 		// create project collaborators
 		projCollab1 = new ProjectCollaborator(user1, 2);
 		projCollab2 = new ProjectCollaborator(user2, 2);
 
 		// add collaborators to Project
-		project1.addProjectCollaboratorToProjectTeam(projCollab1);
-		project1.addProjectCollaboratorToProjectTeam(projCollab2);
+		project1.createProjectCollaborator(user1, 2);
+		project1.createProjectCollaborator(user2, 2);
 
 		// create tasks
-		task1 = project1.getTaskService().createTask("Create class User");
-		task2 = project1.getTaskService().createTask("Create class User");
-		task3 = project1.getTaskService().createTask("create test for method set name in class user");
-
-		// add tasks to task repository
-		project1.getTaskService().addTaskToProject(task1);
-		project1.getTaskService().addTaskToProject(task2);
-		project1.getTaskService().addTaskToProject(task3);
+		task1 = taskService.createTask("Task 1", project1);
+		task2 = taskService.createTask("Task 2", project1);
+		task3 = taskService.createTask("Task 3", project1);
 
 		// create a estimated Task Start Date
 		startDateTest = Calendar.getInstance();
@@ -118,9 +136,8 @@ public class US347CancelOnGoingTaskControllerTest {
 
 		// Creates State Objects planned for task.
 		task1.setTaskState(new Planned());
-        task2.setTaskState(new Planned());
-        task3.setTaskState(new Planned());
-
+		task2.setTaskState(new Planned());
+		task3.setTaskState(new Planned());
 
 		// set estimated task start date and task dead line to tasks
 		task1.setEstimatedTaskStartDate(estimatedTaskStartDateTest);
@@ -146,36 +163,20 @@ public class US347CancelOnGoingTaskControllerTest {
 		task2.addTaskCollaboratorToTask(taskCollab2);
 		task3.addTaskCollaboratorToTask(taskCollab1);
 
-        // Sets the tasks to "Ready"
-        task1.setTaskState(new Ready());
-        task2.setTaskState(new Ready());
-        task3.setTaskState(new Ready());
+		// Sets the tasks to "Ready"
+		task1.setTaskState(new Ready());
+		task2.setTaskState(new Ready());
+		task3.setTaskState(new Ready());
 
 		// Sets the tasks to "onGoing"
 		task1.setTaskState(new OnGoing());
 		task2.setTaskState(new OnGoing());
 		task3.setTaskState(new OnGoing());
-	}
 
-	@After
-	public void tearDown() {
-		projectContainer = null;
-		userContainer = null;
-		user1 = null;
-		user2 = null;
-		projectManager = null;
-		project1 = null;
-		projCollab1 = null;
-		projCollab2 = null;
-		task1 = null;
-		task2 = null;
-		task3 = null;
-		taskCollab1 = null;
-		taskCollab2 = null;
-		startDateTest = null;
-		estimatedTaskStartDateTest = null;
-		taskDeadlineDateTest = null;
-		taskExpiredDeadlineDateTest = null;
+		// create controller
+		controllerCancel = new US347CancelOnGoingTaskController(task1.getTaskID(), project1);
+		controllerCancel.taskService = taskService;
+
 	}
 
 	/**
@@ -184,11 +185,8 @@ public class US347CancelOnGoingTaskControllerTest {
 	 */
 	@Test
 	public void testViewTaskState() {
-		// create controller
-		US347CancelOnGoingTaskController uS347CancelOnGoingTaskController = new US347CancelOnGoingTaskController(
-				task1.getTaskID(), project1);
 
-		assertEquals("OnGoing", uS347CancelOnGoingTaskController.viewTaskState());
+		assertEquals("OnGoing", controllerCancel.viewTaskState());
 	}
 
 	/**
@@ -198,45 +196,34 @@ public class US347CancelOnGoingTaskControllerTest {
 	@Test
 	public void testCancelOnGoingTask() {
 
-		// create controller
-		US347CancelOnGoingTaskController uS347CancelOnGoingTaskController = new US347CancelOnGoingTaskController(
-				task1.getTaskID(), project1);
-
 		// Sets task2 to "cancelled"
-		task2.setTaskState(new Cancelled());
+		task1.setTaskState(new Cancelled());
 
 		// Sets a cancel date for the task1
 		task1.setCancelDate();
 
 		// use of control to set task1 to state cancelled
-		uS347CancelOnGoingTaskController.cancelOnGoingTask();
+		controllerCancel.cancelOnGoingTask();
 
 		// asserts that task1 has state cancelled
 		assertEquals("Cancelled", task1.viewTaskStateName());
 	}
 
-
 	/**
-	 * This test verifies if task in state OnGoing and then marked with a finish date
-	 * can not be changed to Cancelled using
-	 * the controller.
+	 * This test verifies if task in state OnGoing and then marked with a finish
+	 * date can not be changed to Cancelled using the controller.
 	 */
 	@Test
 	public void testNotCancelOnGoingTask() {
-
-		// create controller
-		US347CancelOnGoingTaskController uS347NotCancelOnGoingTaskController = new US347CancelOnGoingTaskController(task1.getTaskID(), project1);
-
 
 		// Sets a finish date for the task1
 		task1.setFinishDate(Calendar.getInstance());
 
 		// use of control to set task1 to state cancelled
-		uS347NotCancelOnGoingTaskController.cancelOnGoingTask();
+		controllerCancel.cancelOnGoingTask();
 
 		// asserts that task1 has state cancelled
 		assertEquals("OnGoing", task1.viewTaskStateName());
 	}
-
 
 }
