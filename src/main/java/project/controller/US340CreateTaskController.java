331b4c61c8c39ad54e9b631ffe8fe51002755a11
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-03-23 10:44:17 +0000

    US340CreateTaskController: remove unused method

--- src/main/java/project/controller/US340CreateTaskController.java
+++ src/main/java/project/controller/US340CreateTaskController.java
@@ -12,7 +12,7 @@ public class US340CreateTaskController {
 	@Autowired
 	private TaskService taskService;
 
-	private Project chosenProject;
+
 
 	/**
 	 * This constructor creates a target controller. Currently, it receives a
@@ -22,10 +22,6 @@ public class US340CreateTaskController {
 	public US340CreateTaskController() {
 		//Empty constructor created for JPA integration tests
 
-	}
-
-	public void setChosenProject(Project chosenProject) {
-		this.chosenProject = chosenProject;
 	}
 
 	public TaskService getTaskService() {
