bd360608c3c613f649769ff7c97986a2b617cdc7
Fábio  Brás <1160058@isep.ipp.pt>
2018-04-04 16:39:10 +0100

    Removed the effort variable and organized the code to be done in the POST Request

--- src/main/java/project/restControllers/us204AssignTaskRequest.java
+++ src/main/java/project/restControllers/us204AssignTaskRequest.java
@@ -1,10 +1,7 @@
 package project.restControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.model.Project;
 import project.model.ProjectCollaborator;
 import project.model.Task;
@@ -14,14 +11,13 @@ import project.services.TaskService;
 import project.services.UserService;
 
 @RestController
-@RequestMapping("/projects/{projectID}/tasks/{taskID}/assignmentrequests")
+@RequestMapping("/projects/{projectId}/tasks/{taskID}")
 public class us204AssignTaskRequest {
 
     private UserService userService;
     private TaskService taskService;
     private ProjectService projectService;
     private User user;
-    private Project project;
 
     @Autowired
     public us204AssignTaskRequest(UserService userService, TaskService taskService, ProjectService projectService) {
@@ -31,27 +27,18 @@ public class us204AssignTaskRequest {
     }
 
 
-    @RequestMapping(method = RequestMethod.GET)
-    public User getUserByEmail (String email){
-        return this.user = userService.getUserByEmail(email);
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Project getProjectFromTask (@PathVariable String taskID){
-        Task task = taskService.getTaskByTaskID(taskID);
-        return this.project = task.getProject();
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public void createRequestAddCollabToTask (@PathVariable String taskID, int effort){
+    @RequestMapping(value = "/CreateAssignmentRequest" , method = RequestMethod.POST)
+    public void createRequestAddCollabToTask (@PathVariable String taskID, @PathVariable int projectId, @RequestBody String email){
         ProjectCollaborator projectCollaborator;
+
+        Project project = projectService.getProjectById(projectId);
+
         Task task = taskService.getTaskByTaskID(taskID);
-        if(this.projectService.isUserActiveInProject(this.user, this.project)){
-            projectCollaborator = this.projectService.findActiveProjectCollaborator(this.user, this.project);
-            task.createTaskAssignementRequest(projectCollaborator);
-        }
-        else{
-            projectCollaborator = this.project.createProjectCollaborator(this.user, effort);
+
+        this.user = userService.getUserByEmail(email);
+
+        if(this.projectService.isUserActiveInProject(this.user, project)){
+            projectCollaborator = this.projectService.findActiveProjectCollaborator(this.user, project);
             task.createTaskAssignementRequest(projectCollaborator);
         }
     }
