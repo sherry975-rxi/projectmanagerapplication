d7c1a5370a8ef6539eb6009018a1526cb60889f6
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-04 12:48:51 +0100

    Added annotation @JsonBackReference to the User variable in the Address class beucase it was causing a StackOverFlowError due to a invalid recursive call.

--- src/main/java/project/model/Address.java
+++ src/main/java/project/model/Address.java
@@ -1,5 +1,7 @@
 package project.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 
 
@@ -25,7 +27,8 @@ public class Address{
 	private String district;
 	private String country;
 	static final long serialVersionUID = 42L;
-	
+
+	@JsonBackReference
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "User_id")
 	private User user;
