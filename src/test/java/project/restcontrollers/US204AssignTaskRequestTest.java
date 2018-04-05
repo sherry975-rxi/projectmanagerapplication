5b68cba656a965d5f38004d387a6b7bb2868f077
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-04-06 00:42:30 +0100

    refs #53 && #54 US 204 Rest Controller Test: adding more context

--- src/test/java/project/restcontrollers/US204AssignTaskRequestTest.java
+++ src/test/java/project/restcontrollers/US204AssignTaskRequestTest.java
@@ -1,81 +1,107 @@
 package project.restcontrollers;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import project.model.Project;
-import project.model.ProjectCollaborator;
-import project.model.Task;
-import project.model.User;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import project.model.*;
+import project.restControllers.us204AssignTaskRequest;
 import project.services.ProjectService;
 import project.services.TaskService;
 import project.services.UserService;
 
 
-//Configure H2
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@RunWith(MockitoJUnitRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
 public class US204AssignTaskRequestTest {
 
-
-    @Autowired
-    UserService userService;
-    @Autowired
-    ProjectService projectService;
-    @Autowired
-    TaskService taskService;
-
-    @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    UserService userService;
+    @Mock
+    ProjectService projectService;
+    @Mock
+    TaskService taskService;
 
-    private User userOne;
+    @InjectMocks
+    private us204AssignTaskRequest controller;
+
+    private User userPM;
     private User userTwo;
+    private String userTwoEmail;
     private User userThree;
+    private String userThreeEmail;
     private Project projectOne;
+    private Integer projectId;
     private Task taskOne;
+    private String taskIdOne;
     private Task taskTwo;
+    private String taskIdTwo;
     private ProjectCollaborator projCollabTwo;
     private ProjectCollaborator projCollabThree;
+
+    //This objects will be initialized by the initFields below
+    private JacksonTester<Project> projectJack;
+    private JacksonTester<Task> taskJack;
+    private JacksonTester<TaskTeamRequest> requestJacksonTester;
 
 
     @Before
     public void setUp() throws Exception{
 
-        // create userOne and save in DB
-        userOne = userService.createUser("Ana", "ana@gmail.com", "01", "collaborator", "221238442", "Rua Porto",
+        JacksonTester.initFields(this, new ObjectMapper());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        // create userPM and save in DB
+        userPM = userService.createUser("Ana", "ana@gmail.com", "01", "collaborator", "221238442", "Rua Porto",
                 "4480", "Porto", "Porto", "Portugal");
 
         // create userTwo and save in DB
         userTwo = userService.createUser("Joao", "joao@gmail.com", "02", "collaborator", "221238447", "Rua Porto",
                 "5480", "Porto", "Porto", "Portugal");
+        userTwoEmail = "joao@gmail.com";
 
         // create user3
         userThree = userService.createUser("Rui", "rui@gmail.com", "03", "collaborator", "221378449", "Rua Porto",
                 "4480", "Porto", "Porto", "Portugal");
+        userThreeEmail = "rui@gmail.com";
 
 
-        // create a project with userOne as manager
-        projectOne = projectService.createProject("Restful", "Implement API Rest", userOne);
+        // create a project with userPM as manager
+        projectOne = projectService.createProject("Restful", "Implement API Rest", userPM);
+        projectId = 1;
 
         // create 2 tasks
         taskOne = taskService.createTask("Rest Controller", projectOne);
+        taskIdOne = "1.1";
 
         taskTwo = taskService.createTask("Rest Controller Test", projectOne);
+        taskIdTwo = "1.2";
+
 
         // add users to projectOne creating a project collaborator for each
         projCollabTwo = projectService.createProjectCollaborator(userTwo, projectOne, 20);
         projCollabThree = projectService.createProjectCollaborator(userThree, projectOne, 60);
-
-
 
 
     }
@@ -83,7 +109,7 @@ public class US204AssignTaskRequestTest {
 
     @After
     public void clear() throws Exception{
-        userOne = null;
+        userPM = null;
         userTwo = null;
         projectOne = null;
         taskOne = null;
@@ -93,9 +119,57 @@ public class US204AssignTaskRequestTest {
 
     }
     @Test
-    public void test() {
+    public void testSetUpInfo() {
+
+        assertEquals(userTwo, userService.getUserByEmail(userTwoEmail));
+        assertEquals(userThree, userService.getUserByEmail(userThreeEmail));
+        assertEquals(projCollabTwo, projectService.findActiveProjectCollaborator(userTwo, projectOne));
+        assertEquals(projCollabThree, projectService.findActiveProjectCollaborator(userThree, projectOne));
+
+        //Given
+        given(userService.getUserByEmail(userTwoEmail)).willReturn(userTwo);
+        given(userService.getUserByEmail(userThreeEmail)).willReturn(userThree);
+
+        given(projectService.getProjectById(projectId)).willReturn(projectOne);
+
+        given(taskService.getTaskByTaskID(taskIdOne)).willReturn(taskOne);
+        given(taskService.getTaskByTaskID(taskIdTwo)).willReturn(taskTwo);
+
+
     }
 
+    @Test
+    public void canCreateAnAssignmentRequest() throws Exception {
+        //Given
+        given(userService.getUserByEmail(userTwoEmail)).willReturn(userTwo);
+        given(userService.getUserByEmail(userThreeEmail)).willReturn(userThree);
 
+        given(projectService.getProjectById(projectId)).willReturn(projectOne);
+
+        given(taskService.getTaskByTaskID(taskIdOne)).willReturn(taskOne);
+        given(taskService.getTaskByTaskID(taskIdTwo)).willReturn(taskTwo);
+
+        // when
+//        MockHttpServletResponse response2 = mockMvc.perform(
+//                post("/projects/" + projectId + "/tasks/" + taskIdOne + "/CreateAssignmentRequest").contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+//        MockHttpServletResponse response = mockMvc.perform(
+//                post("/projects/" + projectId + "/tasks/" + taskIdOne + "/CreateAssignmentRequest").contentType(MediaType.APPLICATION_JSON).header(userTwoEmail)).andReturn().getResponse();
+
+        String uRi = "/projects/" + projectId + "/tasks/" + taskIdOne + "/CreateAssignmentRequest";
+        TaskTeamRequest newReq = new TaskTeamRequest(projCollabTwo, taskOne);
+        newReq.setType(TaskTeamRequest.ASSIGNMENT);
+        taskOne.getPendingTaskAssignementRequests().add(newReq);
+
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.
+//                post(uRi).contentType(MediaType.APPLICATION_JSON).content(requestJacksonTester.write(newReq).getJson()).header(userTwoEmail)).andReturn().getResponse();
+
+        MockHttpServletResponse response = mockMvc.perform(
+                post(uRi).contentType(MediaType.APPLICATION_JSON).content(taskJack.write(taskOne).getJson()).header(userTwoEmail)).andReturn().getResponse();
+
+
+        // then
+        assertEquals(response.getStatus(),HttpStatus.OK.value());
+    }
 
 }
