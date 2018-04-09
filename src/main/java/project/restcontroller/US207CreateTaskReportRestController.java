2300580d6fd55c88a6e0b7d45664c91fdc39cf9a
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-04-09 20:18:30 +0100

    refs #69 & #70 US207 Rest Controller: adding other method

--- src/main/java/project/restcontroller/US207CreateTaskReportRestController.java
+++ src/main/java/project/restcontroller/US207CreateTaskReportRestController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import project.model.Project;
-import project.model.Task;
-import project.model.User;
+import project.model.*;
 import project.services.ProjectService;
 import project.services.TaskService;
 import project.services.UserService;
@@ -28,20 +26,58 @@ public class US207CreateTaskReportRestController {
         this.projectService = projectService;
     }
 
-    @RequestMapping(value = "/CreateReport" , method = RequestMethod.POST)
-    public ResponseEntity<?> createTaskReport (@RequestHeader double timeToReport, @PathVariable String taskId, @PathVariable int projectId, @RequestHeader String userEmail){
+    @RequestMapping(value = "/CreateReport" , method = RequestMethod.PUT)
+    public ResponseEntity<?> createTaskReport (@RequestBody String timeToReport, @PathVariable String taskId, @PathVariable int projectId, @RequestHeader String userEmail){
         ResponseEntity<?> result = new ResponseEntity<>(HttpStatus.FORBIDDEN);
 
+        double timeReported = Double.valueOf(timeToReport);
 
         Project project = projectService.getProjectById(projectId);
 
         Task task = taskService.getTaskByTaskID(taskId);
 
+        User user = userService.getUserByEmail(userEmail);
+
         Calendar dateOfReport = Calendar.getInstance();
 
-        if(task.createReport(task.getTaskCollaboratorByEmail(userEmail), dateOfReport, timeToReport)){
+        if(task.createReport(task.getTaskCollaboratorByEmail(userEmail), dateOfReport, timeReported)){
             this.taskService.saveTask(task);
-            result = ResponseEntity.status(HttpStatus.OK).body("Report created!" + "\nValue of: " + timeToReport);
+            result = ResponseEntity.status(HttpStatus.OK).body("Report created!\nINFO:" + "\nTask ID: " + task.getTaskID() +"\nDescription: " + task.getDescription() + "\nUser: " + user.getName() + "\nTime reported: " + timeToReport);
+            //result = new ResponseEntity<>(HttpStatus.OK);
+
+        }
+
+
+        return result;
+    }
+
+    @RequestMapping(value = "/CreateReportTwo" , method = RequestMethod.PUT)
+    public ResponseEntity<?> createTaskReportTwo (@RequestBody Report reportOfTime, @PathVariable String taskId, @PathVariable int projectId, @RequestHeader String userEmail){
+        ResponseEntity<?> result = new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        Project project = projectService.getProjectById(projectId);
+
+        Task task = taskService.getTaskByTaskID(taskId);
+
+        User user = userService.getUserByEmail(userEmail);
+
+        TaskCollaborator taskCollab = task.getTaskCollaboratorByEmail(userEmail);
+
+        Calendar dateOfReport = Calendar.getInstance();
+
+        double timeReported = reportOfTime.getReportedTime();
+
+        String time = Double.toString(timeReported);
+
+        System.out.println("Updating Task " + task.getTaskID());
+
+
+
+
+        if(task.createReport(taskCollab, dateOfReport, timeReported)){
+            this.taskService.saveTask(task);
+            System.out.println();
+            result = ResponseEntity.status(HttpStatus.OK).body("Report created!\nINFO:" + "\nTask ID: " + task.getTaskID() +"\nDescription: " + task.getDescription() + "\nUser: " + user.getName() + "\nTime reported: " + time);
             //result = new ResponseEntity<>(HttpStatus.OK);
 
         }
