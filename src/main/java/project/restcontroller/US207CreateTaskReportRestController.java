74e0fb4171e0028c578910ba35b7807429513565
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-04-09 15:54:00 +0100

    refs #69 & #70 US207 Rest Controller: adding method createTaskReport()

--- src/main/java/project/restcontroller/US207CreateTaskReportRestController.java
+++ src/main/java/project/restcontroller/US207CreateTaskReportRestController.java
@@ -1,4 +1,58 @@
 package project.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.model.Project;
+import project.model.Task;
+import project.model.User;
+import project.services.ProjectService;
+import project.services.TaskService;
+import project.services.UserService;
+
+import java.util.Calendar;
+
+@RestController
+@RequestMapping("/projects/{projectId}/tasks/{taskId}")
 public class US207CreateTaskReportRestController {
+
+    private UserService userService;
+    private TaskService taskService;
+    private ProjectService projectService;
+
+    @Autowired
+    public US207CreateTaskReportRestController(UserService userService, TaskService taskService, ProjectService projectService) {
+        this.taskService = taskService;
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
+    @RequestMapping(value = "/CreateReport" , method = RequestMethod.POST)
+    public ResponseEntity<?> createTaskReport (@RequestBody double timeToReport, @PathVariable String taskId, @PathVariable int projectId, @RequestHeader String userEmail){
+        ResponseEntity<?> result = new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+
+        Project project = projectService.getProjectById(projectId);
+
+        Task task = taskService.getTaskByTaskID(taskId);
+
+        User user = userService.getUserByEmail(userEmail);
+
+        Calendar dateOfReport = Calendar.getInstance();
+
+        if(task.createReport(task.getTaskCollaboratorByEmail(userEmail), dateOfReport, timeToReport)){
+            this.taskService.saveTask(task);
+            result = ResponseEntity.status(HttpStatus.OK).body("Report created!/n Info: " + timeToReport);
+            //result = new ResponseEntity<>(HttpStatus.OK);
+
+        }
+
+
+        return result;
+    }
+
+
+
+
 }
