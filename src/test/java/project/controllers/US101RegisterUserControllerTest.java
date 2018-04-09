d8c1a511c385749110c196fc266e271f952767f9
Daniel Pereira <1171460@isep.ipp.pt>
2018-04-09 15:55:26 +0100

    Added an assert to check a new user attribute

--- src/test/java/project/controllers/US101RegisterUserControllerTest.java
+++ src/test/java/project/controllers/US101RegisterUserControllerTest.java
@@ -83,6 +83,8 @@ public class US101RegisterUserControllerTest {
 		// verifies if the addNewUser method returns false when user email is valid
 		assertEquals(testUserRegistrationController.isUserEmailValid(user1.getEmail()), true);
 
+		assertFalse(user1.hasLoggedIn());
+
 	}
 
 	@Test
