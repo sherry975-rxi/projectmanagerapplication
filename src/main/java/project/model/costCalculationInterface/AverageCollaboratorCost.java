dcabf04076e661488a58ad0978e15f200b851cf7
AnaRosa <1171453@isep.ipp.pt>
2018-04-10 12:18:27 +0100

    #64 and #65 Implemented interface for project cost calculation method: based on first and last collaborator cost, and added comments

--- src/main/java/project/model/costCalculationInterface/AverageCollaboratorCost.java
+++ src/main/java/project/model/costCalculationInterface/AverageCollaboratorCost.java
@@ -5,6 +5,11 @@ import project.model.Report;
 
 import java.util.List;
 
+/**
+ * This interface will be used to calculate project cost based on the average of the first and  instance of a user,
+ * as a project collaborator
+ */
+
 public class AverageCollaboratorCost implements CostCalculationInterface {
     
     @Override
