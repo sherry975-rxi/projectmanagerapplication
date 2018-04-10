2376b444881cfe7ffa0b18805b1a98047d0ba420
Daniel Monteiro <1171461@isep.ipp.pt>
2018-04-10 16:53:30 +0100

    #62 add test to US105 Controller

--- src/test/java/project/controllers/US105CreatePasswordAndAuthenticationMechanismControllerTest.java
+++ src/test/java/project/controllers/US105CreatePasswordAndAuthenticationMechanismControllerTest.java
@@ -4,16 +4,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
-import project.model.Profile;
-import project.model.User;
+import project.model.*;
 import project.services.UserService;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -22,9 +31,18 @@ public class US105CreatePasswordAndAuthenticationMechanismControllerTest {
 
     @Autowired
     UserService userService;
-    @Autowired
+
+    @InjectMocks
     US105CreatePasswordAndAuthenticationMechanismController controller;
 
+    @Mock
+    SMSMessage smsMessage;
+
+    @Mock
+    SendEmail sendEmail;
+
+    @Mock
+    CodeGenerator codeGenerator;
 
     User user1;
 
@@ -37,6 +55,12 @@ public class US105CreatePasswordAndAuthenticationMechanismControllerTest {
 
         // set user as collaborator
         user1.setUserProfile(Profile.COLLABORATOR);
+
+        // set question to user
+        user1.setQuestion("1");
+
+        //set answer to user
+        user1.setAnswer("test answer");
 
         userService.updateUser(user1);
 
@@ -58,6 +82,49 @@ public class US105CreatePasswordAndAuthenticationMechanismControllerTest {
         controller.setUserPassword(user1, newPassword);
 
         assertTrue(user1.hasPassword());
+
+    }
+
+    @Test
+    public void questionAuthenticationTest() {
+
+        assertEquals("1",controller.questionAuthentication(user1));
+
+    }
+
+    @Test
+    public void isRightAnswerTest() {
+
+        assertTrue(controller.isRightAnswer("test answer", user1));
+
+    }
+
+    @Test
+    public void smsAuthenticationTest() {
+
+        controller.smsAuthentication("+351937429087");
+
+        verify(smsMessage, times(1)).sendMessage(anyString(), anyString());
+
+    }
+
+    @Test
+    public void emailAuthenticationTest() throws Exception{
+
+        controller.emailAuthentication("dsomonteiro@gmail.com");
+
+        verify(sendEmail, times(1)).sendmail(anyObject());
+
+    }
+
+    @Test
+    public void isCodeValidTest() {
+
+        when(codeGenerator.generateCode()).thenReturn("123456");
+
+        controller.emailAuthentication("dsomonteiro@gmail.com");
+
+        assertTrue(controller.isCodeValid("123456"));
 
     }
 }
