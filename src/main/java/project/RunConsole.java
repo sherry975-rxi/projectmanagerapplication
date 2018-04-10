c0f9c70463aabde1bc23e19d8bb2106efd7432e8
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-10 13:21:25 +0100

    #67 [SPRINT 7] [US901v2]  Refactored the calls of the getLoadProjectType and loadProject methods to getReader and readProjectFile

--- src/main/java/project/RunConsole.java
+++ src/main/java/project/RunConsole.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import project.model.Profile;
-import project.model.SendEmail;
 import project.model.User;
-import project.model.EmailMessage;
 import project.repository.UserRepository;
 import project.services.UserService;
 import project.ui.console.MainMenuUI;
@@ -70,9 +68,9 @@ public class RunConsole implements CommandLineRunner {
 		LoadUser loadUserDb = loadUserFactory.getLoadUserType("Utilizador_v00_Dt1.xml");
 		loadUserDb.loadUsers("Utilizador_v00_Dt1.xml");
 
-		LoadProject loadProjectDb = loadProjectFactory.getLoadProjectType("Projeto_v00_Dt1.xml");
+		LoadProject loadProjectDb = loadProjectFactory.getReader("Projeto_v00_Dt1.xml");
 
-    	loadProjectDb.loadProject("Projeto_v00_Dt1.xml");
+    	loadProjectDb.readProjectFile("Projeto_v00_Dt1.xml");
 
 
     	//Test sending an email
