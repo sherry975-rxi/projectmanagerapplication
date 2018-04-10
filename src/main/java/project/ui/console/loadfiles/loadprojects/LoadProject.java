f5e617efa6f6902ca5f6b371013102eb2a54787a
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-10 13:20:04 +0100

    #67 [SPRINT 7] [US901v2]  Deleted the acess modifier of the method readProjectFile in the LoadProject interface.

--- src/main/java/project/ui/console/loadfiles/loadprojects/LoadProject.java
+++ src/main/java/project/ui/console/loadfiles/loadprojects/LoadProject.java
@@ -20,5 +20,5 @@ public interface LoadProject {
      * @throws DOMException
      * @throws ParseException
      */
-    public void loadProject(String filePath) throws ParserConfigurationException, SAXException, IOException, DOMException, ParseException;
+     void readProjectFile(String filePath) throws ParserConfigurationException, SAXException, IOException, DOMException, ParseException;
 }
