2ee62f0692e338ecd0a9c8bc702cdf069be5f0ee
João Leite <1161661@isep.ipp.pt>
2018-04-11 11:23:18 +0100

    #62 Instantiated MessageSender and EmailSender in SendCodeFactory

--- src/main/java/project/model/sendcode/MessageSender.java
+++ src/main/java/project/model/sendcode/MessageSender.java
@@ -6,5 +6,5 @@ import java.io.IOException;
 
 public interface MessageSender {
 
-    void codeSender (String codeSender, String receipientPhoneNum, String email, String msg) throws IOException, MessagingException;
+    void codeSender (String receipientPhoneNum, String email, String msg) throws IOException, MessagingException;
 }
