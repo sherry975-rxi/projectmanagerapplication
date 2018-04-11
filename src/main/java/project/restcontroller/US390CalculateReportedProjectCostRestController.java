2d4edfd9312b75ded154ed1a52fb6113c7826998
Inês Pinto <1171476@isep.ipp.pt>
2018-04-11 12:09:32 +0100

    refs #72 and #93 Create getProjectCost method.

--- src/main/java/project/restcontroller/US390CalculateReportedProjectCostRestController.java
+++ src/main/java/project/restcontroller/US390CalculateReportedProjectCostRestController.java
@@ -3,11 +3,9 @@ package project.restcontroller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.model.Project;
-import project.model.Task;
 import project.services.ProjectService;
 import project.services.TaskService;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/projects/{projectId}")
@@ -27,11 +25,24 @@ public class US390CalculateReportedProjectCostRestController {
         return ResponseEntity.ok(project);
     }
 
+    /**
+     * This controller's method uses PUT to set the calculation method of project's cost. The method of calculation is defined by a int (FIRST_COLLABORATOR = 1;
+     * LAST_COLLABORATOR = 2; FIRST_LAST_COLLABORATOR = 3; AVERAGE_COLLABORATOR = 4).
+     */
     @RequestMapping(value = "/calculationmethod", method = RequestMethod.PUT)
     public ResponseEntity<Project> setCalculationMethod(@RequestBody Project project, @PathVariable int projectId) {
         Project myProject = this.projectService.getProjectById(projectId);
         myProject.setCalculationMethod(project.getCalculationMethod());
         projectService.addProjectToProjectContainer(myProject);
         return ResponseEntity.ok().body(myProject);
+    }
+    /**
+     * This controller's method uses GET to get the cost of the project through the calculation method defined previously.
+     */
+    @RequestMapping(value = "/cost", method = RequestMethod.GET)
+    public ResponseEntity<String> getProjectCost(@PathVariable int projectId) {
+        Project project = this.projectService.getProjectById(projectId);
+        taskService.getTotalCostReportedToProjectUntilNow(project);
+        return  ResponseEntity.ok().body("Project Cost: " + Double.toString(taskService.getTotalCostReportedToProjectUntilNow(project)));
     }
 }
