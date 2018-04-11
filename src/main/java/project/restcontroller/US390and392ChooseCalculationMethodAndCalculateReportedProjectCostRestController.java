926de33b2d5973528ac5eb095e7cf7d7ccf2862c
Inês Pinto <1171476@isep.ipp.pt>
2018-04-11 18:01:29 +0100

    refs #72 #93 updates in restController; add tests.

--- src/main/java/project/restcontroller/US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestController.java
+++ src/main/java/project/restcontroller/US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestController.java
@@ -9,12 +9,12 @@ import project.services.TaskService;
 
 @RestController
 @RequestMapping("/projects/{projectId}")
-public class US390CalculateReportedProjectCostRestController {
+public class US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestController {
 
     private final ProjectService projectService;
     private final TaskService taskService;
 
-    public US390CalculateReportedProjectCostRestController(TaskService taskService, ProjectService projectsService) {
+    public US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestController(TaskService taskService, ProjectService projectsService) {
         this.taskService = taskService;
         this.projectService = projectsService;
     }
@@ -29,8 +29,8 @@ public class US390CalculateReportedProjectCostRestController {
      * This controller's method uses PUT to set the calculation method of project's cost. The method of calculation is defined by a int (FIRST_COLLABORATOR = 1;
      * LAST_COLLABORATOR = 2; FIRST_LAST_COLLABORATOR = 3; AVERAGE_COLLABORATOR = 4).
      */
-    @RequestMapping(value = "/calculationmethod", method = RequestMethod.PUT)
-    public ResponseEntity<Project> setCalculationMethod(@RequestBody Project project, @PathVariable int projectId) {
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Project> updateCalculationMethod(@RequestBody Project project, @PathVariable int projectId) {
         Project myProject = this.projectService.getProjectById(projectId);
         myProject.setCalculationMethod(project.getCalculationMethod());
         projectService.updateProject(myProject);
@@ -44,7 +44,6 @@ public class US390CalculateReportedProjectCostRestController {
     @RequestMapping(value = "/cost", method = RequestMethod.GET)
     public ResponseEntity<String> getProjectCost(@PathVariable int projectId) {
         Project project = this.projectService.getProjectById(projectId);
-        taskService.getTotalCostReportedToProjectUntilNow(project);
         return  ResponseEntity.ok().body("Project Cost: " + Double.toString(taskService.getTotalCostReportedToProjectUntilNow(project)));
     }
 }
--- src/test/java/project/restControllers/US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestControllerTest.java
+++ src/test/java/project/restControllers/US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestControllerTest.java
@@ -1,4 +1,90 @@
 package project.restControllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import project.model.Project;
+import project.model.ProjectCollaborator;
+import project.model.User;
+import project.restcontroller.US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestController;
+import project.services.ProjectService;
+import project.services.TaskService;
+
+import javax.print.attribute.standard.Media;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+@RunWith(MockitoJUnitRunner.class)
 public class US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestControllerTest {
+    @Mock
+    private TaskService taskServiceMock;
+    @Mock
+    private ProjectService projectServiceMock;
+    @Mock
+    private Project projectMock;
+
+    @InjectMocks
+    private US390and392ChooseCalculationMethodAndCalculateReportedProjectCostRestController victim;
+
+    private MockMvc mvc;
+    private Integer projectId;
+    private JacksonTester<Project> jacksonProject;
+    private User userRui;
+    private ProjectCollaborator projectCollaborator;
+
+    @Before
+    public void setup(){
+        JacksonTester.initFields(this, new ObjectMapper());
+        mvc = MockMvcBuilders.standaloneSetup(victim).build();
+        userRui = new User("Rui", "rui@gmail.com", "02", "Simples colaborador", "638192945");
+        projectId = 1;
+        this.projectCollaborator = new ProjectCollaborator(userRui,  2);
+        projectCollaborator.setProject(projectMock);
+        this.projectMock = new Project("Project", "description", userRui);
+    }
+
+    @Test
+    public void testGetProjectById() throws Exception {
+        //given
+        when(projectServiceMock.getProjectById(any(Integer.class))).thenReturn(projectMock);
+
+        //when
+        MockHttpServletResponse response = mvc.perform(get("/projects/" + projectId).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        //then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(jacksonProject.write(projectMock).getJson(), response.getContentAsString());
+        verify(projectServiceMock, times(1)).getProjectById(projectId);
+    }
+
+    @Test
+    public void testGetProjectCost() throws Exception {
+        //given
+        when(projectServiceMock.getProjectById(any(Integer.class))).thenReturn(projectMock);
+        when(taskServiceMock.getTotalCostReportedToProjectUntilNow(projectMock)).thenReturn(7.0);
+
+        //when
+        MockHttpServletResponse response = mvc.perform(get("/projects/" + projectId + "/cost").accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        //then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("Project Cost: 7.0", response.getContentAsString());
+        verify(projectServiceMock, times(1)).getProjectById(projectId);
+    }
 }
