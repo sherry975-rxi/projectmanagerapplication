b64ce9f77f5fc8d98bdf2ae6d933fb0a8b991b4d
Fábio  Brás <1160058@isep.ipp.pt>
2018-04-12 14:51:08 +0100

    added the setUp() for the test and tested if the controller was being initialized.

--- src/test/java/project/restControllers/US207CreateTaskReportRestControllerTest.java
+++ src/test/java/project/restControllers/US207CreateTaskReportRestControllerTest.java
@@ -1,3 +1,153 @@
+package project.restControllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import project.model.*;
+import project.repository.ProjCollabRepository;
+import project.repository.ProjectsRepository;
+import project.repository.TaskRepository;
+import project.repository.UserRepository;
+import project.restcontroller.US204AssignTaskRequestRestController;
+import project.restcontroller.US207CreateTaskReportRestController;
+import project.services.ProjectService;
+import project.services.TaskService;
+import project.services.UserService;
+
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@RunWith(MockitoJUnitRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
 public class US207CreateTaskReportRestControllerTest {
 
-}
+    private MockMvc mockMvc;
+
+    @Mock
+    TaskRepository taskRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    ProjectsRepository projectsRepository;
+
+    @Mock
+    ProjCollabRepository projCollabRepository;
+
+    //@Mock
+    @InjectMocks
+    UserService userService;
+    //@Mock
+    @InjectMocks
+    ProjectService projectService;
+    //@Mock
+    @InjectMocks
+    TaskService taskService;
+
+    //@InjectMocks
+    private US207CreateTaskReportRestController controller;
+
+    private User userPM;
+    private User userTwo;
+    private String userTwoEmail;
+    private int userTwoId;
+    private Project projectOne;
+    private Integer projectId;
+    private Task taskOne;
+    private String taskIdOne;
+    private Task taskTwo;
+    private String taskIdTwo;
+    private ProjectCollaborator projCollabTwo;
+    private Report report;
+    private Report report2;
+
+    @Before
+    public void setUp() throws Exception {
+        //initMocks(this);
+
+        // create userPM
+        userPM = userService.createUser("Ana", "ana@gmail.com", "01", "collaborator", "221238442", "Rua Porto",
+                "4480", "Porto", "Porto", "Portugal");
+
+        // create userTwo
+        userTwo = userService.createUser("Joao", "joao@gmail.com", "02", "collaborator", "221238447", "Rua Porto",
+                "5480", "Porto", "Porto", "Portugal");
+        userTwoEmail = "joao@gmail.com";
+        userTwoId = 02;
+
+        // create a project with userPM as manager
+        projectOne = projectService.createProject("Restful", "Implement API Rest", userPM);
+        projectId = 1;
+
+        // create 2 tasks
+        taskOne = taskService.createTask("Rest Controller", projectOne);
+        taskIdOne = "1.1";
+
+        taskTwo = taskService.createTask("Rest Controller Test", projectOne);
+        taskIdTwo = "1.2";
+
+        // add users to projectOne creating a project collaborator for each
+        projCollabTwo = projectService.createProjectCollaborator(userTwo, projectOne, 20);
+
+        //Create Task Collaborator
+        taskOne.addProjectCollaboratorToTask(projCollabTwo);
+        taskService.saveTask(taskOne);
+
+        Calendar date = Calendar.getInstance();
+
+        taskOne.createReport(taskOne.getTaskCollaboratorByEmail(userTwoEmail), date, 20.0);
+        taskOne.createReport(taskOne.getTaskCollaboratorByEmail(userTwoEmail),date,35);
+        taskService.saveTask(taskOne);
+
+
+        // initialing the rest controller
+        controller = new US207CreateTaskReportRestController(userService, taskService, projectService);
+
+        // building mockMVC
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        //creating an expected list of project collaborators from projectOne
+        List<ProjectCollaborator> projCollabsList = new ArrayList<>();
+        projCollabsList.add(projCollabTwo);
+
+
+        //creating an expected list of project collaborators of userTwo
+        List<ProjectCollaborator> userTwoProjCollab = new ArrayList<>();
+        userTwoProjCollab.add(projCollabTwo);
+
+        Mockito.when(userRepository.findByEmail(userTwoEmail)).thenReturn(Optional.of(userTwo));
+        Mockito.when(userRepository.findById(userTwoId)).thenReturn(userTwo);
+        Mockito.when(projectsRepository.findById(projectId)).thenReturn(Optional.of(projectOne));
+        Mockito.when(projCollabRepository.findAllByCollaborator(userTwo)).thenReturn(userTwoProjCollab);
+        Mockito.when(projCollabRepository.findAllByProject(projectOne)).thenReturn(projCollabsList);
+        Mockito.when(taskRepository.findByTaskID(taskIdOne)).thenReturn(Optional.of(taskOne));
+    }
+
+    @Test
+    public void controllerInitializedCorrectly() {
+        assertNotNull(controller);
+    }
+
