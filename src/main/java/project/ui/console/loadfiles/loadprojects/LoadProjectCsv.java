20aa95c4fed75ca14e0d8699c6da353cc8d459ff
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-15 16:30:15 +0100

    #codeSmellFix Removed unused imports and unused exception that was being thrown.

--- src/main/java/project/ui/console/loadfiles/loadprojects/LoadProjectCsv.java
+++ src/main/java/project/ui/console/loadfiles/loadprojects/LoadProjectCsv.java
@@ -1,12 +1,6 @@
 package project.ui.console.loadfiles.loadprojects;
 
 import org.springframework.stereotype.Service;
-import org.w3c.dom.DOMException;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.text.ParseException;
 
 @Service
 public class LoadProjectCsv implements LoadProject {
--- src/main/java/project/ui/console/loadfiles/loadprojects/LoadProjectXml.java
+++ src/main/java/project/ui/console/loadfiles/loadprojects/LoadProjectXml.java
@@ -1,6 +1,5 @@
 package project.ui.console.loadfiles.loadprojects;
 
-import org.w3c.dom.DOMException;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -9,5 +8,5 @@ import java.text.ParseException;
 
 public interface LoadProjectXml extends LoadProject {
 
-    void readProjectFile(String filePath) throws ParserConfigurationException, SAXException, IOException, DOMException, ParseException;
+    void readProjectFile(String filePath) throws ParserConfigurationException, SAXException, IOException, ParseException;
 }
