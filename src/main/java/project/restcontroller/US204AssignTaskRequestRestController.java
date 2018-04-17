da8ef6d9b4a84314afeff575073debef7eb0f249
Rita Machado <1171471@isep.ipp.pt>
2018-04-17 23:20:24 +0100

    refs 98 codeSmellFix Remove unused imports

--- src/main/java/project/restcontroller/US204AssignTaskRequestRestController.java
+++ src/main/java/project/restcontroller/US204AssignTaskRequestRestController.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import project.model.Project;
-import project.model.ProjectCollaborator;
+
 import project.model.Task;
 import project.model.User;
 import project.services.ProjectService;
