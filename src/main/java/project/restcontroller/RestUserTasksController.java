0b80810a7aef1ffb207752c8a352e8436a9e78bc
Daniel Pereira <1171460@isep.ipp.pt>
2018-04-18 11:51:19 +0100

    Added Test classes for new REST controllers. Refactored said controllers to add a new extends for "ResourceSupport"

--- src/main/java/project/restcontroller/RestUserTasksController.java
+++ src/main/java/project/restcontroller/RestUserTasksController.java
@@ -1,9 +1,10 @@
 package project.restcontroller;
 
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users/{userid}/tasks/")
-public class RestUserTasksController {
+public class RestUserTasksController extends ResourceSupport {
 }
