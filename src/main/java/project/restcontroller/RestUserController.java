ff735929764a729ee11158b40c3f7fb40ba3867b
Fábio  Brás <1160058@isep.ipp.pt>
2018-04-18 12:22:47 +0100

    Implemented a restController method to change the user profiles Refs #104 & #105

--- src/main/java/project/restcontroller/RestUserController.java
+++ src/main/java/project/restcontroller/RestUserController.java
@@ -1,9 +1,41 @@
 package project.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.model.Profile;
+import project.model.User;
+import project.services.ProjectService;
+import project.services.TaskService;
+import project.services.UserService;
 
 @RestController
 @RequestMapping("users/")
 public class RestUserController {
+
+    private UserService userService;
+
+    @Autowired
+    public RestUserController (UserService userService, TaskService taskService, ProjectService projectService) {
+        this.userService = userService;
+
+    }
+
+    @RequestMapping(value = "/profiles" , method = RequestMethod.POST)
+    public ResponseEntity<?> changeUserProfile (@RequestBody User updatedProfile) {
+        ResponseEntity<?> result = new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        Profile profileChange = updatedProfile.getUserProfile();
+
+        User userToChange = userService.getUserByEmail(updatedProfile.getEmail());
+        if(userToChange != null){
+            userToChange.setUserProfile(profileChange);
+            result = new ResponseEntity<>("User "+userToChange.getName()+"\nProfile changed sucessfully",HttpStatus.OK);
+        }
+        return result;
+    }
+
+
+
 }
