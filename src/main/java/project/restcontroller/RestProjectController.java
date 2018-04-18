89e413bd3e6b3ec79c877c2bd612671dbe71f677
Daniel Monteiro <1171461@isep.ipp.pt>
2018-04-18 14:29:29 +0100

    Refs #111 fix method uri, and save update on database

--- src/main/java/project/restcontroller/RestProjectController.java
+++ src/main/java/project/restcontroller/RestProjectController.java
@@ -1,8 +1,5 @@
 package project.restcontroller;
 
-import org.springframework.hateoas.ResourceSupport;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +10,8 @@ import project.services.ProjectService;
 import project.services.UserService;
 
 @RestController
-@RequestMapping("projects/")
-public class RestProjectController extends ResourceSupport {
+@RequestMapping("projects")
+public class RestProjectController {
 
     private final ProjectService projectService;
     private final UserService userService;
@@ -33,18 +30,18 @@ public class RestProjectController extends ResourceSupport {
      * @param projectId
      * @return
      */
-    @RequestMapping(value = "/{projectId}" , method = RequestMethod.PUT)
+    @RequestMapping(value = "/{projectId}/" , method = RequestMethod.PUT)
     public ResponseEntity<String> changeProjectManager(@RequestBody Project project, @PathVariable int projectId){
 
         Project projectToUpdate = projectService.getProjectById(projectId);
 
-        String userEmail = project.getProjectManager().getEmail();
+        User user = userService.getUserByEmail(project.getProjectManager().getEmail());
 
-        User projectManager = userService.getUserByEmail(userEmail);
+        if(user != null) {
 
-        if(!(projectManager == null)) {
+            projectToUpdate.setProjectManager(user);
 
-            projectToUpdate.setProjectManager(projectManager);
+            projectService.updateProject(projectToUpdate);
 
             return new ResponseEntity<>("Project manager successfully changed!",HttpStatus.OK);
 
