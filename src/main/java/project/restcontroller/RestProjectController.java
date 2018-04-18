79d127a62be9ada0cb4b32db129cfebc25ee0b5b
Daniel Monteiro <1171461@isep.ipp.pt>
2018-04-18 12:32:20 +0100

    Refs# 111 create method in RestProjectController to change de project manager from a given project

--- src/main/java/project/restcontroller/RestProjectController.java
+++ src/main/java/project/restcontroller/RestProjectController.java
@@ -1,9 +1,52 @@
 package project.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.model.Project;
+import project.model.User;
+import project.services.ProjectService;
+import project.services.UserService;
 
 @RestController
 @RequestMapping("projects/")
 public class RestProjectController {
+
+    private final ProjectService projectService;
+    private final UserService userService;
+
+    @Autowired
+    public RestProjectController(ProjectService projectService, UserService userService){
+
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
+    /**
+     * This method change the project manager of a given project
+     *
+     * @param project
+     * @param projectId
+     * @return
+     */
+    @RequestMapping(value = "/{projectId}" , method = RequestMethod.PUT)
+    public ResponseEntity<String> changeProjectManager(@RequestBody Project project, @PathVariable int projectId){
+
+        Project projectToUpdate = projectService.getProjectById(projectId);
+
+        String userEmail = project.getProjectManager().getEmail();
+
+        User projectManager = userService.getUserByEmail(userEmail);
+
+        if(!(projectManager == null)) {
+
+            projectToUpdate.setProjectManager(projectManager);
+
+            return new ResponseEntity<>("Project manager successfully changed!",HttpStatus.OK);
+
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
