b940362ba4ae823f06ae635a748f71ccaa4e8f80
Inês Pinto <you@example.com>
2018-04-18 13:25:12 +0100

    refs #129 add getProjectDetails method and unit test

--- src/main/java/project/restcontroller/RestProjectController.java
+++ src/main/java/project/restcontroller/RestProjectController.java
@@ -1,10 +1,23 @@
 package project.restcontroller;
 
 import org.springframework.hateoas.ResourceSupport;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.model.Project;
+import project.services.ProjectService;
 
 @RestController
 @RequestMapping("projects/")
 public class RestProjectController extends ResourceSupport {
+    private final ProjectService projectService;
+
+    public RestProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @RequestMapping(value= "{projectId}", method = RequestMethod.GET)
+    public ResponseEntity<Project> getProjectDetails(@PathVariable int projectId) {
+        Project project = this.projectService.getProjectById(projectId);
+        return ResponseEntity.ok(project);
+    }
 }
