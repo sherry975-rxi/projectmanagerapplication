c778b3a29aa8a7b6c288695b39fb53ecc568c45e
João Leite <1161661@isep.ipp.pt>
2018-04-18 16:31:33 +0100

    ref 109 - created method in RestProjectController to return the active projects

--- src/main/java/project/restcontroller/RestProjectController.java
+++ src/main/java/project/restcontroller/RestProjectController.java
@@ -1,9 +1,47 @@
 package project.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import project.model.Project;
+import project.model.Task;
+import project.services.ProjectService;
+import project.services.TaskService;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 
 @RestController
 @RequestMapping("projects/")
 public class RestProjectController {
+
+    ProjectService projectService;
+
+    @Autowired
+    public RestProjectController(ProjectService projServ) {
+        this.projectService = projServ;
+    }
+
+
+    @RequestMapping(value = "/active", method = RequestMethod.GET)
+    public ResponseEntity<List<Project>> getActiveProjects() {
+
+        List<Project> activeProjects = this.projectService.getActiveProjects();
+        for (Project project : activeProjects) {
+            Link selfLink = linkTo(RestProjectController.class).slash(project.getProjectId()).withSelfRel();
+            project.add(selfLink);
+
+
+        }
+
+        return ResponseEntity.ok().body(activeProjects);
+
+    }
 }
