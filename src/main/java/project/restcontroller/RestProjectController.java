36068aad61794dc7136b21df99fd98cd69fd498d
Rita Machado <1171471@isep.ipp.pt>
2018-04-19 17:19:40 +0100

    ref #108 [SPRINT3S2] Added condition to save EffortUnit and Budget along with Project creation

--- src/main/java/project/restcontroller/RestProjectController.java
+++ src/main/java/project/restcontroller/RestProjectController.java
@@ -12,7 +12,7 @@ import project.services.UserService;
 
 
 @RestController
-@RequestMapping("projects/")
+@RequestMapping("/projects/")
 public class RestProjectController  {
 
     private final ProjectService projectService;
@@ -24,27 +24,38 @@ public class RestProjectController  {
         this.userService = userService;
     }
 
+
     @RequestMapping(value= "{projectId}", method = RequestMethod.GET)
     public ResponseEntity<Project> getProjectDetails(@PathVariable int projectId) {
         Project project = this.projectService.getProjectById(projectId);
         return ResponseEntity.ok(project);
     }
 
-    @RequestMapping(value = "" , method = RequestMethod.POST)
-    public ResponseEntity<Project> createProject (@RequestBody Project projectDTO){
 
-        User projectManager= userService.getUserByEmail(projectDTO.getProjectManager().getEmail());
+    /**
+     * Creates a Project with the parameters Name, Description and Project Manager if the response body only has this
+     * info. In case the Response Body has an EffortUnit and/or a Budget, the project will be created with the all the
+     * Response body information.
+     */
+    @RequestMapping(value = "" , method = RequestMethod.POST)
+    public ResponseEntity<Project> createProject(@RequestBody Project projectDTO) {
+
+        User projectManager = userService.getUserByEmail(projectDTO.getProjectManager().getEmail());
 
         Project proj = projectService.createProject(projectDTO.getName(), projectDTO.getDescription(), projectManager);
 
+        if (projectDTO.getEffortUnit() != null){
+                proj.setEffortUnit(projectDTO.getEffortUnit());
+            }
 
-            this.projectService.addProjectToProjectContainer(proj);
-
-
-            return ResponseEntity.ok().body(proj);
+        if (projectDTO.getBudget() != 0) {
+            proj.setProjectBudget(projectDTO.getBudget());
         }
 
+        this.projectService.addProjectToProjectContainer(proj);
 
+        return ResponseEntity.ok().body(proj);
+    }
     }
     
 
