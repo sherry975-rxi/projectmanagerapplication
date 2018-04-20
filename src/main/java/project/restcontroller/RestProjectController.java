d0bfda3b84670d31cfb33a37bd09d969e74828fc
Daniel Monteiro <1171461@isep.ipp.pt>
2018-04-20 14:21:31 +0100

    Refs #111 add unit test to updateProject in projectService 	 * Given 	 * a project with only the information we want to update 	 * When 	 * update une given project 	 * Then 	 * the information given for update will replace the respective information in the given project.

--- src/main/java/project/restcontroller/RestProjectController.java
+++ src/main/java/project/restcontroller/RestProjectController.java
@@ -1,9 +1,7 @@
 package project.restcontroller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.model.Project;
