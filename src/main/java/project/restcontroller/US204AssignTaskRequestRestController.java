a56299b8ff3694344c15a85ac32a23e423f13338
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-04-20 16:40:34 +0100

    refs #123 US 204 Rest Controller: - updated uri - added rel links - added uri builder - added location in headers

--- src/main/java/project/restcontroller/US204AssignTaskRequestRestController.java
+++ src/main/java/project/restcontroller/US204AssignTaskRequestRestController.java
@@ -1,19 +1,31 @@
 package project.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import project.model.*;
 import project.services.ProjectService;
 import project.services.TaskService;
 import project.services.UserService;
 
+import javax.persistence.criteria.Order;
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 
 /**
@@ -34,18 +46,18 @@ public class US204AssignTaskRequestRestController {
         this.userService = userService;
         this.projectService = projectService;
     }
+
     /**
      * This method allows the collaborator to get a
      * list of pending requests from a specific task.
      *
-     * @param taskId
-     *          Task id associated to the task to be made the request
-     *
+     * @param taskId Task id associated to the task to be made the request
      * @return ResponseEntity
      */
-    @RequestMapping(value = "/requests" , method = RequestMethod.GET)
-    public ResponseEntity<List<TaskTeamRequest>> getAllRequests (@PathVariable String taskId) {
+    @RequestMapping(value = "/requests", method = RequestMethod.GET)
+    public ResponseEntity<List<TaskTeamRequest>> getAllRequests(@PathVariable String taskId/*, @PathVariable int projectId*/) {
 
+//        Project project = projectService.getProjectById(projectId);
 
         Task task = taskService.getTaskByTaskID(taskId);
 
@@ -57,23 +69,21 @@ public class US204AssignTaskRequestRestController {
      * This method allows the collaborator to choose between getting a
      * list of assignment or removal pending requests from a specific task.
      *
-     * @param taskId
-     *          Task id associated to the task to be made the request
-     *
+     * @param taskId Task id associated to the task to be made the request
      * @return ResponseEntity
      */
-    @RequestMapping(value = "/requests/{reqType}" , method = RequestMethod.GET)
-    public ResponseEntity<List<TaskTeamRequest>> getAllFilteredRequests (@PathVariable String taskId, @PathVariable String reqType) {
+    @RequestMapping(value = "/requests/list/{reqType}", method = RequestMethod.GET)
+    public ResponseEntity<List<TaskTeamRequest>> getAllFilteredRequests(@PathVariable String taskId, @PathVariable String reqType) {
 
 
         Task task = taskService.getTaskByTaskID(taskId);
 
 
-        if ("assignment".equals(reqType)){
+        if ("assignment".equals(reqType)) {
 
             return new ResponseEntity<>(task.getPendingTaskAssignmentRequests(), HttpStatus.OK);
-        }
-        else if ("removal".equals(reqType)){
+
+        } else if ("removal".equals(reqType)) {
 
             return ResponseEntity.ok(task.getPendingTaskRemovalRequests());
         }
@@ -82,51 +92,118 @@ public class US204AssignTaskRequestRestController {
     }
 
     /**
-     * This method allows the collaborator to create a request of assignment to a specific task.
+     * This method returns a specific request.
      *
+     * @param requestId Request id associated to the request
      * @param taskId    Task id associated to the task to be made the request
-     * @param projectId Project id associated to the project where the task belongs
-     * @param userDTO   User related to the collaborator that wants to make the request.
      * @return ResponseEntity
      */
-    @RequestMapping(value = "/requests/assignmentRequest", method = RequestMethod.POST)
-    public ResponseEntity<TaskTeamRequest> createAssignmentRequest(@PathVariable String taskId, @PathVariable int projectId, @RequestBody User userDTO) {
+    @RequestMapping(value = "/requests/{requestId}", method = RequestMethod.GET)
+    public ResponseEntity<TaskTeamRequest> getRequestDetails(@PathVariable int requestId, @PathVariable String taskId, @PathVariable int projectId) {
 
         Project project = projectService.getProjectById(projectId);
 
         Task task = taskService.getTaskByTaskID(taskId);
 
-        String userDTOEmail = userDTO.getEmail();
+        for (TaskTeamRequest other : task.getPendingTaskTeamRequests()) {
+            if (other.getDbId() == requestId) {
 
-        User user = userService.getUserByEmail(userDTOEmail);
+                Link selfLink = linkTo(methodOn(US204AssignTaskRequestRestController.class).getAllRequests(taskId)).slash(other.getDbId()).withSelfRel();
+                other.add(selfLink);
 
-        Optional<User> userOptional = Optional.of(user);
-
-        if (userOptional.isPresent()) {
-            //userOptional.get();
-
-
-            ProjectCollaborator projCollab = projectService.findActiveProjectCollaborator(user, project);
-
-            if (projCollab != null && !task.isProjectCollaboratorActiveInTaskTeam(projCollab) && !task.isAssignmentRequestAlreadyCreated(projCollab)) {
-
-                task.createTaskAssignmentRequest(projCollab);
-                taskService.saveTask(task);
-
-                for (TaskTeamRequest other : task.getPendingTaskTeamRequests()) {
-                    if (other.getTask().equals(task) && other.getProjCollab().equals(projCollab) && other.isAssignmentRequest()) {
-                        TaskTeamRequest requestCreated = other;
-
-                        return new ResponseEntity<>(requestCreated, HttpStatus.CREATED);
-                    }
-                }
-
+                return new ResponseEntity<>(other, HttpStatus.OK);
             }
-            // user, project and task exist but user is not in this project
-            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
-        }
 
-        // either user, project or task don't exist
-        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
-}
+
+        /**
+         * This method allows the collaborator to create a request of assignment to a specific task.
+         *
+         * @param taskId    Task id associated to the task to be made the request
+         * @param projectId Project id associated to the project where the task belongs
+         * @param userDTO   User related to the collaborator that wants to make the request.
+         * @return ResponseEntity
+         */
+        @RequestMapping(value = "/requests/assignmentRequest", method = RequestMethod.POST)
+        public ResponseEntity<TaskTeamRequest> createAssignmentRequest (@PathVariable String taskId,
+        @PathVariable int projectId, @RequestBody User userDTO){
+
+            Project project = projectService.getProjectById(projectId);
+
+            Task task = taskService.getTaskByTaskID(taskId);
+
+            String userDTOEmail = userDTO.getEmail();
+
+            User user = userService.getUserByEmail(userDTOEmail);
+
+            Optional<User> userOptional = Optional.of(user);
+
+            if (userOptional.isPresent()) {
+                //userOptional.get();
+
+
+                ProjectCollaborator projCollab = projectService.findActiveProjectCollaborator(user, project);
+
+                if (projCollab != null && !task.isProjectCollaboratorActiveInTaskTeam(projCollab) && !task.isAssignmentRequestAlreadyCreated(projCollab)) {
+
+                    task.createTaskAssignmentRequest(projCollab);
+                    taskService.saveTask(task);
+
+                    for (TaskTeamRequest request : task.getPendingTaskTeamRequests()) {
+                        if (request.getTask().equals(task) && request.getProjCollab().equals(projCollab) && request.isAssignmentRequest()) {
+
+                            Link order = linkTo(methodOn(getClass()).getRequestDetails(request.getDbId(), taskId, projectId)).withRel("Request details");
+                            request.add(order);
+                            //Link ordersLink = linkTo(getRequestDetails(request.getDbId(), taskId)).withRel("Request Details");
+//                            Link methodLinkBuilder =
+//                                    methodOn(US204AssignTaskRequestRestController.class).getAllRequests(taskId);
+//                            Link ordersLink = linkTo(methodLinkBuilder).withRel("allOrders");
+
+                            //Link reference = linkTo(getRequestDetails(request.getDbId(), taskId)).withRel("Request Details");
+//                            Link reference = linkTo(US204AssignTaskRequestRestController.class).slash(request.getDbId()).withRel("Request Details");
+//
+////
+//                            request.add(reference);
+                            //Link referenceTwo = linkTo(methodOn( getRequestDetails(request.getDbId(), taskId)).toUri();
+                            //URI result = MvcUriComponentsBuilder.fromController(getClass()).path("/requests/{requestId}").buildAndExpand(request.getDbId()).toUri();
+                            //URI result = MvcUriComponentsBuilder.fromMethodCall(getRequestDetails(request.getDbId(), taskId, projectId)).build().toUri();
+//                                    fromMethod(getClass(), getRequestDetails(request.getDbId(),taskId)).buildAndExpand()
+//                                    fromController(getClass()).path("/requests/{requestId}").buildAndExpand(request.getDbId()).toUri();
+
+                            UriComponents ucbT =linkTo(methodOn(getClass()).getRequestDetails(request.getDbId(), taskId, projectId)).toUriComponentsBuilder().build();
+
+                            //UriComponents ucb = ServletUriComponentsBuilder.fromHttpUrl(order.toString()).build();
+                            URI teste = ucbT.toUri();
+
+
+                            /*URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/requests/{id}").buildAndExpand(request.getDbId()).toUri();
+                            URI zzzz = ServletUriComponentsBuilder.fromRequest(methodOn(getClass()).getRequestDetails(request.getDbId(), taskId, projectId);*/
+
+
+                            //URI locationZ = MvcUriComponentsBuilder.fromMethodName(getClass(), "getRequestDetails", request.getDbId(), taskId, projectId).path("/{id}").buildAndExpand(request.getDbId()).toUri();
+
+                            //URI locationTwo = linkTo(getRequestDetails(request.getDbId(), taskId)).toUri();
+                            HttpHeaders headers = new HttpHeaders();
+                            headers.setLocation(teste);
+
+
+                            //URI location = ServletUriComponentsBuilder.fromRequestUri(getAllRequests(taskId))
+
+                            return new ResponseEntity<>(request, headers, HttpStatus.CREATED);
+                            //return ResponseEntity.created(locationZ).body(request);
+                        }
+                    }
+
+                }
+                // user, project and task exist but user is not in this project
+                return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+            }
+
+            // either user, project or task don't exist
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+
