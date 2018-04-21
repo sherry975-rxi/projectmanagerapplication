5477aab332101aa8d4c4d9cf8959970f04dc7382
Rita Machado <1171471@isep.ipp.pt>
2018-04-22 00:45:45 +0100

    ref #124 [SPRINT3S2] Implementation of method createReport in RestReportController

--- src/main/java/project/restcontroller/RestReportController.java
+++ src/main/java/project/restcontroller/RestReportController.java
@@ -1,9 +1,54 @@
 package project.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.model.Project;
+import project.model.Report;
+import project.model.Task;
+import project.model.TaskCollaborator;
+import project.services.ProjectService;
+import project.services.TaskService;
+import project.services.UserService;
+
+import java.util.Calendar;
 
 @RestController
 @RequestMapping("projects/{projid}/tasks/{taskid}/reports/")
 public class RestReportController {
+
+
+    private final ProjectService projectService;
+    private final TaskService taskService;
+    private final UserService userService;
+
+    @Autowired
+    public RestReportController(ProjectService projectService, TaskService taskService, UserService userService) {
+        this.projectService = projectService;
+        this.taskService = taskService;
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Report> createReport(@RequestBody Report reportDto, @PathVariable String taskID, @PathVariable int projectID, @PathVariable String email ){
+
+        Project project = projectService.getProjectById(projectID);
+        Task task = taskService.getTaskByTaskID(taskID);
+        //User user = userService.getUserByEmail(email);
+        TaskCollaborator taskCollaborator = task.getTaskCollaboratorByEmail(email);
+
+        double reportedTime;
+        Calendar firstDateOfReport;
+        firstDateOfReport = Calendar.getInstance();
+
+        if (taskCollaborator == null){
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        } else {
+            task.createReport(reportDto.getTaskCollaborator(), reportDto.getFirstDateOfReport(), reportDto.getCost());
+            taskService.saveTask(task);
+            return ResponseEntity.ok().body(reportDto);
+        }
+
+    }
 }
