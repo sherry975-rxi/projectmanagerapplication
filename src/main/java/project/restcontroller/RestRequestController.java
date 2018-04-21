65bb1eaa90c993298c0cebfbd1a108d6b39393df
Rita Machado <1171471@isep.ipp.pt>
2018-04-21 12:43:44 +0100

    ref #124 [SPRINT3S2] Added autowired constructor to RestRequestController

--- src/main/java/project/restcontroller/RestRequestController.java
+++ src/main/java/project/restcontroller/RestRequestController.java
@@ -1,9 +1,26 @@
 package project.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.services.ProjectService;
+import project.services.TaskService;
+import project.services.UserService;
 
 @RestController
 @RequestMapping("projects/{projid}/tasks/{taskid}/requests/")
 public class RestRequestController {
+
+
+    private final ProjectService projectService;
+    private final TaskService taskService;
+    private final UserService userService;
+
+    @Autowired
+    public RestRequestController(ProjectService projectService, TaskService taskService, UserService userService) {
+        this.projectService = projectService;
+        this.taskService = taskService;
+        this.userService = userService;
+    }
+
 }
