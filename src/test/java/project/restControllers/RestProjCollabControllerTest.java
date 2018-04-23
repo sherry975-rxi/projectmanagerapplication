37f3ea4d36a229fc656b129e56cc5d52a011e3fa
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-23 14:55:50 +0100

    refs #110 Deleted unused import in the RestProjCollabControllerTest Class.

--- src/test/java/project/restControllers/RestProjCollabControllerTest.java
+++ src/test/java/project/restControllers/RestProjCollabControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
