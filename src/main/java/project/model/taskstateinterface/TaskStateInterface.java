932f7c8af5e65d7af72d9c428b9aaf2e783cf8c9
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-24 15:25:17 +0100

    addresses #135 Declared the methoid addRels in the TaskStateInterface

--- src/main/java/project/model/taskstateinterface/TaskStateInterface.java
+++ src/main/java/project/model/taskstateinterface/TaskStateInterface.java
@@ -1,5 +1,4 @@
 package project.model.taskstateinterface;
-
 import project.model.Task;
 
 public interface TaskStateInterface {
@@ -10,5 +9,15 @@ public interface TaskStateInterface {
 	 * @param task Task to verify and possible change state
 	 */
 	void doAction(Task task);
+
+	/**
+	 * Method that will add the links available to the task depending of its state
+	 *
+	 * @param task Task to get links from
+	 * @param projectId Id of the project to create the links
+	 *
+	 * @return List of Links
+	 */
+	void addRels(Task task, int projectId);
 
 }
