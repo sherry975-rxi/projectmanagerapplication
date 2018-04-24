695467a4a2a24cbd5f3e91973c01bc13e8639ce6
João Bessa <1171465@isep.ipp.pt>
2018-04-24 14:58:19 +0100

    refs #115 Began work on API log in implementation

--- src/main/java/project/restcontroller/RestAccountController.java
+++ src/main/java/project/restcontroller/RestAccountController.java
@@ -1,9 +1,50 @@
 package project.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.model.User;
+import project.services.UserService;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
 
 @RestController
 @RequestMapping("account/")
 public class RestAccountController {
+
+    private final UserService userService;
+
+    @Autowired
+    public RestAccountController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @RequestMapping(value="login", method= RequestMethod.GET)
+    public ResponseEntity<User> doLogin(@RequestBody User logInDTO) {
+
+        ResponseEntity<User> response = new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        User toLogIn = userService.getUserByEmail(logInDTO.getEmail());
+
+        if(toLogIn == null || !toLogIn.hasPassword()) {
+            return response;
+        } else if(!toLogIn.hasPassword()) {
+            //TODO Implement user validation
+            response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+            return response;
+        } else if(!toLogIn.checkLogin(logInDTO)) {
+            return response;
+        }
+
+        Link userDetails = linkTo(RestUserController.class).slash("users").slash(toLogIn.getUserID()).withSelfRel();
+        toLogIn.add(userDetails);
+
+        response = new ResponseEntity<>(toLogIn, HttpStatus.OK);
+
+        return response;
+    }
 }
