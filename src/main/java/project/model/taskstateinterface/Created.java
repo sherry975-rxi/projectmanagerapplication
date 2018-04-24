b8af89e2caac0e72742aacf61ca4b8daf182b2d8
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-24 15:23:54 +0100

    addresses #135 Implemented method addRels in the Created State

--- src/main/java/project/model/taskstateinterface/Created.java
+++ src/main/java/project/model/taskstateinterface/Created.java
@@ -1,7 +1,14 @@
 package project.model.taskstateinterface;
 
+import org.springframework.hateoas.Link;
 import project.model.StateEnum;
 import project.model.Task;
+import project.restcontroller.RestProjectController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 public class Created implements TaskStateInterface {
 
@@ -18,4 +25,22 @@ public class Created implements TaskStateInterface {
 			task.setCurrentState(StateEnum.PLANNED);
 	    }
     }
+
+	/**
+	 * Method that creates the rels available to this state and adds the links to the task
+	 *
+ 	 * @param task Task to get links from
+	 * @param projectId Id of the project to create the links
+	 */
+	@Override
+	public void addRels(Task task, int projectId){
+
+		//Not yet implemented - PATCH
+		Link reference = linkTo(RestProjectController.class).slash(projectId).slash("tasks").slash(task.getTaskID()).withRel("Add estimated dates");
+		task.add(reference);
+
+		//Not yet implemented POST
+		Link reference2 = linkTo(RestProjectController.class).slash(projectId).slash("tasks").slash(task.getTaskID()).slash("team").withRel("Add Project Collaborator");
+		task.add(reference2);
+	}
 }
