151ec5abddcbc6ad7ce69204183c5c543ff55d98
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-26 23:01:37 +0100

    addresses #125 Corrected statement that was causing code smell.

--- src/main/java/project/services/TaskService.java
+++ src/main/java/project/services/TaskService.java
@@ -996,7 +996,8 @@ public class TaskService {
 
 		for(Task other : finishedTaskListDecreasingOrder) {
 			TaskDTO taskDTO = new TaskDTO(other);
-			finishedDecreasing.add(taskDTO); }
+			finishedDecreasing.add(taskDTO);
+		}
 
 		return finishedDecreasing;
 	}
