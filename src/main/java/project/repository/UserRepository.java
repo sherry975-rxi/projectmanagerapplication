7b3623c5980ad8e18fbcb2a489a476fe94fc84a9
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-04-26 21:28:03 +0100

    addresses #98 changed findByUserID() to return an Optional of User

--- src/main/java/project/repository/UserRepository.java
+++ src/main/java/project/repository/UserRepository.java
@@ -13,7 +13,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	Optional<User> findByEmail(String email);
 
-	User findByUserID(int id);
+	Optional<User> findByUserID(int id);
 
 	List<User> findAllByUserProfile(Profile profile);
 
