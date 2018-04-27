ce79c4f0d1d64b19336e17e50449bddcfca6d66a
Daniel Almeida <1171459@isep.ipp.pt>
2018-04-27 09:58:16 +0100

    #139 Deleted unused methods from the class EffortUnit

--- src/main/java/project/model/EffortUnit.java
+++ src/main/java/project/model/EffortUnit.java
@@ -2,53 +2,8 @@ package project.model;
 
 public enum EffortUnit {
 
-    //Allows control of the value set for each enumeration
-    HOURS(0, "Hours"),
-    PM(1, "Person Month");
+    HOURS, PM
 
-    private int code;
-    private String description;
 
-    /**
-     * Constructor for Enum Class. By convention, a constructor of a Class of the type enum is private,
-     * so its omission prevents redundancy.
-     * @param code EffortUnit code
-     * @param description Description for enum type
-     */
-    EffortUnit(int code, String description) {
-        this.code = code;
-        this.description = description;
-    }
-
-    /**
-     * This method returns the enumerated Effort Unit corresponding to a code
-     *
-     * @param code Integer corresponding to an enum Effort Unit
-     * @return The EffortUnit if it exists, null, if code provided is null or throws an exception if the code provided
-     * doesn't corresponds to an existent EffortUnit
-     */
-
-    public static EffortUnit toEnum(Integer code) {
-
-        if (code == null) {
-            return null;
-        }
-        for (EffortUnit x : EffortUnit.values()) {
-
-            if (code.equals(x.getCode())) {
-                return x;
-            }
-        }
-
-        throw new IllegalArgumentException("Código inválido " + code);
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
 
