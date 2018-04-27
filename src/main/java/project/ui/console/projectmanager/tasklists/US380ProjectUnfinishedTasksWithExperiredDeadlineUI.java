0858e99dcc79293d352bc6992b7e57b6d81c091e
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-04-27 03:49:30 +0100

    addresses #98 UI US 380: editing code to avoid duplicated blocks

--- src/main/java/project/ui/console/projectmanager/tasklists/US380ProjectUnfinishedTasksWithExperiredDeadlineUI.java
+++ src/main/java/project/ui/console/projectmanager/tasklists/US380ProjectUnfinishedTasksWithExperiredDeadlineUI.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class US380ProjectUnfinishedTasksWithExperiredDeadlineUI {
 
 	@Autowired
-	private PrintProjectInfoController projectInfo;
+	private PrintProjectInfoController projectDetailsInfoController;
 
 	@Autowired
 	private US380GetProjectExpiredTaskListController controller;
@@ -21,22 +21,22 @@ public class US380ProjectUnfinishedTasksWithExperiredDeadlineUI {
 		Scanner scannerInput = new Scanner(System.in);
 		String line = "___________________________________________________";
 
-		projectInfo.setProject(project);
+		projectDetailsInfoController.setProject(project);
 
 		boolean loop = true;
 		while (loop) {
 			loop = false;
 			System.out.println();
-		System.out.println("PROJECT " + projectInfo.printProjectNameInfo().toUpperCase());
+		System.out.println("PROJECT " + projectDetailsInfoController.printProjectNameInfo().toUpperCase());
 		System.out.println(line);
-		System.out.println("ID: " + projectInfo.printProjectIDCodeInfo());
-		System.out.println("STATUS: " + projectInfo.printProjectStatusInfo());
-		System.out.println("DESCRIPTION: " + projectInfo.printProjectDescriptionInfo());
-		System.out.println("START DATE: " + projectInfo.printProjectStartDateInfo());
-		System.out.println("FINISH DATE: " + projectInfo.printProjectFinishDateInfo());
-		System.out.println("PROJECT MANAGER: " + projectInfo.printProjectManagerInfo());
-		System.out.println("PROJECT TEAM: " + projectInfo.printProjectTeamInfo());
-		System.out.println("PROJECT BUDGET: " + projectInfo.printProjectBudgetInfo());
+		System.out.println("ID: " + projectDetailsInfoController.printProjectIDCodeInfo());
+		System.out.println("STATUS: " + projectDetailsInfoController.printProjectStatusInfo());
+		System.out.println("DESCRIPTION: " + projectDetailsInfoController.printProjectDescriptionInfo());
+		System.out.println("START DATE: " + projectDetailsInfoController.printProjectStartDateInfo());
+		System.out.println("FINISH DATE: " + projectDetailsInfoController.printProjectFinishDateInfo());
+		System.out.println("PROJECT MANAGER: " + projectDetailsInfoController.printProjectManagerInfo());
+		System.out.println("PROJECT TEAM: " + projectDetailsInfoController.printProjectTeamInfo());
+		System.out.println("PROJECT BUDGET: " + projectDetailsInfoController.printProjectBudgetInfo());
 			System.out.println(line);
 		System.out.println(line);
 		System.out.println("     UNFINISHED TASKS WITH EXPIRED DEADLINE");
