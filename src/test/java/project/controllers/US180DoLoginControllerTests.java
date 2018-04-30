2b6fd71edab55c4afd18b3b4e57c7594b9122b51
Inês Pinto <1171476@isep.ipp.pt.com>
2018-04-30 23:10:47 +0100

    Improve coverage in tests, add mocks and given-when-then. refs #146

--- src/test/java/project/controllers/US180DoLoginControllerTests.java
+++ src/test/java/project/controllers/US180DoLoginControllerTests.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,86 +15,84 @@ import project.model.Profile;
 import project.model.User;
 import project.services.UserService;
 
+import static com.sun.javaws.JnlpxArgs.verify;
+import static com.sun.tools.doclint.Entity.times;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ComponentScan({ "project.services", "project.model", "project.controllers" })
 public class US180DoLoginControllerTests {
 
-	@Autowired
-	UserService userContainer;
+	@Mock
+	private UserService userServiceMock;
 
-	User user1;
-	User userAdmin;
-	@Autowired
-	US180DoLoginController doLoginController;
+	@Mock
+	private User userMock;
+
+	@InjectMocks
+	private US180DoLoginController doLoginController;
 
 	@Before
 	public void setUp() {
 
-		// create user
-		user1 = userContainer.createUser("Daniel", "daniel@gmail.com", "001", "collaborator", "910000000", "Rua",
-				"2401-00", "Test", "Testo", "Testistan");
-
-		// Sets a password for the user1
-		user1.setPassword("123456");
-		// set user as collaborator
-		user1.setUserProfile(Profile.COLLABORATOR);
-
-		userContainer.addUserToUserRepositoryX(user1);
 
 	}
 
 	@After
 	public void clear() {
-		user1 = null;
-		userAdmin = null;
 	}
 
-	// Test with a valid password and valid email
+	/**
+	 *GIVEN a valid email and password
+	 *WHEN user exists in the repository and the password is correct
+	 *THEN the login is performed
+	 */
 	@Test
 	public void DoValidLoginControllerTest() {
+		//GIVEN
+		String validEmail = "daniel@gmail.com";
+		String validPassword = "123456";
 
-		String validEmail = new String("daniel@gmail.com");
-		String validPassword = new String("123456");
+		//WHEN
+		when(userServiceMock.getUserByEmail(anyString())).thenReturn(userMock);
+		when(userMock.checkLogin(anyString())).thenReturn(true);
+		when(userServiceMock.isUserinUserContainer(any(User.class))).thenReturn(true);
 
+		//THEN
 		assertTrue(doLoginController.doLogin(validEmail, validPassword));
-
 	}
 
-	// Test with an invalid Pasword
 	@Test
 	public void DoInvalidPasswordLoginControllerTest() {
 
-		String validEmail = new String("daniel@gmail.com");
-		String invalidPassword = new String("12345");
-
+		String validEmail = "daniel@gmail.com";
+		String invalidPassword = "12345";
 		assertFalse(doLoginController.doLogin(validEmail, invalidPassword));
-
 	}
 
-//	// Test with an invalid Email
-//	@Test
-//	public void DoInvalidEmailLoginControllerTest() {
-//
-//		String invalidEmail = new String("invalid@gmail.com");
-//		String validPassword = new String("123456");
-//
-//		assertFalse(doLoginController.doLogin(invalidEmail, validPassword));
-//
-//	}
-//
-//	// Test with an invalid Email and invalid password
-//	@Test
-//	public void DoInvalidEmailAndPasswordLoginControllerTest() {
-//
-//		String invalidEmail = new String("invalid@gmail.com");
-//		String invalidPassword = new String("12345");
-//
-//		assertFalse(doLoginController.doLogin(invalidEmail, invalidPassword));
-//
-//	}
+	/**
+	 * GIVEN a string "email"
+	 * WHEN perform the method findUserByEmail
+	 * THEN the method call the getUserByEmail one time with the string passed in method
+	 */
+	@Test
+	public void shouldReturnUserFoundByEmail(){
+		//GIVEN
+		String email = "mail";
+
+		//WHEN
+		when(userServiceMock.getUserByEmail(anyString())).thenReturn(userMock);
+		doLoginController.findUserByEmail(email);
+
+		//THEN
+		Mockito.verify(userServiceMock, Mockito.times(1)).getUserByEmail(email);
+	}
 
 }
