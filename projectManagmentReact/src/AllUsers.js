09a80d8eed53147b85b4f4ea0e7d11d50977d9ac
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-05-10 18:51:53 +0100

    addresses #168 : - created AllUsers component; - updated index

---
+++ projectManagmentReact/src/AllUsers.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './AllUsers.css';
+
+class AllUsers extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+
+      componentDidMount() {
+        this.loadUsersFromServer();
+      }
+      
+      // Load users from database
+      loadUsersFromServer() {
+          fetch('users/allUsers',{ method: 'get'}) 
+          .then((response) => response.json()) 
+          .then((batatas) => { 
+              this.setState({ 
+                  users: batatas, 
+              }); 
+          });     
+      }
+
+
+    renderUsers(){
+        return this.state.users.map((userItem) =>{
+            return(
+                <div>
+                <p>{userItem.userID}</p>
+                <p>{userItem.name}</p>
+                <p>{userItem.email}</p>
+                <p>{userItem.function}</p>
+                <p>{userItem.userProfile}</p>
+                <hr/>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to Project Managment App</h2>
+                </div>
+                <p className="App-intro">
+                    Here follows:
+                </p>                
+                <h3>List of All Users</h3>   
+                {this.renderUsers()}                        
+            </div>
+        );
+    }
+}
+
+export default AllUsers;
