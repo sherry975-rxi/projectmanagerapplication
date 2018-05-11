54c9f8a99dddb00f0feeae82544478cb1cd613da
Daniel Almeida <1171459@isep.ipp.pt>
2018-05-11 16:50:21 +0100

    Addresses #152 Removed unused div in the index.html

--- projectManagmentReact/src/App.js
+++ projectManagmentReact/src/App.js
@@ -4,16 +4,22 @@ import Main from './components/main/main';
 import ActiveProjects from './components/activeProjects/ActiveProjects';
 import AllUsers from './components/Admin/AllUsers';
 import UsersList from './components/Admin/UsersList';
+import UserOngoing from './components/tasks/ongoing/UserOngoing'; 
+
 
 class App extends Component {
   render() {
 
     let subtitle = "Project Management Web-Application"
-
+ 
     return (
+     
+      
       <div className="App">
           {/* <Main title="mySwitch" subTitle= {subtitle}/>/>*/}
-          <Navbar/>
+        
+          <Navbar />
+          <UserOngoing />
           <ActiveProjects />
           <AllUsers />
           <UsersList />
