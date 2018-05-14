5253ba1041848569b4e20b7e8bf02628aaa22647
Fábio  Brás <1160058@isep.ipp.pt>
2018-05-14 16:04:51 +0100

    Changed the ROLE_USER to ROLE_COLLABORATOR

--- src/main/java/project/restcontroller/RestUserTasksController.java
+++ src/main/java/project/restcontroller/RestUserTasksController.java
@@ -38,7 +38,7 @@ public class RestUserTasksController {
      * @param userId
      * @return
      */
-    @PreAuthorize("hasRole('ROLE_USER') and #user.id == userId")
+    @PreAuthorize("hasRole('ROLE_COLLABORATOR') and #user.id == userId")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<List<Task>> getAllTasks(@PathVariable Integer userId) {
 
@@ -68,7 +68,7 @@ public class RestUserTasksController {
      * @param userId
      * @return
      */
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_COLLABORATOR')")
     @RequestMapping(value = "totaltimespent/completed/lastmonth", method = RequestMethod.GET)
     public ResponseEntity<Double> getTotalTimeSpentOnTasksCompletedLastMonth(@PathVariable Integer userId) {
 
@@ -84,7 +84,7 @@ public class RestUserTasksController {
 
     }
 
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_COLLABORATOR')")
     @RequestMapping(value = "finished", method = RequestMethod.GET)
     public List<Task> getUserFinishedTasks(@PathVariable Integer userId) {
         List<Task> taskList = taskService.getAllFinishedUserTasksInDecreasingOrder(this.userService.getUserByID(userId));
@@ -95,7 +95,7 @@ public class RestUserTasksController {
         return taskList;
     }
 
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_COLLABORATOR')")
     @RequestMapping(value = "pending", method = RequestMethod.GET)
     public List<Task> getPendingTasks(@PathVariable Integer userId) {
         List<Task> taskList = taskService.getStartedNotFinishedUserTaskList(userService.getUserByID(userId));
