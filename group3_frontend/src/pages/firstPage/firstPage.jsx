b6868124cd4f98cb389fa07a044be381ebb32b93
Inês Pinto <1171476@isep.ipp.pt.com>
2018-05-14 22:44:48 +0100

    Add link from  button on firstPage to SignUp page; add steps to signUp page.  refs #151

--- group3_frontend/src/pages/firstPage/firstPage.jsx
+++ group3_frontend/src/pages/firstPage/firstPage.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import './firstPage.css';
+import React, { Component } from "react";
+import "./firstPage.css";
+import { NavLink } from "react-router-dom";
 
 class firstPage extends Component {
-    state = {}
+    state = {};
     render() {
         return (
             <div className="Bar">
@@ -10,11 +11,20 @@ class firstPage extends Component {
                     <h1 className="switch">appManager</h1>
                     <p className="subTitle">Project Management Application</p>
                 </div>
-                <div className="Login">
-                    <button type="button" class="btn btn-primary btn-lg">REGISTER</button>
+                <div className="SignUp">
+                    <NavLink to="/signup">
+                        <button type="button" class="btn btn-primary btn-lg">
+                            Sign Up
+                        </button>
+                    </NavLink>
+                </div>
+                <div className="LogIn">
+                    <NavLink className="btn btn-primary btn-lg" to="/login">
+                        Log In
+                    </NavLink>
                 </div>
             </div>
-        )
+        );
     }
 }
 
