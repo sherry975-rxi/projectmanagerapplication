a1f8cd2a6bba75395794e3b26dfd92ef441bac71
Fábio  Brás <1160058@isep.ipp.pt>
2018-05-25 15:56:31 +0100

    Changed the parent prop so that it behaves as a button that enables a dropdown list of funcionalities refs #190

--- group3_frontend/src/components/list/List.js
+++ group3_frontend/src/components/list/List.js
@@ -2,17 +2,39 @@ import React from 'react';
 import "./List.css";
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { visibility: 'hide' };
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility() {
+    const newVisibility = this.state.visibility == 'hide' ? 'show' : 'hide';
+    this.setState({ visibility: newVisibility });
+  }
+
   render() {
     let titleText = `${this.props.type}`;
     if (this.props.children instanceof Array) {
     	titleText += 's';
     }
+
+    if(this.state.visibility == 'hide'){
+      return(
+        <div>
+        <h4 className= "Parent"><button className="Parent" type="button" onClick={this.toggleVisibility}>{titleText}</button></h4>
+        </div>
+      )
+    }
+
+    else{
     return (
       <div>
-        <h4 className= "Parent">{titleText}</h4>
+        <h4 className= "Parent"><button className="Parent" type="button" onClick={this.toggleVisibility}>{titleText}</button></h4>
         <ul className= "Children">{this.props.children}</ul>
       </div>
     );
+  }
   }
 }
 
