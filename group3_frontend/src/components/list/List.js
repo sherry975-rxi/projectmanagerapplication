d0b7f5ef5b84b9d8eff4fbc851624996d6d71ec0
João Bessa <1171465@isep.ipp.pt>
2018-06-11 16:44:21 +0100

    #261 Each profile now shows a different set of Links depending on whether the active user's profile

--- group3_frontend/src/components/list/List.js
+++ group3_frontend/src/components/list/List.js
@@ -2,22 +2,7 @@ import React from 'react';
 import './List.css';
 
 class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visibility: 'hide',
-            buttonActive: 'ParentButton'
-        };
-    }
 
-    toggleVisibility = () => {
-        const newVisibility =
-            this.state.visibility === 'hide' ? 'show' : 'hide';
-        this.setState({
-            visibility: newVisibility,
-            buttonActive: 'buttonActive'
-        });
-    };
 
     render() {
         let icon = (
@@ -26,45 +11,18 @@ class List extends React.Component {
             </div>
         );
 
-        let titleText = `${this.props.type}`;
-        if (this.props.children instanceof Array) {
-            titleText += 's';
-        }
 
-        if (this.state.visibility === 'hide') {
-            return (
-                <div>
-                    <div className="Parent">
-                        <button
-                            className="ParentButton"
-                            type="button"
-                            onClick={this.toggleVisibility}
-                        >
-                            <div className="title">{titleText}</div>
-                        </button>
+        return (
+            <div>
+                <div className="Parent">
+                    <button className="ParentButton" >
+                        <div className="title">{this.props.children}</div>
+                    </button>
                         {icon}
-                    </div>
                 </div>
-            );
-        } else {
-            return (
-                <div>
-                    <div className="Parent">
-                        <button
-                            className={this.state.buttonActive}
-                            type="button"
-                            onClick={this.toggleVisibility}
-                        >
-                            <div className="title">{titleText}</div>
-                        </button>
-                        {icon}
-                    </div>
-                    <div className="list">
-                        <ul className="Children">{this.props.children}</ul>
-                    </div>
-                </div>
-            );
-        }
+            </div>
+        );
+
     }
 }
 
