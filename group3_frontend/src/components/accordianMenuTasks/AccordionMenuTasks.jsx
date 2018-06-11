da29e81f7be63cab2ca70131530aae742dae4e65
Inês Pinto <1171476@isep.ipp.pt.com>
2018-06-11 15:07:26 +0100

    refs #280 #277 Reorganize files and clean errors

--- group3_frontend/src/components/accordianMenuTasks/AccordionMenuTasks.jsx
+++ group3_frontend/src/components/accordianMenuTasks/AccordionMenuTasks.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { PanelGroup, Panel } from 'react-bootstrap';
-import './AccordionMenuTasks.css'
-import * as Constants from './titleConstants'
+import './AccordionMenuTasks.css';
+import * as Constants from './utils/titleConstants';
 import SmallButton from '../button/smallButton.jsx';
-import { handleTaskHeaders } from './handleList';
+import { handleTaskHeaders } from './utils/handleList';
 
 class AccordionMenu extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -19,36 +18,44 @@ class AccordionMenu extends Component {
     }
 
     renderTitles() {
-        return (
-            Constants.TASKS.map((element) => <th> {element}</th>
-            ))
+        return Constants.TASKS.map(element => <th> {element}</th>);
     }
 
     renderList(list) {
         let key = 1;
-        return (
-            handleTaskHeaders(list).map((element) =>
-                <Panel eventKey={key}>
-                    <Panel.Heading>
-                        <Panel.Title toggle><div className="taskContent"> <table className="table table-content">
-                            <thead>
-                                <tr>
-                                    {element.map((detail) => <th> {detail} </th>, key++)}
-                                    <th><SmallButton text="Edit" /> </th>
-                                </tr>
-                            </thead>
-                        </table></div></Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible> <ul className="bodyContent">
-                        {element.map((detail) => <li> {detail} </li>, key++)}
-                    </ul></Panel.Body>
-                </Panel>
-            )
-        )
+        return handleTaskHeaders(list).map(element => (
+            <Panel eventKey={key}>
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        <div className="taskContent">
+                            {' '}
+                            <table className="table table-content">
+                                <thead>
+                                    <tr>
+                                        {element.map(
+                                            detail => <th> {detail} </th>,
+                                            key++
+                                        )}
+                                        <th>
+                                            <SmallButton text="Edit" />{' '}
+                                        </th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                    {' '}
+                    <ul className="bodyContent">
+                        {element.map(detail => <li> {detail} </li>, key++)}
+                    </ul>
+                </Panel.Body>
+            </Panel>
+        ));
     }
 
     render() {
-
         return (
             <PanelGroup
                 accordion
@@ -59,9 +66,7 @@ class AccordionMenu extends Component {
                 <Panel eventKey="1">
                     <table className="table table-title">
                         <thead>
-                            <tr>
-                                {this.renderTitles()}
-                            </tr>
+                            <tr>{this.renderTitles()}</tr>
                         </thead>
                     </table>
                 </Panel>
