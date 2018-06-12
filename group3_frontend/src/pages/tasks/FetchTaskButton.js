07dbab3328215f9978cd964ebca059040271589f
Daniel Pereira <1171460@isep.ipp.pt>
2018-06-12 12:35:56 +0100

    refs #276 removed CSS property "mediumButton:focus", as it didn't solve the problem. Added a component for when a medium button is "selected". Added filter buttons with toggle action to the task menu.

--- group3_frontend/src/pages/tasks/FetchTaskButton.js
+++ group3_frontend/src/pages/tasks/FetchTaskButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import MediumButton from '../../../src/components/button/mediumButton'
+import MediumButton from '../../components/button/mediumButton'
+import MediumButtonSelected from '../../components/button/mediumButtonSelected'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {changeToFinished, changeToOnGoing, changeToStandBy, changeToNotStarted, changeToAllTasks}  from '../../actions/filterActions';
@@ -16,24 +17,108 @@ class FetchTaskButton extends Component{
 
 
     render(){
-        return(
-            <table className="table table-title">
-            <thead>
-                <tr>
-                     <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButton text="All Tasks"/> </th></td>
-                     <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButton text="On Going"/> </th></td>
-                     <td onClick={(e) => this.props.changeToFinished()}><th><MediumButton text="Finished"/> </th></td>
-                     <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButton text="Not Started"/> </th></td>
-                     <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButton text="Stand By"/> </th></td>
+        switch (this.props.filter) {
+            case 'all':
+                return (
+                    <table className="table table-title">
+                        <thead>
+                        <tr>
+                            <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButtonSelected text="All Tasks"/> </th></td>
+                            <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButton text="On Going"/> </th></td>
+                            <td onClick={(e) => this.props.changeToFinished()}><th><MediumButton text="Finished"/> </th></td>
+                            <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButton text="Not Started"/> </th></td>
+                            <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButton text="Stand By"/> </th></td>
 
 
-                </tr>
-            </thead>
-        </table>
-        )
+                        </tr>
+                        </thead>
+                    </table>
+                )
+            case 'unfinished':
+                return (
+                    <table className="table table-title">
+                        <thead>
+                        <tr>
+                            <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButton text="All Tasks"/> </th></td>
+                            <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButtonSelected text="On Going"/> </th></td>
+                            <td onClick={(e) => this.props.changeToFinished()}><th><MediumButton text="Finished"/> </th></td>
+                            <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButton text="Not Started"/> </th></td>
+                            <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButton text="Stand By"/> </th></td>
+
+
+                        </tr>
+                        </thead>
+                    </table>
+                )
+            case 'finished':
+                return (
+                    <table className="table table-title">
+                        <thead>
+                        <tr>
+                            <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButton text="All Tasks"/> </th></td>
+                            <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButton text="On Going"/> </th></td>
+                            <td onClick={(e) => this.props.changeToFinished()}><th><MediumButtonSelected text="Finished"/> </th></td>
+                            <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButton text="Not Started"/> </th></td>
+                            <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButton text="Stand By"/> </th></td>
+
+
+                        </tr>
+                        </thead>
+                    </table>
+                )
+            case 'withoutCollaborators':
+                return (
+                    <table className="table table-title">
+                        <thead>
+                        <tr>
+                            <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButton text="All Tasks"/> </th></td>
+                            <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButton text="On Going"/> </th></td>
+                            <td onClick={(e) => this.props.changeToFinished()}><th><MediumButton text="Finished"/> </th></td>
+                            <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButton text="Not Started"/> </th></td>
+                            <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButtonSelected text="Stand By"/> </th></td>
+
+
+                        </tr>
+                        </thead>
+                    </table>
+                )
+            case 'notstarted':
+                return (
+                    <table className="table table-title">
+                        <thead>
+                        <tr>
+                            <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButton text="All Tasks"/> </th></td>
+                            <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButton text="On Going"/> </th></td>
+                            <td onClick={(e) => this.props.changeToFinished()}><th><MediumButton text="Finished"/> </th></td>
+                            <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButtonSelected text="Not Started"/> </th></td>
+                            <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButton text="Stand By"/> </th></td>
+
+
+                        </tr>
+                        </thead>
+                    </table>
+                )
+            default:
+                return (
+                    <table className="table table-title">
+                        <thead>
+                        <tr>
+                            <td onClick={(e) => this.props.changeToAllTasks()}><th><MediumButton text="All Tasks"/> </th></td>
+                            <td onClick={(e) => this.props.changeToOnGoing()}><th><MediumButton text="On Going"/> </th></td>
+                            <td onClick={(e) => this.props.changeToFinished()}><th><MediumButton text="Finished"/> </th></td>
+                            <td onClick={(e) => this.props.changeToNotStarted()}><th><MediumButton text="Not Started"/> </th></td>
+                            <td onClick={(e) => this.props.changeToStandBy()}><th><MediumButton text="Stand By"/> </th></td>
+
+
+                        </tr>
+                        </thead>
+                    </table>
+                )
+        }
     }
 
 }
 
+const mapStateToProps = state => { return ({ filter: state.filterReducer.filterType}) }
 const mapDispatchToProps = dispatch => bindActionCreators({ changeToFinished, changeToNotStarted, changeToOnGoing, changeToStandBy, changeToAllTasks}, dispatch)
-export default connect(null, mapDispatchToProps)(FetchTaskButton);
+export default connect(mapStateToProps, mapDispatchToProps)(FetchTaskButton);
