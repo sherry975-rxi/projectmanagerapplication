d8f81a60f0b9af0bc82dffcdb1e8b1784015d078
Inês Pinto <1171476@isep.ipp.pt.com>
2018-06-12 09:58:58 +0100

    remove unused imports refs #280 #277 #263 #279

--- group3_frontend/src/pages/tasks/Test.js
+++ group3_frontend/src/pages/tasks/Test.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
-import Moment from 'react-moment';
+import React, { Component } from 'react';
 import Error from './../../components/error/error';
-import MarkTaskAsFinished from "./MarkTaskAsFinished";
-import CreateReport from "../reports/CreateReport";
-import Reports from "../reports/Reports"
-import { Link } from "react-router-dom";
+import MarkTaskAsFinished from './MarkTaskAsFinished';
+import CreateReport from '../reports/CreateReport';
+import Reports from '../reports/Reports';
+import { Link } from 'react-router-dom';
 import MediumButton from './../../components/button/mediumButton';
-import FetchTaskButton from '../tasks/FetchTaskButton'
+import FetchTaskButton from '../tasks/FetchTaskButton';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import AuthService from './../loginPage/AuthService';
-import AccordionMenu from '../../components/accordianMenuTasks/AccordionMenuTasks.jsx'
-import { handleTaskHeaders } from '../../components/utils/handleList'
-
-
+import AccordionMenu from '../../components/accordianMenuTasks/AccordionMenuTasks.jsx';
 
 class Test extends Component {
     constructor(props) {
@@ -26,7 +22,7 @@ class Test extends Component {
         };
 
         this.refreshPage = this.refreshPage.bind(this);
-        this.AuthService = new AuthService()
+        this.AuthService = new AuthService();
     }
 
     //TODO: Add sort by ascending or descending order to these tables
@@ -37,44 +33,42 @@ class Test extends Component {
 
     componentDidUpdate(prevProps) {
         if (prevProps.filter !== this.props.filter) {
-          this.refreshPage();
+            this.refreshPage();
         }
-      }
-
-    refreshPage() {
-
-        this.AuthService.fetch(`/projects/2/tasks/${this.props.filter}`, { method: 'get' })
-            .then((responseData) => {
-                console.log(responseData);
-                this.setState({
-                    tasks: responseData,
-                    message: responseData.error
-                });
-            })
-
     }
 
-
+    refreshPage() {
+        this.AuthService.fetch(`/projects/2/tasks/${this.props.filter}`, {
+            method: 'get'
+        }).then(responseData => {
+            console.log(responseData);
+            this.setState({
+                tasks: responseData,
+                message: responseData.error
+            });
+        });
+    }
 
     render() {
-
         if (this.state.message != null) {
-            return (<Error message={this.state.message} />)
-        }
-        else {
+            return <Error message={this.state.message} />;
+        } else {
             return (
-                 <div>
-                   {console.log("AAAAA")}
+                <div>
+                    {console.log('AAAAA')}
                     {console.log(this.props.filter)}
                     <FetchTaskButton />
-                  <AccordionMenu list={this.state.tasks} />
+                    <AccordionMenu list={this.state.tasks} />
                 </div>
-
-            )
+            );
         }
     }
 }
 
-const mapStateToProps = state => { return ({ filter: state.filterReducer.filterType}) }
-export default connect(mapStateToProps, null)(Test);
-
+const mapStateToProps = state => {
+    return { filter: state.filterReducer.filterType };
+};
+export default connect(
+    mapStateToProps,
+    null
+)(Test);
