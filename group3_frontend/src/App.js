1e985a819b4adbc93116b9538436a01459f67fdb
Rita Machado <1171471@isep.ipp.pt>
2018-06-13 16:19:07 +0100

    addresses #275 create component DeleteTask and a button to activate action

--- group3_frontend/src/App.js
+++ group3_frontend/src/App.js
@@ -28,6 +28,7 @@ import ProjectTasks from './pages/tasks/ProjectTasks';
 import ActiveTaskTeam from './pages/tasks/ActiveTaskTeam';
 import CreateRequest from './pages/requests/CreateRequest';
 import AvailableListOfCollaborators from './pages/tasks/AvailableListOfCollaborators';
+import DeleteTask from './pages/tasks/DeleteTask';
 
 
 class App extends Component {
@@ -141,6 +142,10 @@ class App extends Component {
                     <Route
                         exact path="/projects/:projectID/activeTeam"
                         component={requiresAuth(AvailableListOfCollaborators)}
+                    />
+                    <Route
+                        exact path="/projects/:projectID/tasks/:taskID/delete"
+                        component={requiresAuth(DeleteTask)}
                     />
 
                 </Switch>
