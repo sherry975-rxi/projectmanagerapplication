7b6e52346e63adaa745a1864007917207d1ab1d8
Joao Leite <1161661@isep.ipp.pt>
2018-06-14 16:25:26 +0100

    addresses 296 - Enhancement in register page

--- group3_frontend/src/components/signUpForm/SignUpForm.js
+++ group3_frontend/src/components/signUpForm/SignUpForm.js
@@ -89,143 +89,187 @@ class SignUpForm extends Component {
     stepOneForm = () => (
         <div className="SignUp">
             <form onSubmit={this.handleSubmit}>
-                <FormGroup controlId="name" className="formField">
-                    <ControlLabel>Name</ControlLabel>
-                    <FormControl
-                        autoFocus
-                        type="text"
-                        value={this.state.name}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+            <table className="SignUpTable">
+                <tr>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="name" className="formField">
+                        <ControlLabel>Name</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="text"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                        </FormGroup>
+                    </td>
 
-                <FormGroup controlId="email" className="formField">
-                    <ControlLabel>Email</ControlLabel>
-                    <FormControl
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="email" className="formField">
+                        <ControlLabel>Email</ControlLabel>
+                        <FormControl
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                        </FormGroup>
+                    </td>
+                </tr>
 
-                <FormGroup controlId="userFunction" className="formField">
-                    <ControlLabel>Function</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.userFunction}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+                <tr>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="userFunction" className="formField">
+                        <ControlLabel>Function</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.userFunction}
+                            onChange={this.handleChange}
+                        />
+                        </FormGroup>
+                    </td>
 
-                <FormGroup controlId="phone" className="formField">
-                    <ControlLabel>Phone</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.phone}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="phone" className="formField">
+                        <ControlLabel>Phone</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.phone}
+                            onChange={this.handleChange}
+                        />
+                        </FormGroup>
+                    </td>
+                </tr>
 
-                <FormGroup controlId="street" className="formField">
-                    <ControlLabel>Street</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.street}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+                <tr>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="Country" className="formField">
+                        <ControlLabel>Country</ControlLabel>
 
-                <FormGroup controlId="zipCode" className="formField">
-                    <ControlLabel>Zip Code</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.zipCode}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+                        <CountryDropdown
+                            classes="formFieldDropDown form-control"
+                            value={this.state.country}
+                            onChange={val => this.selectCountry(val)}
+                            componentClass="select"
+                            placeholder="select"
+                        />
+                         </FormGroup>
+                    </td>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="District" className="formField">
+                            <ControlLabel>District</ControlLabel>
 
-                <FormGroup controlId="city" className="formField">
-                    <ControlLabel>City</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.city}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
+                            <RegionDropdown
+                                classes="formFieldDropDown form-control"
+                                country={this.state.country}
+                                value={this.state.district}
+                                onChange={val => this.selectRegion(val)}
+                            />
+                         </FormGroup>
+                    </td>
+                </tr>
 
-                <FormGroup controlId="Country" className="formField">
-                    <ControlLabel>Country</ControlLabel>
+                <tr>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="city" className="formField">
+                            <ControlLabel>City</ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={this.state.city}
+                                onChange={this.handleChange}
+                            />
+                         </FormGroup>
+                    </td>
+                    <td className="tdSignUpForm">
+                         <FormGroup controlId="street" className="formField">
+                            <ControlLabel>Street</ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={this.state.street}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                    </td>
+                </tr>
 
-                    <CountryDropdown
-                        classes="formFieldDropDown form-control"
-                        value={this.state.country}
-                        onChange={val => this.selectCountry(val)}
-                        componentClass="select"
-                        placeholder="select"
-                    />
-                </FormGroup>
+                <tr>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="zipCode" className="formField">
+                            <ControlLabel>Zip Code</ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={this.state.zipCode}
+                                onChange={this.handleChange}
+                            />
+                         </FormGroup>
 
-                <FormGroup controlId="District" className="formField">
-                    <ControlLabel>District</ControlLabel>
+                    </td>
+                    <td className="tdSignUpForm">
+                        <FormGroup controlId="password" className="formField">
+                            <ControlLabel>Password</ControlLabel>
+                            <FormControl
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                                type="password"
+                            />
+                      </FormGroup>
+                    </td>
+                </tr>
 
-                    <RegionDropdown
-                        classes="formFieldDropDown form-control"
-                        country={this.state.country}
-                        value={this.state.district}
-                        onChange={val => this.selectRegion(val)}
-                    />
-                </FormGroup>
-
-                <FormGroup controlId="password" className="formField">
-                    <ControlLabel>Password</ControlLabel>
-                    <FormControl
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                        type="password"
-                    />
-                </FormGroup>
-
-                <FormGroup controlId="securityQuestion" className="formField">
-                    <ControlLabel>Security Question</ControlLabel>
-                    <FormControl
-                        value={this.state.securityQuestion}
-                        onChange={this.handleChange}
-                        componentClass="select"
-                        placeholder="select"
-                    >
-                        <option value="" disabled defaultValue="">
-                            Select your option
-                        </option>
-                        <option value="pet">
-                            What is the name of your first pet?
-                        </option>
-                        <option value="school">
-                            What elementary school did you attend?
-                        </option>
-                        <option value="honeymoon">
-                            Where did you go for your honeymoon?
-                        </option>
-                    </FormControl>
-                </FormGroup>
-
-                <FormGroup controlId="securityAnswer" className="formField">
-                    <ControlLabel>Security Answer</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.securityAnswer}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
-
-                <FormGroup controlId="idNumber" className="formField">
-                    <ControlLabel>Company collaborator ID</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.idNumber}
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>
-
+                <tr>
+                    <td className="tdSignUpForm">
+                    <FormGroup controlId="securityQuestion" className="formField">
+                        <ControlLabel>Security Question</ControlLabel>
+                        <FormControl
+                            value={this.state.securityQuestion}
+                            onChange={this.handleChange}
+                            componentClass="select"
+                            placeholder="select"
+                        >
+                            <option value="" disabled defaultValue="">
+                                Select your option
+                            </option>
+                            <option value="pet">
+                                What is the name of your first pet?
+                            </option>
+                            <option value="school">
+                                What elementary school did you attend?
+                            </option>
+                            <option value="honeymoon">
+                                Where did you go for your honeymoon?
+                            </option>
+                        </FormControl>
+                     </FormGroup>
+                    </td>
+                    <td className="tdSignUpForm">
+                        
+                    <FormGroup controlId="securityAnswer" className="formField">
+                        <ControlLabel>Security Answer</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.securityAnswer}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="tdSignUpForm">
+                        
+                        <FormGroup controlId="idNumber" className="formField">
+                            <ControlLabel>Company collaborator ID</ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={this.state.idNumber}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                    </td>
+                    <td className="tdSignUpForm">
+                    </td>
+                </tr>
+                
+            </table>
+              
+            
                 <div className="termsAndConditions">
                     By using this application, you agree to be bound by, and to
                     comply with these <b>Terms and Conditions</b>. To proceed
