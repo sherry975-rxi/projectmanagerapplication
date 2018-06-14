46da531113a01bb91714fc75447169827c353e29
Daniel Almeida <1171459@isep.ipp.pt>
2018-06-14 23:45:17 +0100

    addresses #297 Improved CreateProject css.

--- group3_frontend/src/pages/projects/CreateProject.js
+++ group3_frontend/src/pages/projects/CreateProject.js
@@ -3,7 +3,7 @@ import './CreateProject.css';
 import { FormGroup, FormControl, ControlLabel, Button, Radio } from "react-bootstrap";
 import AuthService from './../loginPage/AuthService';
 import ListPossibleProjectManagers from './ListPossibleProjectManagers';
-import {toastr} from "react-redux-toastr";
+import { toastr } from "react-redux-toastr";
 import { Redirect } from 'react-router-dom';
 
 class CreateProject extends Component {
@@ -12,7 +12,7 @@ class CreateProject extends Component {
         this.state = {
             name: "",
             description: "",
-            projectManager: { name: "None Selected!"},
+            projectManager: { name: "None Selected!" },
             budget: "",
             effortUnit: "HOURS",
             awaitingResponse: false
@@ -39,7 +39,7 @@ class CreateProject extends Component {
 
     handleEffortSelection = event => {
         this.setState({
-           effortUnit: event.target.value
+            effortUnit: event.target.value
         });
     }
 
@@ -53,20 +53,22 @@ class CreateProject extends Component {
     handleSubmit = event => {
         event.preventDefault();
 
-        const {name,
-        description,
-        projectManager,
-        budget,
-        effortUnit} = this.state;
-
-        const projectDTO = {name,
+        const { name,
             description,
             projectManager,
             budget,
-            effortUnit};
+            effortUnit } = this.state;
+
+        const projectDTO = {
+            name,
+            description,
+            projectManager,
+            budget,
+            effortUnit
+        };
 
         this.setState({
-            awaitingResponse:true
+            awaitingResponse: true
         });
 
         this.AuthService.fetch(
@@ -78,7 +80,7 @@ class CreateProject extends Component {
         ).then(responseData => {
 
             toastr.success('Project Created Successfully!');
-            setTimeout(function() {
+            setTimeout(function () {
                 window.location.href = "/activeprojects";
             }, 1000);
 
@@ -99,68 +101,70 @@ class CreateProject extends Component {
 
         return (
             <div className=" table-striped">
-                <h3>
+                <span className="pageTitle">
                     <b>Create project</b>
-                </h3>
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="name" bsSize="large">
-                        <ControlLabel>Project Name</ControlLabel>
-                        <FormControl
-                            className="textForm"
-                            autoFocus
-                            type="text"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
+                </span>
+                <div className="createProjectForm">
+                    <form onSubmit={this.handleSubmit}>
+                        <FormGroup controlId="name" bsSize="large">
+                            <ControlLabel className="formTitle">Project Name</ControlLabel>
+                            <FormControl
+                                className="textForm"
+                                autoFocus
+                                type="text"
+                                value={this.state.name}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
 
-                    <FormGroup controlId="description" bsSize="large">
-                        <ControlLabel>Description</ControlLabel>
-                        <FormControl
-                            className="textForm"
-                            autoFocus
-                            type="text"
-                            value={this.state.description}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
+                        <FormGroup controlId="description" bsSize="large">
+                            <ControlLabel className="formTitle">Description</ControlLabel>
+                            <FormControl
+                                className="textForm"
+                                autoFocus
+                                type="text"
+                                value={this.state.description}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
 
-                    <FormGroup controlId="budget" bsSize="large">
-                        <ControlLabel>Budget</ControlLabel>
-                        <FormControl
-                            className="textForm"
-                            autoFocus
-                            type="number"
-                            pattern="[0-9]*"
-                            inputmode="numeric"
-                            value={this.state.budget}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
+                        <FormGroup controlId="budget" bsSize="large">
+                            <ControlLabel className="formTitle" >Budget</ControlLabel>
+                            <FormControl
+                                className="textForm"
+                                autoFocus
+                                type="number"
+                                pattern="[0-9]*"
+                                inputmode="numeric"
+                                value={this.state.budget}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
 
-                    <FormGroup controlId="effortUnit">
-                        <ControlLabel>Effort Unit</ControlLabel>
-                        <Radio value="HOURS" checked={this.state.effortUnit === "HOURS"}
-                               onChange={this.handleEffortSelection}>
-                            Hours
+                        <FormGroup controlId="effortUnit">
+                            <ControlLabel className="formTitle"><b>Effort Unit</b></ControlLabel>
+                            <Radio value="HOURS" checked={this.state.effortUnit === "HOURS"}
+                                onChange={this.handleEffortSelection}>
+                                Hours
                         </Radio>{' '}
-                        <Radio value="PM" checked={this.state.effortUnit === "PM"}
-                            onChange={this.handleEffortSelection}>
-                            Person/Month
+                            <Radio value="PM" checked={this.state.effortUnit === "PM"}
+                                onChange={this.handleEffortSelection}>
+                                Person/Month
                         </Radio>
-                    </FormGroup>
+                        </FormGroup>
 
-                    <ControlLabel> Project Manager ({this.state.projectManager.name})  </ControlLabel>
-                    <br />
-                    <ListPossibleProjectManagers onSelect={this.selectManager}/>
+                        <ControlLabel className="formTitle"> Project Manager ({this.state.projectManager.name})  </ControlLabel>
+                        <br />
+                        <ListPossibleProjectManagers onSelect={this.selectManager} />
 
-                    <br />
+                        <br />
 
 
-                    <Button block className="btn btn-primary" disabled={!this.validateForm()} type="submit" >
-                        Create Project
+                        <Button block className="btn btn-primary" disabled={!this.validateForm()} type="submit" >
+                            Create Project
                     </Button>
-                </form>
+                    </form>
+                </div>
             </div>
         );
     }
