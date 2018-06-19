3bdc83a2369bc2f3cf35207fd08cdcc03439329e
Rui Miguel Borges <1171473@isep.ipp.pt>
2018-06-19 23:14:27 +0100

    addresses #304 updated component  of user tasks

--- group3_frontend/src/pages/tasks/OngoingTasks.js
+++ group3_frontend/src/pages/tasks/OngoingTasks.js
@@ -7,30 +7,36 @@ import AuthService from '../loginPage/AuthService';
 import Moment from 'react-moment';
 import Error from './../../components/error/error';
 import MarkTaskAsFinished from './MarkTaskAsFinished';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import MediumButton from './../../components/button/mediumButton';
 import UserTasksFilter from '../tasks/UserTasksFilter';
 import AccordionMenu from '../../components/accordianMenuTasks/AccordionMenuTasks.jsx';
 import { connect } from 'react-redux';
+import LoadingComponent from './../../components/loading/LoadingComponent';
+
 
 class OngoingTasks extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            tasks: [],
-            project: {},
-            externalData: null
-        };
         this.AuthService = new AuthService();
+        this.renderTasks = this.renderTasks.bind(this);
+
     }
 
     //TODO: Add sort by ascending or descending order to these tables
 
-    componentDidMount() {
-        this.refreshPage();
-    }
+    
 
     renderTasks() {
+
+        if (this.props.tasksLoading) {
+            return (<LoadingComponent />)
+        }
+
+        else if (this.props.error) {
+            return <Redirect to="/login" />;
+        }
+
         switch (this.props.filter) {
             case 'all':
                 return <AccordionMenu list={this.props.allTasks} />;
@@ -38,84 +44,14 @@ class OngoingTasks extends Component {
                 return <AccordionMenu list={this.props.ongoingTasks} />;
             case 'finished':
                 return <AccordionMenu list={this.props.finishedTasks} />;
-            case 'notstarted':
-                return <AccordionMenu list={this.props.notStartedTasks} />;
             default:
-                return;
+                return <LoadingComponent />;
         }
     }
 
-    refreshPage = () => {
-        this.AuthService.fetch(
-            `/users/${this.AuthService.getUserId()}/tasks/pending`,
-            { method: 'get' }
-        ).then(responseData => {
-            this.setState({
-                tasks: responseData,
-                message: responseData.error
-            });
-        });
-    };
-
-    renderOngoingTasks() {
-        return this.state.tasks.map((taskItem, index) => {
-            return (
-                <tr className="line" key={index}>
-                    <td>{taskItem.taskID}</td>
-                    <td>{taskItem.project['projectId']}</td>
-                    <td>{taskItem.description}</td>
-                    <td>
-                        <Moment format="YYYY/MM/DD">
-                            {taskItem.startDate}
-                        </Moment>
-                    </td>
-                    <td>
-                        <Moment format="YYYY/MM/DD">
-                            {taskItem.taskDeadline}
-                        </Moment>
-                    </td>
-                    <td>
-                        <MarkTaskAsFinished
-                            id={taskItem.taskID}
-                            project={taskItem.project}
-                            onClick={this.refreshPage}
-                        />
-                    </td>
-                    <td>
-                        <Link
-                            to={
-                                '/projects/' +
-                                taskItem.project +
-                                '/tasks/' +
-                                taskItem.taskID +
-                                '/createreport'
-                            }
-                        >
-                            <MediumButton text="Create Report" />
-                        </Link>
-                    </td>
-                    <td>
-                        <Link
-                            to={
-                                '/projects/' +
-                                taskItem.project +
-                                '/tasks/' +
-                                taskItem.taskID +
-                                '/reports'
-                            }
-                        >
-                            <MediumButton text="View Reports" />
-                        </Link>
-                    </td>
-                </tr>
-            );
-        });
-    }
 
     render() {
-        if (this.state.message != null) {
-            return <Error message={this.state.message} />;
-        } else {
+      
             return (
                 <div className=" table-striped">
                     <UserTasksFilter userID={this.AuthService.getUserId()} />
@@ -128,15 +64,14 @@ class OngoingTasks extends Component {
             );
         }
     }
-}
+
 
 const mapStateToProps = state => {
     return {
-        filter: state.filterReducer.filterType,
-        finishedTasks: state.projectTasks.finishedTasks,
-        ongoingTasks: state.projectTasks.ongoingTasks,
-        notStartedTasks: state.projectTasks.notStartedTasks,
-        allTasks: state.projectTasks.allTasks
+        filter: state.userTasksFilter.filterType,
+        finishedTasks: state.userTasks.finishedTasks,
+        ongoingTasks: state.userTasks.ongoingTasks,
+        allTasks: state.userTasks.allTasks
     };
 };
 
