6e99058e0617185e2380150199ef0c6abfa304f9
Daniel Almeida <1171459@isep.ipp.pt>
2018-06-22 16:32:30 +0100

    addresses #307 Created and implemented the methods displayConfirmation, cancelCreation and createTaskAndDisplayConfirmation in the CreateTask component

--- group3_frontend/src/pages/tasks/createTask/CreateTask.jsx
+++ group3_frontend/src/pages/tasks/createTask/CreateTask.jsx
@@ -7,7 +7,15 @@ import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'rea
 import 'react-dates/lib/css/_datepicker.css';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { createTask } from '../../../actions/createTaskActions'
+import { createTask, deleteTaskCreated } from '../../../actions/createTaskActions'
+import {
+    updateAllTasks,
+    updateNotStartedTasks, updateUnfinishedTasks
+} from '../../../actions/projectTasksActions';
+import LoadingComponent from './../../../components/loading/LoadingComponent';
+import '../../../components/loading/LoadingComponent.css'
+import { toastr } from 'react-redux-toastr';
+
 
 class CreateTask extends Component {
 
@@ -20,12 +28,13 @@ class CreateTask extends Component {
             deadline: '',
             dependency: '',
             withDetails: false,
-            focusedInput: ''
+            focusedInput: '',
+            confirmCreation: false
         };
     }
 
     handleClose() {
-        this.setState({ show: false });
+        this.setState({ show: false, confirmCreation: false });
     }
 
     handleShow() {
@@ -34,37 +43,111 @@ class CreateTask extends Component {
 
     handleChange = (event) => {
         let input = event.target.value;
-        let taskDescription = input.replace(/[^a-z0-9]/gi, '');
+        let taskDescription = input.replace(/[^a-z0-9 ]/gi, '');
         this.setState({ description: taskDescription })
     }
 
     createWithDetails() {
         this.setState({ withDetails: !this.state.withDetails })
+        if (this.state.withDetails === false) {
+            this.setState({ expectedStart: '', deadline: '' })
+        }
     }
 
-    createTask() {
 
-        let taskDetails = {
-            description: this.state.description
-        };
+    displayConfirmation() {
 
-        console.log(taskDetails)
+        if (this.props.beingCreated)
+            return (
+                <div>
+                    <Modal.Header >
+                        <Modal.Title>Confirm Task Creation</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="loadingTaskWrapper">
+                            <center><div class="loadingTask"></div> </center></div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    </Modal.Footer>
+                </div>
+            )
+
+        return (
+            <div>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Task Creation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p><b>Description:</b> {this.props.task.description} </p>
+                    <p><b>Task ID:</b> {this.props.task.taskID} </p>
+                    <p><b>Estimated Start Date:</b> {this.props.task.estimatedTaskStartDate} </p>
+                    <p><b>Estimated Deadline:</b> {this.props.task.taskDeadline} </p>
+                    <p><b>State:</b> {this.props.task.currentState} </p>
+                    <p><b>Project:</b> {this.props.projectId} </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <span><button className="cancelButton" onClick={this.cancelCreation.bind(this)}>Cancel</button></span><span><button className="genericButton" onClick={this.confirmCreation.bind(this)}>Confirm</button> </span>
+                </Modal.Footer>
+            </div>
+        )
+    }
+
+    cancelCreation = () => {
+        this.setState({ confirmCreation: false })
+        console.log(this.props.projectId)
+        this.props.deleteTaskCreated(this.props.projectId, this.props.task.taskID)
+
+    }
+
+    confirmCreation() {
+        this.setState({ show: false, confirmCreation: false });
+
+        switch (this.props.filter) {
+            case 'notstarted':
+                this.props.updateNotStartedTasks(this.props.projectId)
+            case 'unfinished':
+                this.props.updateUnfinishedTasks(this.props.projectId)
+            default:
+                this.props.updateAllTasks(this.props.projectId)
+        }
+        toastr.success('Task successfully created');
+    }
+
+
+    createTaskAndDisplayConfirmation() {
+
+        this.setState({ confirmCreation: !this.state.confirmCreation })
+
+        let taskDetails = ''
+        if (this.state.withDetails)
+            taskDetails = {
+                description: this.state.description,
+                estimatedTaskStartDate: this.state.expectedStart,
+                taskDeadline: this.state.deadline
+            };
+        else {
+            taskDetails = {
+                description: this.state.description
+            };
+        }
+
         this.props.createTask(taskDetails, this.props.projectId)
     }
 
     renderDetails() {
         return (
-
             <FormGroup bsSize="large">
-                <ControlLabel>Expected Dates</ControlLabel>
+                <ControlLabel>Estimated Dates</ControlLabel>
                 <div className="calendar">
                     <DateRangePicker
+                        startDatePlaceholderText='Estimated Start'
+                        endDatePlaceholderText='Deadline'
                         endDateId='endDate1'
                         startDateId='startDate1'
-                        startDate={this.state.estimatedStart}
                         showClearDate
+                        startDate={this.state.expectedStart}
                         endDate={this.state.deadline}
-                        onDatesChange={({ startDate, endDate }) => this.setState({ estimatedStart: startDate, deadline: endDate })}
+                        onDatesChange={({ startDate, endDate }) => this.setState({ expectedStart: startDate, deadline: endDate })}
                         placeholder="dasdad"
                         focusedInput={this.state.focusedInput}
                         onFocusChange={focusedInput => this.setState({ focusedInput })}
@@ -72,66 +155,76 @@ class CreateTask extends Component {
                         inputIconPosition="after"
                     />
                 </div>
-                {this.renderTaskDependencies()}
             </FormGroup>
 
         )
     }
 
-    renderTaskDependencies() {
-        return (
-            <FormGroup bsSize="large">
-                <ControlLabel>Create Task Dependency</ControlLabel>
-                <FormControl componentClass="select" placeholder="Select">
-                    <option value="other">...</option>
-                </FormControl>
-            </FormGroup>
-        )
-    }
-
-
-    render() {
-
-        console.log(this.state.expectedStart)
+    renderTaskCreationProcess() {
 
         let details = !this.state.withDetails ? '+details' : 'less'
 
         return (
             <div>
-                <button className="genericButton" onClick={this.handleShow.bind(this)}>
-                    Create Task
-            </button>
 
-                <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Create Task</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form >
-                            <FormGroup bsSize="large">
-                                <ControlLabel>Task Description</ControlLabel>
-                                <FormControl id="descriptionForm"
-                                    autoFocus
-                                    type="text"
-                                    value={this.state.description}
-                                    onChange={this.handleChange}
-                                    placeholder="Enter description"
-                                />
-                            </FormGroup>
-                        </Form>
-                        {this.state.withDetails ? this.renderDetails() : null}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <div className="moreDetails" align="right"><p onClick={this.createWithDetails.bind(this)}>{details}</p></div>
-                        <button className="genericButton" onClick={this.createTask.bind(this)}>Create</button>
-                    </Modal.Footer>
-                </Modal>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create Task</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form >
+                        <FormGroup bsSize="large">
+                            <ControlLabel>Task Description</ControlLabel>
+                            <FormControl id="descriptionForm"
+                                autoFocus
+                                type="text"
+                                value={this.state.description}
+                                onChange={this.handleChange}
+                                placeholder="Enter description"
+                            />
+                        </FormGroup>
+                    </Form>
+                    {this.state.withDetails ? this.renderDetails() : null}
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className="moreDetails" align="right"><p onClick={this.createWithDetails.bind(this)}>{details}</p></div>
+                    <button className="genericButton" onClick={this.createTaskAndDisplayConfirmation.bind(this)}>Create</button>
+                </Modal.Footer>
             </div>
         )
     }
 
+    render() {
+
+        var toRender = this.renderTaskCreationProcess()
+
+        if (this.state.confirmCreation) {
+            toRender = this.displayConfirmation()
+        }
+
+        return (
+
+            <div>
+                <button className="genericButton" onClick={this.handleShow.bind(this)}>
+                    Create Task
+        </button>
+
+                <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
+                    {toRender}
+                </Modal>
+            </div>
+
+        )
+    }
+
 }
+const mapStateToProps = state => {
+    return {
+        taskCreated: state.createTask.taskCreated,
+        filter: state.filterReducer.filterType,
+        task: state.createTask.task,
+        beingCreated: state.createTask.beingCreated
+    };
+};
+const mapDispatchToProps = dispatch => bindActionCreators({ createTask, deleteTaskCreated, updateAllTasks, updateNotStartedTasks, updateUnfinishedTasks }, dispatch);
 
-const mapDispatchToProps = dispatch => bindActionCreators({ createTask }, dispatch);
-
-export default connect(null, mapDispatchToProps)(CreateTask);
+export default connect(mapStateToProps, mapDispatchToProps)(CreateTask);
