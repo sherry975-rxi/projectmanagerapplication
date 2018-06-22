0a10b7ee796b2809440d3e0224e30c5b9df8e076
Daniel Almeida <1171459@isep.ipp.pt>
2018-06-22 16:28:40 +0100

    addresses #307 Added case 'Task_Deleted' to the createTaskReducer

--- group3_frontend/src/reducers/createTaskReducer.js
+++ group3_frontend/src/reducers/createTaskReducer.js
@@ -1,13 +1,16 @@
 const INITIAL_STATE = {
     tasksToCreateDependency: [],
     beingCreated: false,
-    taskCreated: false
+    taskCreated: false,
+    task: ''
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'TASK_CREATED':
-            return { ...state, beingCreated: false, taskCreated: true }
+            return { ...state, beingCreated: false, taskCreated: true, task: action.task }
+        case 'TASK_DELETED':
+            return { ...state, beingCreated: false, taskCreated: false, task: {} }
         case 'DEPENDENCY_TASKS_FETCHED':
             return { ...state, tasksToCreateDependency: action.dependencyTasks, beingCreated: false, taskCreated: false }
         case 'TASK_IS_BEING_CREATED':
