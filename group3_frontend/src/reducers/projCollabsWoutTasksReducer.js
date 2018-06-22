65b86fa471f0786e54783168addea5b83c51171a
Rita Machado <1171471@isep.ipp.pt>
2018-06-22 16:59:31 +0100

    addresses #317 update in reducer ItemIsLoading

--- group3_frontend/src/reducers/projCollabsWoutTasksReducer.js
+++ group3_frontend/src/reducers/projCollabsWoutTasksReducer.js
@@ -9,7 +9,7 @@ export default (state = INITIAL_STATE, action) => {
         case 'COLLABWOUTTASKS_FETCHED':
             return { ...state, itemIsLoading: false, collabs: action.collabs, error: false }
         case 'ITEMCOLLABSWOUTTASKS_LOADING':
-            return { ...state, itemIsLoading: true, allTasks: action.allTasks, error: false }
+            return { ...state, itemIsLoading: true, collabs: action.collabs, error: false }
         case 'FETCH_HAS_ERRORED':
             return { ...state, itemIsLoading: false, error: true }
         default:
