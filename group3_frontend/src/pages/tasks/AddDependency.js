149091d21113b4666881e2c484419e58f2521fe1
João Bessa <1171465@isep.ipp.pt>
2018-06-27 17:00:51 +0100

    #328 Add Dependency component can now recieve a number of days to delay the estimated start date of the Child task according to the expected deadline of the parent task

--- group3_frontend/src/pages/tasks/AddDependency.js
+++ group3_frontend/src/pages/tasks/AddDependency.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { createTaskDependency, updateNotStartedTasks } from "../../actions/projectTasksActions";
-import { DropdownButton, MenuItem } from "react-bootstrap";
+import '../../components/button/genericButton.css'
+import {ControlLabel, DropdownButton, FormControl, FormGroup, MenuItem} from "react-bootstrap";
 import { toastr } from "react-redux-toastr";
 
 
@@ -10,21 +11,52 @@ class AddDependency extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            open: false,
+            daysToPostpone: 0
+        }
+
+    }
+
+    validateForm() {
+        return this.state.daysToPostpone >= 0;
     }
 
     handleClick = () => {
-        console.log(this.props.taskList);
-        this.props.updateNotStartedTasks(this.props.projectID);
+        if(!this.state.open) {
+            console.log(this.props.taskList);
+            this.props.updateNotStartedTasks(this.props.projectID);
+            this.setState({
+                open: true
+            })
+        } else {
+            this.setState({
+                open: false
+            })
+        }
+
     }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    };
+
+
+
 
     handleSelect = eventKey => {
 
         console.log(this.props.taskList[eventKey].taskID);
 
-        this.props.createTaskDependency(this.props.projectID, this.props.taskID, this.props.taskList[eventKey].taskID, 2);
+        this.props.createTaskDependency(this.props.projectID, this.props.taskID, this.props.taskList[eventKey].taskID, this.state.daysToPostpone);
 
         if(this.props.error) {
             toastr.success('Dependency added!');
+            this.setState({
+                open: false
+            })
         } else {
             toastr.error('lolnope');
         }
@@ -43,9 +75,26 @@ class AddDependency extends Component {
                 id={`dropdown-basic-0`}
                 value={this.props.projectID}
                 onClick={this.handleClick}
+                open={this.state.open}
             >
+                <MenuItem
+                    eventKey="w0t"
+                >
+                    <FormGroup controlId="daysToPostpone" bsSize="small">
+                        <ControlLabel>Days To Postpone</ControlLabel>
+                        <FormControl
+                            autoFocus
+                            type="number"
+                            pattern="[0-9]*"
+                            inputMode="numeric"
+                            value={this.state.daysToPostpone}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                </MenuItem>
                 {taskList.map((listItem, index) => (
                     <MenuItem
+                        disabled={!this.validateForm()}
                         eventKey={index}
                         key={index}
                         onSelect={this.handleSelect}
@@ -61,7 +110,8 @@ class AddDependency extends Component {
 const mapStateToProps = state => {
     return {
         taskList: state.tasks.tasksList,
-        error: state.tasks.error
+        error: state.tasks.error,
+        loading: state.tasks.loading
     };
 };
 
