eaf05d0df01425aad3ac8bffb0c7b3702edf23ae
João Leite <1161661@isep.ipp.pt>
2018-06-27 11:08:12 +0100

    addresses 429 - Small fixes in filter and css in FetchTask

--- group3_frontend/src/pages/tasks/UserTasks.js
+++ group3_frontend/src/pages/tasks/UserTasks.js
@@ -66,9 +66,10 @@ class UserTasks extends Component {
       
             return (
                 <div className=" table-striped">
+                                        <b>My Tasks</b>
+
                     <UserTasksFilter userID={this.AuthService.getUserId()} />
                         <h3>
-                        <b>My Tasks</b>
                         </h3>
                         &nbsp;
                         {this.renderTasks()}
