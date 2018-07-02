8b5851d42983441abc415ed461053394ff139f61
Daniel Pereira <1171460@isep.ipp.pt>
2018-07-02 12:43:09 +0100

    refs #294 Search bar was present for user tasks, but not for project tasks. Now it also appears in the project tasks menu.

--- group3_frontend/src/reducers/tasksReducer.js
+++ group3_frontend/src/reducers/tasksReducer.js
@@ -18,6 +18,7 @@ export default (state = {}, action) => {
                 ...state,
                 itemIsLoading: false,
                 tasksList: action.tasks,
+                updatedList: action.tasks,
                 error: false
             };
         case 'DEPENDENCIES_FETCHED':
@@ -52,7 +53,8 @@ export default (state = {}, action) => {
                 ...state,
                 taskFilter: action.filter
             };
-
+        case 'SEARCHTASKS_FETCHED':
+            return { ...state, itemIsLoading: false, updatedList: action.updatedList, error: false }
         default:
             return state;
     }
