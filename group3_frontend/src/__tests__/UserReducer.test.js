f7859aee635df5167ecf41a6bd0f0403d7365e8b
Inês Pinto <1171476@isep.ipp.pt>
2018-07-04 22:40:18 +0100

    refs #338 Updates in UserReducer tests and create usersFilterReducer.

--- group3_frontend/src/__tests__/UserReducer.test.js
+++ group3_frontend/src/__tests__/UserReducer.test.js
@@ -1,27 +1,112 @@
-import reducer from '../reducers/UserReducers'
-import * as types from '../actions/actions'
+import reducer from '../reducers/UserReducers';
+import * as types from '../actions/actions';
 
 describe('UserReducer', () => {
-  it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-      {
-        usersUpdated: false,
-        allUsers: [],
-        emailUsers: [],
-        allCollaborators: [],
-        allDirector: [],
-        allAdministrator: [],
-        searchList: [],
-        allVisitors:[]
-      }
-    )
-  })
+    const initialState = {
+        usersUpdated: false
+    };
 
-  it('should handle ALL USERS and return the array empty', () => {
-    expect(reducer([] , {        
-        type: types.ALLUSERS_FETCHED,
-        allUsers: {user1: 'mike',user2: 'john',user3: 'lily'}})
-    ).toEqual({allUsers: {user1: 'mike',user2: 'john',user3: 'lily'}, error: undefined, usersUpdated: true})
-})
-})
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            usersUpdated: false,
+            allUsers: [],
+            emailUsers: [],
+            allCollaborators: [],
+            allDirector: [],
+            allAdministrator: [],
+            searchList: [],
+            allVisitors: []
+        });
+    });
 
+    it('should handle ALL USERS and return the array empty', () => {
+        expect(
+            reducer([], {
+                type: types.ALLUSERS_FETCHED,
+                allUsers: { user1: 'mike', user2: 'john', user3: 'lily' }
+            })
+        ).toEqual({
+            allUsers: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        });
+    });
+
+    it('should change the state for action type EMAILUSERS_FETCHED', () => {
+        const expectedState = {
+            emailUsers: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        };
+        const action = {
+            type: 'EMAILUSERS_FETCHED',
+            emailUsers: { user1: 'mike', user2: 'john', user3: 'lily' }
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+    });
+
+    it('should change the state for action type ALLCOLLABORATORS_FETCHED', () => {
+        const expectedState = {
+            allCollaborators: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        };
+        const action = {
+            type: 'ALLCOLLABORATORS_FETCHED',
+            allCollaborators: { user1: 'mike', user2: 'john', user3: 'lily' }
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+    });
+
+    it('should change the state for action type ALLDIRECTORS_FETCHED', () => {
+        const expectedState = {
+            allDirector: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        };
+        const action = {
+            type: 'ALLDIRECTORS_FETCHED',
+            allDirector: { user1: 'mike', user2: 'john', user3: 'lily' }
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+    });
+
+    it('should change the state for action type ALLADMINISTRATOR_FETCHED', () => {
+        const expectedState = {
+            allAdministrator: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        };
+        const action = {
+            type: 'ALLADMINISTRATOR_FETCHED',
+            allAdministrator: { user1: 'mike', user2: 'john', user3: 'lily' }
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+    });
+
+    it('should change the state for action type SEARCHUSERS_FETCHED', () => {
+        const expectedState = {
+            searchList: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        };
+        const action = {
+            type: 'SEARCHUSERS_FETCHED',
+            searchList: { user1: 'mike', user2: 'john', user3: 'lily' }
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+    });
+
+    it('should change the state for action type ALLVISITORS_FETCHED', () => {
+        const expectedState = {
+            allVisitors: { user1: 'mike', user2: 'john', user3: 'lily' },
+            error: undefined,
+            usersUpdated: true
+        };
+        const action = {
+            type: 'ALLVISITORS_FETCHED',
+            allVisitors: { user1: 'mike', user2: 'john', user3: 'lily' }
+        };
+        expect(reducer(initialState, action)).toEqual(expectedState);
+    });
+});
